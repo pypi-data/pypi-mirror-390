f25fa8e27ab8a4e8cb3c372c87ce96da89c7be22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:04:50 +0000

    Add package agglovar/0.0.1.dev4/agglovar-0.0.1.dev4.tar.gz

---
+++ packages/agglovar/agglovar-0.0.1.dev4.tar.gz/agglovar-0.0.1.dev4/src/agglovar/__init__.py
@@ -0,0 +1,25 @@
+"""Agglovar: A toolkit for fast genomic variant transformations and intersects."""
+
+__version__ = '0.0.1.dev4'
+
+__all__ = [
+    'align',
+    'fa',
+    'pairwise',
+    'io',
+    'kmer',
+    'merge',
+    'meta',
+    'schema',
+    'seqmatch',
+]
+
+from . import align
+from . import fa
+from . import pairwise
+from . import io
+from . import kmer
+from . import merge
+from . import meta
+from . import schema
+from . import seqmatch
