09139d95cc25b6db6c4da2b8e92c488d52571f43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 15:04:20 +0000

    Add package scUNAGI/0.5.4/scunagi-0.5.4-py3-none-any.whl

---
+++ packages/scUNAGI/scunagi-0.5.4-py3-none-any.whl/UNAGI/perturbations/__init__.py
@@ -0,0 +1,6 @@
+'''
+The perturbations module allows to perform the pathway and compounds in-silico perturbations and obatin the perturbation results.
+'''
+
+from .compounds import get_top_compounds, get_top_single_genes
+from .pathways import get_top_pathways
