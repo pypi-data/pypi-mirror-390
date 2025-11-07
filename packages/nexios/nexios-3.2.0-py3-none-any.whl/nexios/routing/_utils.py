00943be0d4b1b55fbaf317608d79cf2be90a8f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:41 +0000

    Add package nexios/3.2.0/nexios-3.2.0-py3-none-any.whl

---
+++ packages/nexios/nexios-3.2.0-py3-none-any.whl/nexios/routing/_utils.py
@@ -0,0 +1,19 @@
+from nexios.types import Scope
+
+
+def get_route_path(scope: Scope) -> str:
+    path: str = scope["path"]
+    root_path = scope.get("root_path", "")
+    if not root_path:
+        return path
+
+    if not path.startswith(root_path):
+        return path
+
+    if path == root_path:
+        return ""
+
+    if path[len(root_path)] == "/":
+        return path[len(root_path) :]
+
+    return path
