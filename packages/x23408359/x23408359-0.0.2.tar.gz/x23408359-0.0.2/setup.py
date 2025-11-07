d28102dcdb57dea1c4a81c12f3e9ff1e3b48b7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:08 +0000

    Add package x23408359/0.0.2/x23408359-0.0.2.tar.gz

---
+++ packages/x23408359/x23408359-0.0.2.tar.gz/x23408359-0.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setuptools.setup(
+name="x23408359",
+# Replace with your own username above
+version="0.0.2",
+author="aftab_ahmed",
+author_email="aftabkhna41.ak92@gmail.com",
+description="carbon emission and absorption caluclator",
+long_description=long_description,
+long_description_content_type="text/markdown",
+url="https://github.com/pypa/sampleproject",
+packages=setuptools.find_packages(),
+# if you have libraries that your module/package/library
+#you would include them in the install_requires argument
+install_requires=[''],
+classifiers=[
+"Programming Language :: Python :: 3",
+"License :: OSI Approved :: MIT License",
+"Operating System :: OS Independent",
+],
+python_requires='>=3.6',
+)
