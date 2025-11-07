d289d7ef796aac9cc606e877a28dbad2d208a1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 16:05:48 +0000

    Add package dicom-ups/0.0.6/dicom_ups-0.0.6-py3-none-any.whl

---
+++ packages/dicom-ups/dicom_ups-0.0.6-py3-none-any.whl/dicom_ups/options.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import enum
+import os
+import tomllib
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Final
+
+
+@dataclass(frozen=True)
+class Server:
+    host: str
+    port: int | None
+    ae: str | None
+
+    def __str__(self) -> str:
+        if self.ae:
+            return f'DIMSE: "{self.ae}" {self.host}:{self.port}'
+        return f'DICOM Web: "{self.host}"'
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class ActionType(enum.IntEnum):
+    DELETE = 1
+    RESTART = 2
+    SUBSCRIBE = 3
+    UNSUBSCRIBE = 4
+    SUSPEND_SUBSCRIPTION = 5
+
+
+def get_servers() -> dict[str, Server]:
+    servers: dict[str, Server] = {}
+
+    with Path(os.environ['SERVERS_TOML']).open('rb') as f:
+        data = tomllib.load(f)
+
+    for k, v in data['server'].items():
+        servers[k] = Server(v['host'], int(v['port']) if 'port' in v else None, v.get('ae', None))
+
+    return servers
+
+
+TIMEOUT: Final[int] = 15 * 60  # Timeout 15 min
