41611e56292f1ac6d2678ec2d59217dea63a36cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:05:41 +0000

    Add package odoo-addon-l10n-es-vat-prorate/16.0.2.0.2/odoo_addon_l10n_es_vat_prorate-16.0.2.0.2-py3-none-any.whl

---
+++ packages/odoo-addon-l10n-es-vat-prorate/odoo_addon_l10n_es_vat_prorate-16.0.2.0.2-py3-none-any.whl/odoo/addons/l10n_es_vat_prorate/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2022 Creu Blanca
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Prorrata de IVA",
+    "summary": "Prorrata de IVA para la localización española",
+    "version": "16.0.2.0.2",
+    "license": "AGPL-3",
+    "author": "Creu Blanca, Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-spain",
+    "pre_init_hook": "pre_init_hook",
+    "depends": ["l10n_es_aeat"],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/account_template_tax.xml",
+        "views/account_move_views.xml",
+        "views/account_tax_views.xml",
+        "views/res_company_prorate_views.xml",
+        "views/res_company_views.xml",
+    ],
+}
---
+++ packages/odoo-addon-l10n-es-vat-prorate/odoo_addon_l10n_es_vat_prorate-16.0.2.0.2-py3-none-any.whl/odoo/addons/l10n_es_vat_prorate/readme/CONTRIBUTORS.rst
@@ -0,0 +1,8 @@
+* Enric Tobella
+* `Tecnativa <https://www.tecnativa.com/>`_:
+
+  * Pedro M. Baeza
+  * Carolina Fernandez
+* `Sygel <https://www.sygel.es/>`_:
+  * Harald Panten
+  * Alberto Martínez
