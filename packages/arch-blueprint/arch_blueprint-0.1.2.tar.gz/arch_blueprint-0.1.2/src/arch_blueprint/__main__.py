49ce31708d4adc1a84bb17be92ba2eb7d2b0c129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:05:40 +0000

    Add package arch-blueprint/0.1.2/arch_blueprint-0.1.2.tar.gz

---
+++ packages/arch-blueprint/arch_blueprint-0.1.2.tar.gz/arch_blueprint-0.1.2/src/arch_blueprint/__main__.py
@@ -0,0 +1,15 @@
+from arch_blueprint.blueprint import ArchBlueprint
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate component diagrams in plantuml for python applications")
+    parser.add_argument("root", type=str, help="Name of root python module in project (example: 'myapp'")
+    parser.add_argument("--modules", "-m", type=str, nargs='*', action='extend',
+                        help="Selected modules for rendering (examples: 'myapp.somemodule', 'myapp.somemodule.*', 'myapp.somemodule.**')", )
+    args = parser.parse_args()
+
+    ArchBlueprint(root=args.root, target_names=args.modules).run()
+
+
+main()
