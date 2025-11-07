cdf6af8dd25e963dfa3c90ccf5a51dc9712ee0a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 16:05:49 +0000

    Add package zope.event/6.1/zope_event-6.1.tar.gz

---
+++ packages/zope.event/zope_event-6.1.tar.gz/zope_event-6.1/setup.py
@@ -0,0 +1,75 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+# This package is developed by the Zope Toolkit project, documented here:
+# https://zopetoolkit.readthedocs.io/
+# When developing and releasing this package, please follow the documented
+# Zope Toolkit policies as described by this documentation.
+##############################################################################
+"""Setup for zope.event package
+"""
+
+import os
+
+from setuptools import setup
+
+
+def read(*rnames):
+    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
+        return f.read()
+
+
+setup(
+    name='zope.event',
+    version='6.1',
+    url='https://github.com/zopefoundation/zope.event',
+    license='ZPL-2.1',
+    license_files=['LICENSE.txt'],
+    description='Very basic event publishing system',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.dev',
+    long_description=(
+        read('README.rst')
+        + '\n' +
+        read('CHANGES.rst')
+    ),
+    keywords="event framework dispatch subscribe publish",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: Jython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Zope :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    include_package_data=True,
+    python_requires='>=3.10',
+    zip_safe=False,
+    extras_require={
+        'docs': [
+            'Sphinx',
+        ],
+        'test': [
+            'zope.testrunner >= 6.4',
+        ],
+    },
+)
