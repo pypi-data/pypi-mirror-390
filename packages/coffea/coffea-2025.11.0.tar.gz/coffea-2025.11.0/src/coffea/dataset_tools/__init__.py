bfab6ae9cc87b98645fb776c314aefbdbf076207
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 15:05:18 +0000

    Add package coffea/2025.11.0/coffea-2025.11.0.tar.gz

---
+++ packages/coffea/coffea-2025.11.0.tar.gz/coffea-2025.11.0/src/coffea/dataset_tools/__init__.py
@@ -0,0 +1,26 @@
+from coffea.dataset_tools.apply_processor import apply_to_dataset, apply_to_fileset
+from coffea.dataset_tools.manipulations import (
+    filter_files,
+    get_failed_steps_for_dataset,
+    get_failed_steps_for_fileset,
+    max_chunks,
+    max_chunks_per_file,
+    max_files,
+    slice_chunks,
+    slice_files,
+)
+from coffea.dataset_tools.preprocess import preprocess
+
+__all__ = [
+    "preprocess",
+    "apply_to_dataset",
+    "apply_to_fileset",
+    "max_chunks",
+    "max_chunks_per_file",
+    "slice_chunks",
+    "filter_files",
+    "max_files",
+    "slice_files",
+    "get_failed_steps_for_dataset",
+    "get_failed_steps_for_fileset",
+]
