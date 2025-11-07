ed7bfda8768dd7711a48a0cd01d389ef9d4120b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:41 +0000

    Add package space-packet-parser/6.0.1/space_packet_parser-6.0.1.tar.gz

---
+++ packages/space-packet-parser/space_packet_parser-6.0.1.tar.gz/space_packet_parser-6.0.1/space_packet_parser/__init__.py
@@ -0,0 +1,34 @@
+"""Space Packet Parser"""
+
+from pathlib import Path
+from typing import Union
+
+from space_packet_parser.common import SpacePacket
+from space_packet_parser.generators import ccsds_generator
+from space_packet_parser.xtce.definitions import XtcePacketDefinition
+from space_packet_parser.xtce.validation import validate_xtce
+
+__all__ = [
+    "ccsds_generator",
+    "SpacePacket",
+    "XtcePacketDefinition",
+    "load_xtce",
+    "validate_xtce",
+]
+
+
+def load_xtce(filename: Union[str, Path]) -> XtcePacketDefinition:
+    """Create an XtcePacketDefinition object from an XTCE XML file
+
+    This is a shortcut for calling XtcePacketDefinition.from_xtce().
+
+    Parameters
+    ----------
+    filename : Union[str, Path]
+        XTCE XML file
+
+    Returns
+    -------
+    : definitions.XtcePacketDefinition
+    """
+    return XtcePacketDefinition.from_xtce(filename)
