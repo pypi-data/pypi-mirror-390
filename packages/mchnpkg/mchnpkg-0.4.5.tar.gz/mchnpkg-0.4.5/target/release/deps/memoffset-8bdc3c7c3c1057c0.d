1569ce89c6d4afa70b0fee9c835aa851fe2ae685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:58 +0000

    Add package mchnpkg/0.4.5/mchnpkg-0.4.5.tar.gz

---
+++ packages/mchnpkg/mchnpkg-0.4.5.tar.gz/mchnpkg-0.4.5/target/release/deps/memoffset-8bdc3c7c3c1057c0.d
@@ -0,0 +1,10 @@
+/home/ayr0001/mchnpkg/target/release/deps/memoffset-8bdc3c7c3c1057c0.d: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/raw_field.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/offset_of.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/span_of.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libmemoffset-8bdc3c7c3c1057c0.rlib: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/raw_field.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/offset_of.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/span_of.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libmemoffset-8bdc3c7c3c1057c0.rmeta: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/raw_field.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/offset_of.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/span_of.rs
+
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/lib.rs:
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/raw_field.rs:
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/offset_of.rs:
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/memoffset-0.9.1/src/span_of.rs:
---
+++ packages/mchnpkg/mchnpkg-0.4.5.tar.gz/mchnpkg-0.4.5/target/release/deps/unindent-779d2a22fd68e5b6.d
@@ -0,0 +1,8 @@
+/home/ayr0001/mchnpkg/target/release/deps/unindent-779d2a22fd68e5b6.d: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libunindent-779d2a22fd68e5b6.rlib: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libunindent-779d2a22fd68e5b6.rmeta: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs
+
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/lib.rs:
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unindent-0.2.4/src/unindent.rs:
