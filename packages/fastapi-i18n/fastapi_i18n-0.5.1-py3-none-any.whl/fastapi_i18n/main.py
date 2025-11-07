2ab8c54cc701af8a9e93a7616e17120ab9ee6930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 16:05:58 +0000

    Add package fastapi-i18n/0.5.1/fastapi_i18n-0.5.1-py3-none-any.whl

---
+++ packages/fastapi-i18n/fastapi_i18n-0.5.1-py3-none-any.whl/fastapi_i18n/main.py
@@ -0,0 +1,55 @@
+# TODO: Read config from pyproject.toml and fail if config is not found
+import gettext
+import logging
+import os
+from contextvars import ContextVar
+
+from fastapi import Request
+
+
+class Translator:
+    def __init__(self, locale: str):
+        locale_dir = os.getenv("FASTAPI_I18N_LOCALE_DIR")
+        self.translations = gettext.translation(
+            domain="messages",
+            localedir=locale_dir,
+            languages=[locale],
+            fallback=True,
+        )
+
+    def translate(self, message: str):
+        return self.translations.gettext(message)
+
+
+locale: ContextVar[str] = ContextVar("locale")
+translator: ContextVar[Translator] = ContextVar("translator")
+
+
+async def i18n(request: Request):
+    locale_default = os.getenv("FASTAPI_I18N_LOCALE_DEFAULT", "en")
+    locale_value = request.headers.get("Accept-Language", locale_default)
+    token_locale = locale.set(locale_value)
+    token_translator = translator.set(Translator(locale=locale_value))
+    try:
+        yield
+    finally:
+        locale.reset(token_locale)
+        translator.reset(token_translator)
+
+
+def _(message: str) -> str:
+    try:
+        return translator.get().translate(message)
+    except LookupError:
+        logging.debug(
+            "FastAPI I18N translator is not set. Returning message untranslated."
+        )
+        return message
+
+
+def get_locale() -> str:
+    """Get the current setting for locale."""
+    try:
+        return locale.get()
+    except LookupError:
+        return os.getenv("FASTAPI_I18N_LOCALE_DEFAULT", "en")
