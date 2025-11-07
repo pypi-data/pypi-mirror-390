2b97d716394180141a356227a54de3135b42be5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 15:05:15 +0000

    Add package whoruv/0.0.3/whoruv-0.0.3.tar.gz

---
+++ packages/whoruv/whoruv-0.0.3.tar.gz/whoruv-0.0.3/src/whoruv/__init__.py
@@ -0,0 +1,4 @@
+from whoruv.main import main
+from whoruv.whoruv import whoruv, format_python_info, PythonInfo
+
+__all__ = ["main", "whoruv", "format_python_info", "PythonInfo"]
---
+++ packages/whoruv/whoruv-0.0.3.tar.gz/whoruv-0.0.3/src/whoruv/main.py
@@ -0,0 +1,9 @@
+from whoruv.whoruv import whoruv, format_python_info
+
+
+def main() -> None:
+    print(format_python_info(whoruv()))
+
+
+if __name__ == "__main__":
+    main()
