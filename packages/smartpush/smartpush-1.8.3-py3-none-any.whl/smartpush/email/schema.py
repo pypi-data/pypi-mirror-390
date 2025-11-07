19212cf0e270c97b9ad9a4e55488ee871fa5060b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:22:25 +0000

    Add package smartpush/1.8.3/smartpush-1.8.3-py3-none-any.whl

---
+++ packages/smartpush/smartpush-1.8.3-py3-none-any.whl/smartpush/email/schema.py
@@ -0,0 +1,126 @@
+Logo = {
+    "id": "81886ba38",
+    "universalId": "8ff4fe49-c36f-426c-9f8c-d06e1a4877c3",
+    "universalName": "block",
+    "type": "Logo",
+    "props": {
+        "width": "120",
+        "height": "120",
+        "imgRatio": 1,
+        "src": "https://cdn.smartpushedm.com/frontend/smart-push/staging/1741054956275/1758595834300_74d62ae6.png?width=120&height=120",
+        "href": "https://lulu373.myshoplinestg.com",
+        "align": "center",
+        "containerBackgroundColor": "transparent",
+        "paddingLeft": "20px",
+        "paddingRight": "20px",
+        "paddingTop": "20px",
+        "paddingBottom": "20px",
+        "paddingCondition": True,
+        "segmentTypeConfig": 1
+    },
+    "children": []
+}
+Link = {
+    "id": "b78bcb99a",
+    "universalId": "c3db1401-ddae-4222-8ec3-b5dd9e6b16f3",
+    "universalName": "block",
+    "type": "Navigation",
+    "props": {
+        "moduleList": [
+            {
+                "title": "LINK",
+                "link": "",
+                "linkId": "d5c9ace8-ae39-42f3-ab64-3016da91a4ef"
+            },
+            {
+                "title": "LINK",
+                "link": "",
+                "linkId": "fb00e389-06bf-4060-b63f-8acbbb906670"
+            },
+            {
+                "title": "LINK",
+                "link": "",
+                "linkId": "ecd48104-fb52-460e-b2a1-b386ab18d10e"
+            }
+        ],
+        "color": "#242833",
+        "fontSize": "14px",
+        "fontFamily": "arial,helvetica,sans-serif,Arial, Helvetica, sans-serif",
+        "segmentLineColor": "#242833",
+        "segmentLineStyle": "solid",
+        "segmentLineWeight": "1px",
+        "paddingLeft": "10px",
+        "paddingRight": "10px",
+        "justifyContent": "center",
+        "paddingTop": "10px",
+        "paddingBottom": "10px",
+        "paddingCondition": True,
+        "containerBackgroundColor": "transparent"
+    },
+    "children": []
+}
+Image = {
+    "id": "a499b9a69",
+    "universalId": "a3d8c5f1-3384-492c-8984-81e424892357",
+    "universalName": "img",
+    "type": "Image",
+    "props": {
+        "width": "600px",
+        "height": "300px",
+        "src": "",
+        "href": "[[shopURL]]",
+        "align": "left",
+        "containerBackgroundColor": "transparent",
+        "paddingLeft": "0px",
+        "paddingRight": "0px",
+        "paddingTop": "0px",
+        "paddingBottom": "0px",
+        "paddingCondition": True
+    },
+    "children": []
+}
+ImageSet = {
+    "id": "a59b8ba3b",
+    "universalId": "0980b7a8-188e-4afb-b829-e3d93bb78347",
+    "universalName": "block",
+    "type": "ImageSet",
+    "props": {
+        "list": [
+            {
+                "src": "",
+                "width": "300px",
+                "height": "180px",
+                "imgRatio": 0.51,
+                "paddingLeft": "",
+                "paddingRight": "",
+                "paddingTop": "0px",
+                "paddingBottom": "0px",
+                "href": "[[shopURL]]",
+                "selected": False
+            },
+            {
+                "src": "",
+                "width": "300px",
+                "height": "180px",
+                "imgRatio": 0.51,
+                "paddingLeft": "",
+                "paddingRight": "",
+                "paddingTop": "0px",
+                "paddingBottom": "0px",
+                "href": "[[shopURL]]",
+                "selected": False
+            }
+        ],
+        "layout": "horizontal",
+        "layoutPadding": "5px",
+        "containerBackgroundColor": "#ffffff",
+        "paddingLeft": "5px",
+        "paddingRight": "5px",
+        "paddingTop": "10px",
+        "paddingBottom": "10px",
+        "paddingCondition": True,
+        "mobileSwitch": [],
+        "direction": "rtl"
+    },
+    "children": []
+}
