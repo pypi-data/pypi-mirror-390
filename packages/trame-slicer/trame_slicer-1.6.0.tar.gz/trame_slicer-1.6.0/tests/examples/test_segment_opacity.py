4d82a6c1086946b06963c04b2436060bae819a5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:22:23 +0000

    Add package trame-slicer/1.6.0/trame_slicer-1.6.0.tar.gz

---
+++ packages/trame-slicer/trame_slicer-1.6.0.tar.gz/trame_slicer-1.6.0/tests/examples/test_segment_opacity.py
@@ -0,0 +1,12 @@
+from trame_server.utils.typed_state import TypedState
+
+from examples.ui import MedicalViewerLayout, SegmentationOpacityUI, SegmentOpacityState
+
+
+def test_can_be_displayed(a_server, a_server_port):
+    typed_state = TypedState(a_server.state, SegmentOpacityState)
+
+    with MedicalViewerLayout(a_server, is_drawer_visible=True) as ui, ui.drawer:
+        SegmentationOpacityUI(typed_state)
+
+    a_server.start(port=a_server_port)
