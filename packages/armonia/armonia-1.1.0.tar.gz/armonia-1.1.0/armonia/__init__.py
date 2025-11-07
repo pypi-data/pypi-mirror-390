3a27a47be31395b1ba4207803da369470b4c0a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:41 +0000

    Add package armonia/1.1.0/armonia-1.1.0.tar.gz

---
+++ packages/armonia/armonia-1.1.0.tar.gz/armonia-1.1.0/armonia/__init__.py
@@ -0,0 +1,12 @@
+"""
+Armonia - A Python library for theming and color management.
+
+Built on top of polychromos for advanced color manipulation.
+"""
+
+from armonia import serialization
+from armonia.theme import Theme
+
+__version__ = "1.1.0"
+
+__all__ = ["__version__", "Theme", "serialization"]
