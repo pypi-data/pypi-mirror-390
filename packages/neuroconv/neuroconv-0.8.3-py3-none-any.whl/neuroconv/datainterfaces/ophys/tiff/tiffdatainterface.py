e4910d9ec5e6c9ca9e64635c13bc9da0c83be20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:04 +0000

    Add package neuroconv/0.8.3/neuroconv-0.8.3-py3-none-any.whl

---
+++ packages/neuroconv/neuroconv-0.8.3-py3-none-any.whl/neuroconv/datainterfaces/ophys/tiff/tiffdatainterface.py
@@ -0,0 +1,59 @@
+from typing import Literal
+
+from pydantic import FilePath, validate_call
+
+from ..baseimagingextractorinterface import BaseImagingExtractorInterface
+
+
+class TiffImagingInterface(BaseImagingExtractorInterface):
+    """Interface for multi-page TIFF files."""
+
+    display_name = "TIFF Imaging"
+    associated_suffixes = (".tif", ".tiff")
+    info = "Interface for multi-page TIFF files."
+
+    @classmethod
+    def get_source_schema(cls) -> dict:
+        """
+        Get the source schema for the TIFF imaging interface.
+
+        Returns
+        -------
+        dict
+            The JSON schema for the TIFF imaging interface source data,
+            containing file path and other configuration parameters.
+        """
+        source_schema = super().get_source_schema()
+        source_schema["properties"]["file_path"]["description"] = "Path to Tiff file."
+        return source_schema
+
+    @classmethod
+    def get_extractor_class(cls):
+        from roiextractors import TiffImagingExtractor
+
+        return TiffImagingExtractor
+
+    @validate_call
+    def __init__(
+        self,
+        file_path: FilePath,
+        sampling_frequency: float,
+        verbose: bool = False,
+        photon_series_type: Literal["OnePhotonSeries", "TwoPhotonSeries"] = "TwoPhotonSeries",
+    ):
+        """
+        Initialize reading of TIFF file.
+
+        Parameters
+        ----------
+        file_path : FilePath
+        sampling_frequency : float
+        verbose : bool, default: False
+        photon_series_type : {'OnePhotonSeries', 'TwoPhotonSeries'}, default: "TwoPhotonSeries"
+        """
+        super().__init__(
+            file_path=file_path,
+            sampling_frequency=sampling_frequency,
+            verbose=verbose,
+            photon_series_type=photon_series_type,
+        )
