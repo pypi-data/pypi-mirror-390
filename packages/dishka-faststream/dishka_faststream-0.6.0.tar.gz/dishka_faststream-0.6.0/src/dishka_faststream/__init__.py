4c4f2e3fbf44f96c166914f7b5129ec1646bf6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:54 +0000

    Add package dishka-faststream/0.6.0/dishka_faststream-0.6.0.tar.gz

---
+++ packages/dishka-faststream/dishka_faststream-0.6.0.tar.gz/dishka_faststream-0.6.0/src/dishka_faststream/__init__.py
@@ -0,0 +1,14 @@
+__all__ = (
+    "FastStreamProvider",
+    "FromDishka",
+    "inject",
+    "setup_dishka",
+)
+
+from dishka import FromDishka
+
+from .faststream import (
+    FastStreamProvider,
+    inject,
+    setup_dishka,
+)
