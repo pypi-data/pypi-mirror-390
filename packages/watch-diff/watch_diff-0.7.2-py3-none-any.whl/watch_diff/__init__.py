1ff5fa9065c9784e355235726bcfab2e570aa235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:44 +0000

    Add package watch-diff/0.7.2/watch_diff-0.7.2-py3-none-any.whl

---
+++ packages/watch-diff/watch_diff-0.7.2-py3-none-any.whl/watch_diff/__init__.py
@@ -0,0 +1,10 @@
+"""
+"""
+
+__version__ = '0.6.0'
+
+
+from .command import Command
+from .diff import Diff
+from .email import Email
+from .format import DefaultFormatter, ConsoleFormatter, HTMLFormatter, OutputFormatting
