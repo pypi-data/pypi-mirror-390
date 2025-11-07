65ab5fe611eaccbd3e864056a052bbc5148490ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:19 +0000

    Add package artistools/2025.11.6.3/artistools-2025.11.6.3-cp314-cp314-manylinux_2_28_x86_64.whl

---
+++ packages/artistools/artistools-2025.11.6.3-cp314-cp314-manylinux_2_28_x86_64.whl/artistools/gsinetwork/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["plot", "plotqdotabund"]
+
+from artistools.gsinetwork import plotqdotabund as plotqdotabund
+from artistools.gsinetwork.plotqdotabund import addargs as addargs
+from artistools.gsinetwork.plotqdotabund import main as plot
