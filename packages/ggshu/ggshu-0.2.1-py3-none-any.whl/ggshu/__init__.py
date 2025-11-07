8c7bdd9fa10b78ab2ef062bca8c0c10bf4149fe2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 17:04:39 +0000

    Add package ggshu/0.2.1/ggshu-0.2.1-py3-none-any.whl

---
+++ packages/ggshu/ggshu-0.2.1-py3-none-any.whl/ggshu/__init__.py
@@ -0,0 +1,11 @@
+from .aes import aes
+from .geoms import GeomArrow as geom_arrow
+from .geoms import GeomHist as geom_hist
+from .geoms import GeomKde as geom_kde
+from .geoms import GeomMetabolite as geom_metabolite
+from .geoms import GeomBoxPoint as geom_boxpoint
+from .geoms import GeomColumn as geom_column
+from .ggdata import PlotData as ggmap
+from .jupyter import Shu
+
+__version__ = "0.1.0"
