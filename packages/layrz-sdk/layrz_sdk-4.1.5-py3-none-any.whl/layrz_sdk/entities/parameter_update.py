5d348eb660a7d36902d168ec88b400f829e0e7ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 06:04:06 +0000

    Add package layrz-sdk/4.1.5/layrz_sdk-4.1.5-py3-none-any.whl

---
+++ packages/layrz-sdk/layrz_sdk-4.1.5-py3-none-any.whl/layrz_sdk/entities/parameter_update.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from typing import Any
+
+from pydantic import BaseModel, Field, field_serializer, field_validator
+
+
+class ParameterUpdate(BaseModel):
+  asset_id: int = Field(..., description='The unique identifier for the asset.')
+  parameter: str = Field(..., description='The name of the parameter.')
+  updated_at: datetime = Field(..., description='The timestamp when the parameter was updated.')
+  value: Any | None = Field(default=None, description='The current value of the parameter.')
+
+  @field_validator('parameter', mode='before')
+  def validate_parameter(cls, value: str) -> str:
+    return value.replace('__', '.')
+
+  @field_serializer('updated_at', when_used='always')
+  def serialize_updated_at(self, value: datetime) -> float:
+    return value.timestamp()
