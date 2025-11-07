3f6d0a646dafd0f7f43ed5414055fd7d6bda4a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:23 +0000

    Add package rid-lib/3.2.11/rid_lib-3.2.11.tar.gz

---
+++ packages/rid-lib/rid_lib-3.2.11.tar.gz/rid_lib-3.2.11/src/rid_lib/types/__init__.py
@@ -0,0 +1,13 @@
+from .http_s import HTTP, HTTPS
+from .slack_channel import SlackChannel
+from .slack_message import SlackMessage
+from .slack_workspace import SlackWorkspace
+from .slack_user import SlackUser
+from .koi_net_node import KoiNetNode
+from .koi_net_edge import KoiNetEdge
+from .normalized_text import NormalizedText
+from .hackmd_note import HackMDNote
+from .discord_guild import DiscordGuild
+from .discord_channel import DiscordChannel
+from .discord_message import DiscordMessage
+from .discord_user import DiscordUser
