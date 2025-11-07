cebb17ed13f06145bfb3da216fc4606d2e08529b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 06:04:06 +0000

    Add package homa/0.2.96/homa-0.2.96-py3-none-any.whl

---
+++ packages/homa/homa-0.2.96-py3-none-any.whl/homa/ensemble/concerns/ReportsClassificationMetrics.py
@@ -0,0 +1,13 @@
+from .ReportsEnsembleF1 import ReportsEnsembleF1
+from .ReportsEnsembleAccuracy import ReportsEnsembleAccuracy
+from .ReportsEnsembleKappa import ReportsEnsembleKappa
+from .CalculatesMetricNecessities import CalculatesMetricNecessities
+
+
+class ReportsClassificationMetrics(
+    CalculatesMetricNecessities,
+    ReportsEnsembleAccuracy,
+    ReportsEnsembleF1,
+    ReportsEnsembleKappa,
+):
+    pass
