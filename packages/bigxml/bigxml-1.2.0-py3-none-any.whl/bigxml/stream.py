9765178fa654c282d792bd82b1b1939cfba85c98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:46 +0000

    Add package bigxml/1.2.0/bigxml-1.2.0-py3-none-any.whl

---
+++ packages/bigxml/bigxml-1.2.0-py3-none-any.whl/bigxml/stream.py
@@ -0,0 +1,100 @@
+from collections.abc import Generator, Iterable
+from io import IOBase
+import sys
+from typing import Any, cast
+
+from bigxml.typing import Streamable, SupportsRead
+from bigxml.utils import autostart_generator
+
+if sys.version_info < (3, 12):  # pragma: no cover
+    from typing_extensions import Buffer
+else:  # pragma: no cover
+    from collections.abc import Buffer
+
+
+@autostart_generator
+def _flatten_stream(stream: Streamable) -> Generator[memoryview | None, int, None]:
+    yield None
+
+    # buffer protocol (bytes, etc.)
+    try:
+        # we try-except instead of isinstance(stream, Buffer) for compatibility reasons
+        yield memoryview(cast("Buffer", stream))
+        return  # noqa: TRY300
+    except TypeError:
+        pass
+
+    # file-like
+    if hasattr(stream, "read"):
+        while True:
+            size = yield None
+            data = cast("SupportsRead[Any]", stream).read(size)
+            if not data:
+                break  # EOF
+            try:
+                yield memoryview(data)
+            except TypeError as ex:
+                if isinstance(data, str):
+                    raise TypeError(
+                        "Stream read method returned a str, not a bytes-like object."
+                        " Open file objects in binary mode."
+                    ) from ex
+                raise TypeError(
+                    "Stream read method did not return a byte-like object:"
+                    f" {type(data).__name__}"
+                ) from ex
+        return
+
+    # known invalid type (need to be caught here since they are iterable)
+    # we disallow sets to avoid issues with ordering
+    if isinstance(stream, (str, set, dict)):
+        if isinstance(stream, str):
+            raise TypeError(
+                "Invalid stream type: str."
+                " Convert it to a bytes-like object by encoding it."
+            )
+        raise TypeError(f"Invalid stream type: {type(stream).__name__}")
+
+    # stream iterator (recursive)
+    try:
+        substreams = iter(cast("Iterable[Streamable]", stream))
+    except TypeError:
+        # other types not supported
+        raise TypeError(f"Invalid stream type: {type(stream).__name__}") from None
+
+    for substream in substreams:
+        yield from _flatten_stream(substream)
+
+
+@autostart_generator
+def _convert_to_read(
+    data_stream: Generator[memoryview | None, int, None],
+) -> Generator[bytes, int, None]:
+    size = yield b""
+    while True:
+        try:
+            buffer = data_stream.send(size)
+        except StopIteration:
+            break
+
+        while buffer:
+            data, buffer = buffer[:size], buffer[size:]
+            size = yield data.tobytes()
+
+    while True:
+        yield b""
+
+
+class StreamChain(IOBase):
+    def __init__(self, *streams: Streamable) -> None:
+        super().__init__()
+        self._read = _convert_to_read(_flatten_stream(streams))
+
+    def read(self, size: int | None = None) -> bytes:
+        if not isinstance(size, int) or size <= 0:
+            raise NotImplementedError("Read size must be strictly positive")
+        return self._read.send(size)
+
+    @staticmethod
+    def readable() -> bool:
+        return True
---
+++ packages/bigxml/bigxml-1.2.0-py3-none-any.whl/bigxml/utils.py
@@ -0,0 +1,104 @@
+from collections import deque
+from collections.abc import Callable, Generator, Iterable, Iterator
+from functools import wraps
+from inspect import Parameter, signature
+import re
+from typing import cast
+
+from bigxml.typing import P, T, U
+
+
+class IterWithRollback(Iterator[T]):
+    def __init__(self, iterable: Iterable[T]) -> None:
+        self.iteration = 0
+        self._iterator = iter(iterable)
+        self._can_rollback = False
+        self._item_rollback = False
+        self._last_item: T  # will be always set when read
+
+    def __iter__(self) -> Iterator[T]:  # noqa: PYI034
+        return self
+
+    def rollback(self) -> None:
+        if self._can_rollback:
+            self.iteration -= 1
+            self._can_rollback = False
+            self._item_rollback = True
+
+    def __next__(self) -> T:
+        if not self._item_rollback:
+            self._last_item = next(self._iterator)
+        self.iteration += 1
+        self._can_rollback = True
+        self._item_rollback = False
+        return self._last_item
+
+
+_EXTRACT_NAMESPACE_REGEX = re.compile(r"^\{([^}]*)\}(.*)$")
+
+
+def extract_namespace_name(name: str) -> tuple[str, str]:
+    match = _EXTRACT_NAMESPACE_REGEX.match(name)
+    if match:
+        return cast("tuple[str, str]", match.groups())
+    return ("", name)
+
+
+def last_item_or_none(iterable: Iterable[T]) -> T | None:
+    try:
+        return deque(iterable, maxlen=1)[0]
+    except IndexError:
+        return None
+
+
+def consume(iterable: Iterable[object]) -> bool:
+    iterator = iter(iterable)
+    try:
+        next(iterator)
+    except StopIteration:
+        return False
+    last_item_or_none(iterator)
+    return True
+
+
+def transform_to_iterator(
+    fct: Callable[P, Iterable[T] | None],
+) -> Callable[P, Iterator[T]]:
+    @wraps(fct)
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> Iterator[T]:
+        return_value = fct(*args, **kwargs)
+        if return_value is None:
+            return iter(())  # empty iterator
+        return iter(return_value)
+
+    return wrapped
+
+
+def get_mandatory_params(fct: Callable[..., object]) -> tuple[str, ...]:
+    try:
+        sig = signature(fct)
+    except (ValueError, TypeError):  # pragma: no cover
+        return ()  # e.g. for built-in
+    return tuple(
+        param.name
+        for param in sig.parameters.values()
+        if param.kind
+        in (
+            Parameter.POSITIONAL_ONLY,
+            Parameter.POSITIONAL_OR_KEYWORD,
+            Parameter.KEYWORD_ONLY,
+        )
+        and param.default == sig.empty
+    )
+
+
+def autostart_generator(
+    fct: Callable[P, Generator[T, U, None]],
+) -> Callable[P, Generator[T, U, None]]:
+    @wraps(fct)
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> Generator[T, U, None]:
+        generator = fct(*args, **kwargs)
+        next(generator)  # ignore first yielded value
+        return generator
+
+    return wrapped
