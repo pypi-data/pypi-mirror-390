7e62655ecf80c5c73bdd3886e404fcfb99e0d508
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:15 +0000

    Add package bdew-datetimes/0.10.0/bdew_datetimes-0.10.0-py3-none-any.whl

---
+++ packages/bdew-datetimes/bdew_datetimes-0.10.0-py3-none-any.whl/bdew_datetimes/calendar.py
@@ -0,0 +1,106 @@
+"""
+contains the business logic (namely the BDEW calendar information)
+
+"""
+
+from datetime import date
+from typing import Any
+
+from holidays import HolidayBase, HolidaySum
+from holidays.constants import DEC, JUN  # type:ignore[attr-defined]
+from holidays.countries.germany import Germany
+
+
+class BdewDefinedHolidays(HolidayBase):
+    """
+    Special holidays considered by the BDEW, excluding nationwide or state specific holidays.
+
+    To get a full calendar with all holidays use the function `create_bdew_calendar`.
+
+    Currently, in Germany there are no observed holidays e.g. a Holiday is
+    moved to the next Workday, if it's on Sunday.
+
+    """
+
+    def __init__(self, observed: bool = False, **kwargs: Any) -> None:
+        super().__init__(observed=observed, **kwargs)
+
+    def _populate(self, year: int) -> None:
+        self[date(year, DEC, 24)] = "Heiligabend"
+        self[date(year, DEC, 31)] = "Silvester"
+        if year == 2025:
+            self[date(2025, JUN, 6)] = (
+                "Sonderfeiertag"
+                # Anlässlich der (verschobenen) Einführung des 24h Lieferantenwechsels
+            )
+
+
+_relevant_subdivisions: list[str] = [
+    "BB",  # Brandenburg.
+    "BE",  # Berlin.
+    "BW",  # Baden-Württemberg.
+    "BY",  # Bayern.
+    "HB",  # Bremen.
+    "HE",  # Hessen.
+    "HH",  # Hamburg.
+    "MV",  # Mecklenburg-Vorpommern.
+    "NI",  # Niedersachsen.
+    "NW",  # Nordrhein-Westfalen.
+    "RP",  # Rheinland-Pfalz.
+    "SH",  # Schleswig-Holstein.
+    "SL",  # Saarland.
+    "SN",  # Sachsen.
+    "ST",  # Sachsen-Anhalt.
+    "TH",  # Thüringen.
+    # This list contains an explicit list of all BDEW calendar relevant Germany subdivisions.
+    # It does _not_ include the 'Augsburg' subdivision which has been added in holidays v0.78.
+    # https://github.com/vacanza/holidays/commit/feab68b4377369ad52eeee9e98fc8316ec0c6f86
+]
+
+
+def create_bdew_calendar() -> HolidaySum:
+    """Creates a calendar containing all days considered by the BDEW.
+
+    Currently, in Germany there are no observed holidays e.g. a Holiday is
+    moved to the next Workday, if it's on Sunday.
+
+    "Nach GPKE und GeLi Gas sind alle jene Tage Werktage, die weder Sonnabend,
+    Sonntag oder gesetzlicher Feiertag sind. Generell gelten der 24.12. und
+    der 31.12. als Feiertage. Angaben ohne Gewähr."
+
+    https://www.bdew.de/media/documents/GPKE-GeLiGas-Feiertagskalender-2022-Version-1-1_Uu1w6UQ.pdf
+
+    [Accessed 2022-11-02]``
+
+    Returns
+    -------
+    holidays.HolidaySum
+        dict-like holiday calendar, to check if a date/datetime is a holiday
+    """
+
+    # First we need the BDEW specific holidays.
+    calendar = BdewDefinedHolidays(language="de")
+
+    # the type is wrong at assignment but correct after the first loop iteration
+    result: HolidaySum = calendar  # type:ignore[assignment]
+    original_language_before_adding_subdivisions = result.language
+    # If a day is holiday in any subdivision, the holiday is valid nationwide.
+    # Therefore, we add all subdivisions of Germany to the BDEW specific holidays.
+    # Currently, in Germany holidays are not observed.
+    for subdivision in _relevant_subdivisions:
+        # the method __add__ expects a Union[int, "HolidayBase", "HolidaySum"] as `other`
+        # here, we're dealing with a child instance of HolidayBase
+        result += Germany(
+            subdiv=subdivision, observed=False, language="de"
+        )  # type:ignore[assignment]
+    if result.language is None:
+        # This is a workaround to a problem in holidays 0.20-0.53 (at least):
+        # When adding the subdivisions, the language attribute is lost,
+        # although holiday base and subdivision share the same language.
+        # The problem happens here:
+        # https://github.com/vacanza/python-holidays/blob/v0.53/holidays/holiday_base.py#L1164
+        result.language = original_language_before_adding_subdivisions
+    return result
+
+
+__all__ = ["BdewDefinedHolidays", "create_bdew_calendar"]
---
+++ packages/bdew-datetimes/bdew_datetimes-0.10.0-py3-none-any.whl/bdew_datetimes/periods.py
@@ -0,0 +1,143 @@
+"""
+periods is a module that helps to calculate statutory periods
+("gesetzliche Fristen") in the German energy market.
+It is based on the chapter "Fristenberechnung" in the GPKE.
+"""
+
+import datetime
+from datetime import date
+from typing import Optional
+
+from dateutil.relativedelta import relativedelta
+from holidays import SAT, SUN  # type:ignore[attr-defined]
+
+from bdew_datetimes.calendar import create_bdew_calendar
+from bdew_datetimes.enums import DayType, EndDateType, MonthType
+from bdew_datetimes.german_time_zone import GERMAN_TIME_ZONE
+from bdew_datetimes.models import Period
+
+# https://www.bundesnetzagentur.de/DE/Beschlusskammern/1_GZ/BK6-GZ/2020/BK6-20-160/Mitteilung_Nr_2/Leseversion_GPKE.pdf
+# pages 15 onwards
+
+_bdew_calendar = create_bdew_calendar()
+"""
+a static calendar object that is used (module) internally
+"""
+
+
+def is_bdew_working_day(candidate: date) -> bool:
+    """
+    Returns true if and only if the given candidate is a day relevant for the period calculation.
+    Returns false if the given candidate is either a BDEW holiday, a saturday or sunday.
+    """
+    if candidate in _bdew_calendar:
+        return False
+    return candidate.weekday() not in (SAT, SUN)
+
+
+def get_next_working_day(start_date: date) -> date:
+    """
+    If start_date is a working day, the next (+1) working day is returned.
+    If this day is a BDEW holiday or falls on a weekend, the next working day
+    is returned.
+    """
+    result = start_date + datetime.timedelta(
+        days=1
+    )  # in any case the calculation starts at least at the next day
+    while not is_bdew_working_day(result):
+        # if the next day is a holiday, then we add days until we find a working day
+        result += datetime.timedelta(days=1)
+    return result
+
+
+def get_previous_working_day(start_date: date) -> date:
+    """
+    If start_date is a working day, the previous (-1) working day is returned.
+    If this day is a BDEW holiday or falls on a weekend, the previous working day
+    is returned.
+    """
+    result = start_date - datetime.timedelta(
+        days=1
+    )  # in any case the calculation starts at least at the next day
+    while not is_bdew_working_day(result):
+        # if the previous day is a holiday, then we subtract days until we find a working day
+        result -= datetime.timedelta(days=1)
+    return result
+
+
+def add_frist(start: date, period: Period) -> date:
+    """
+    Returns the date that is period after start.
+    """
+    result: date = start
+    if period.number_of_days >= 0:
+        # the period calculation starts at the next working day, even if the number_of_days == 0
+        result = get_next_working_day(result)
+    # result is now the "Beginndatum" of the Fristenberechnung
+    if period.day_type == DayType.CALENDAR_DAY:
+        return result + datetime.timedelta(days=period.number_of_days)
+    # day_type is working day
+    if period.number_of_days == 0:
+        return result
+    if period.number_of_days > 0:
+        days_added = 0
+        while days_added < period.number_of_days:
+            result += datetime.timedelta(days=1)
+            if is_bdew_working_day(result):
+                # it's a usual working day; those are considered for the period calculation
+                days_added += 1
+    else:  # implicitly: elif period.number_of_days < 0:
+        days_subtracted = 0
+        while days_subtracted <= abs(period.number_of_days):
+            result -= datetime.timedelta(days=1)
+            if is_bdew_working_day(result):
+                days_subtracted += 1
+    return result
+
+
+def get_nth_working_day_of_month(
+    number_of_working_day_in_month: int,
+    month_type: MonthType = MonthType.LIEFERMONAT,
+    start: Optional[date] = None,
+) -> date:
+    """
+    Returns the nth working of the month, starting at start.
+    If start is None, then we use the local date/start of month in Germany.
+    Only year and month of the provided start date are taken into account.
+    The date.day is always discarded.
+
+    This function is useful if you're dealing with statutory periods from the
+    GPKE/MaBiS that are defined as "working days since start of month".
+    """
+    if start is None:
+        start = GERMAN_TIME_ZONE.localize(datetime.datetime.utcnow()).date()
+    if month_type == MonthType.LIEFERMONAT:
+        # returns the "nter Werktag des Liefermonats"
+        start = get_previous_working_day(start.replace(day=1))
+        period = Period(
+            number_of_days=number_of_working_day_in_month,
+            day_type=DayType.WORKING_DAY,
+            end_date_type=EndDateType.INCLUSIVE,
+        )
+        result = add_frist(start, period)
+    elif month_type == MonthType.FRISTENMONAT:
+        # returns the "nter Werktag des Fristenmonats"
+        start_of_next_month = start.replace(day=1) + relativedelta(months=1)
+        result = get_nth_working_day_of_month(
+            number_of_working_day_in_month,
+            month_type=MonthType.LIEFERMONAT,
+            start=start_of_next_month,
+        )
+    else:
+        raise ValueError(f"Unhandled month_type {month_type}")
+    return result
+
+
+# pylint:disable=duplicate-code
+__all__ = [
+    "is_bdew_working_day",
+    "get_next_working_day",
+    "get_previous_working_day",
+    "add_frist",
+    "get_nth_working_day_of_month",
+]
