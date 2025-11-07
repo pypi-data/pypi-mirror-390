1b46eb50d927e545fb5d2d00062dea2fa8c75185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:25:08 +0000

    Add package Team-19-Custom-Utils-Lib/0.1.1/team_19_custom_utils_lib-0.1.1-py3-none-any.whl

---
+++ packages/Team-19-Custom-Utils-Lib/team_19_custom_utils_lib-0.1.1-py3-none-any.whl/Team19CustomUtils/generalUtils.py
@@ -0,0 +1,69 @@
+def get_ratio(num1, num2):
+    """
+    calculates the ratio between two numbers
+    Args:
+        num1: first number
+        num2: second number
+    Returns: 
+        _: the ratio between the greater and lesser number (lesser/greater)
+        state: a variable indicating which number is larger
+
+    Dependencies: 
+        None
+    """
+
+    if(num1 > num2):
+        state = 1
+        return ((num2/num1), state)
+
+    elif(num2 > num1):
+        state = 2
+        return ( (num1/num2), state)
+
+    else:
+        return 0
+
+def a_sum(input):
+    """
+    calculates the sum of an array
+    Args:
+        input: a one-dimensional array
+    Returns: 
+        sum: the sum of all values in the array
+
+    Dependencies: 
+        None
+    """
+    sum = 0
+    for i in range (0, len(input)):
+            sum += input[i]
+
+    return sum
+
+def a_mean(input):
+    """
+    calculates the mean of an array
+    Args:
+        input: a one-dimensional array
+    Returns: 
+        the mean of the one-dimensional array
+
+    Dependencies: 
+        None
+    """
+    return ( a_sum(input) / len(input) )
+
+def transpose_matrix(input: list):
+    """
+    transposes an input matrix
+    args:
+        input (2d arr): input matrix which will be transposed
+    returns:
+        m_transposed (2d arr): transposition of input matrix
+    """
+    m_transposed = input.copy()
+    for i in range(0, len(input)):
+        for j in range(0, len(input[0])):
+            m_transposed[i][j] = input[j][i]
+    
+    return m_transposed
