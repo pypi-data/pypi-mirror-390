931c39c1167b781ec2bf8456b0c5e5a07f3a8d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:19 +0000

    Add package m209/1.0.0/m209-1.0.0-py3-none-any.whl

---
+++ packages/m209/m209-1.0.0-py3-none-any.whl/m209/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2013 - 2025 by Brian Neal.
+# This file is part of m209, the M-209 simulation.
+# m209 is released under the MIT License (see LICENSE.txt).
+
+__version__ = '1.0.0'
+
+class M209Error(Exception):
+    """Base Exception class for all M209 errors"""
+    pass
