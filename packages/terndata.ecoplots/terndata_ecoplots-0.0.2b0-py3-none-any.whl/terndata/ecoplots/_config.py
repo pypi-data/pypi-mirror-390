1e4a7def73bb971a17e4386e1f6f3fe137d0ecde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 06:04:07 +0000

    Add package terndata.ecoplots/0.0.2b0/terndata_ecoplots-0.0.2b0-py3-none-any.whl

---
+++ packages/terndata.ecoplots/terndata_ecoplots-0.0.2b0-py3-none-any.whl/terndata/ecoplots/_config.py
@@ -0,0 +1,60 @@
+import os
+import tempfile
+
+# Default configuration
+
+VERSION = "0.0.2"
+
+API_BASE_URL = (
+    os.getenv("ECOPLOTS_URL", "http://ecoplots-test.tern.org.au")
+    if (os.getenv("ECOPLOTS_ENV") == "dev")
+    else "https://ecoplots.tern.org.au"
+)
+
+CACHE_EXPIRE_SECONDS = 60 * 60 * 24 * 14  # Default cache expiration time: 14 days
+CACHE_DIR = os.path.join(tempfile.gettempdir(), "ecoplots_labels_cache")
+
+MAGIC = b"ECPJ"
+
+VOCAB_FACETS = [
+    "region_type",
+    "region",
+    "dataset",
+    "site_id",
+    "site_visit_id",
+    "feature_type",
+    "observed_property",
+    "attributes",
+    # "units",
+    "used_procedure",
+    # "system_type",
+    "core_attributes",
+]
+
+QUERY_FACETS = [
+    "region_type",
+    "region",
+    "dataset",
+    "site_id",
+    "site_visit_id",
+    "feature_type",
+    "observed_property",
+    "spatial",
+    # "description", #TODO: AI based description to facet mapping
+]
+
+DISCOVERY_FACETS = [
+    "region",
+    "dataset",
+    "feature_type",
+    "observed_property",
+    "site_id",
+]
+
+DISCOVERY_ATTRIBUTES = [
+    "dataset",
+    "feature_type",
+    "observation",
+    "site",
+    "site_visit",
+]
