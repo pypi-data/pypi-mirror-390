95d6808046ec09b9dad8592588bf192f3de0fdde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:06:18 +0000

    Add package mchnpkg/0.4.3/mchnpkg-0.4.3.tar.gz

---
+++ packages/mchnpkg/mchnpkg-0.4.3.tar.gz/mchnpkg-0.4.3/target/release/deps/unicode_ident-cbedee256c6400fc.d
@@ -0,0 +1,8 @@
+/home/ayr0001/mchnpkg/target/release/deps/unicode_ident-cbedee256c6400fc.d: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libunicode_ident-cbedee256c6400fc.rlib: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs
+
+/home/ayr0001/mchnpkg/target/release/deps/libunicode_ident-cbedee256c6400fc.rmeta: /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs /home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs
+
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/lib.rs:
+/home/ayr0001/.cache/puccinialin/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.19/src/tables.rs:
