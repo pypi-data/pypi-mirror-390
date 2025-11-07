01f06cca961d22f176d51d7a6841ce2225e94fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:21 +0000

    Add package edgelab/0.1.4/edgelab-0.1.4.tar.gz

---
+++ packages/edgelab/edgelab-0.1.4.tar.gz/edgelab-0.1.4/edgelab/utils/__init__.py
@@ -0,0 +1,5 @@
+"""Utility functions and helpers."""
+
+from edgelab.utils.console import console, print_success, print_error, print_warning, print_info
+
+__all__ = ["console", "print_success", "print_error", "print_warning", "print_info"]
