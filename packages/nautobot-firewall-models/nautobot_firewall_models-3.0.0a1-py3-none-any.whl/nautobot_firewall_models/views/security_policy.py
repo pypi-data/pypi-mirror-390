adc6799046c9b6b249b1453f97574a6d076ae294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:22:26 +0000

    Add package nautobot-firewall-models/3.0.0a1/nautobot_firewall_models-3.0.0a1-py3-none-any.whl

---
+++ packages/nautobot-firewall-models/nautobot_firewall_models-3.0.0a1-py3-none-any.whl/nautobot_firewall_models/views/security_policy.py
@@ -0,0 +1,92 @@
+"""Rule Object Viewsets."""
+
+from django.shortcuts import redirect
+from django.urls import reverse
+from nautobot.apps.views import NautobotUIViewSet
+from nautobot.core.views.mixins import PERMISSIONS_ACTION_MAP
+from rest_framework.decorators import action
+
+from nautobot_firewall_models import details, filters, forms, models, tables
+from nautobot_firewall_models.api import serializers
+
+
+class PolicyRuleUIViewSet(NautobotUIViewSet):
+    """ViewSet for the PolicyRule model."""
+
+    bulk_update_form_class = forms.PolicyRuleBulkEditForm
+    filterset_class = filters.PolicyRuleFilterSet
+    filterset_form_class = forms.PolicyRuleFilterForm
+    form_class = forms.PolicyRuleForm
+    queryset = models.PolicyRule.objects.all()
+    serializer_class = serializers.PolicyRuleSerializer
+    table_class = tables.PolicyRuleTable
+    object_detail_content = details.policy_rule
+
+    lookup_field = "pk"
+
+
+class PolicyUIViewSet(NautobotUIViewSet):
+    """ViewSet for the Policy model."""
+
+    bulk_update_form_class = forms.PolicyBulkEditForm
+    filterset_class = filters.PolicyFilterSet
+    filterset_form_class = forms.PolicyFilterForm
+    form_class = forms.PolicyForm
+    queryset = models.Policy.objects.all()
+    serializer_class = serializers.PolicySerializer
+    table_class = tables.PolicyTable
+    object_detail_content = details.policy
+    prefetch_related = [
+        "policy_rules__source_users",
+        "policy_rules__source_user_groups",
+        "policy_rules__source_zone",
+        "policy_rules__destination_zone",
+        "policy_rules__original_source_addresses",
+        "policy_rules__original_source_address_groups",
+        "policy_rules__original_source_services",
+        "policy_rules__original_source_service_groups",
+        "policy_rules__translated_source_addresses",
+        "policy_rules__translated_source_address_groups",
+        "policy_rules__translated_source_services",
+        "policy_rules__translated_source_service_groups",
+        "policy_rules__original_destination_addresses",
+        "policy_rules__original_destination_address_groups",
+        "policy_rules__original_destination_services",
+        "policy_rules__original_destination_service_groups",
+        "policy_rules__translated_destination_addresses",
+        "policy_rules__translated_destination_address_groups",
+        "policy_rules__translated_destination_services",
+        "policy_rules__translated_destination_service_groups",
+    ]
+
+    lookup_field = "pk"
+
+    def get_queryset(self):
+        """Overload to overwrite permissiosn action map."""
+        queryset = super().get_queryset()
+        _perms = {**PERMISSIONS_ACTION_MAP, "devices": "change", "dynamic_groups": "change"}
+        return queryset.restrict(self.request.user, _perms[self.action])
+
+    @action(detail=True, methods=["post"])
+    def devices(self, request, pk, *args, **kwargs):
+        # pylint: disable=invalid-name, arguments-differ
+        """Method to set weight on a Device & Policy Relationship."""
+        form_data = dict(request.POST)
+        form_data.pop("csrfmiddlewaretoken", None)
+        for device, weight in form_data.items():
+            m2m = models.PolicyDeviceM2M.objects.get(device=device, policy=pk)
+            m2m.weight = weight[0]
+            m2m.validated_save()
+        return redirect(reverse("plugins:nautobot_firewall_models:policy", kwargs={"pk": pk}))
+
+    @action(detail=True, methods=["post"])
+    def dynamic_groups(self, request, pk, *args, **kwargs):
+        # pylint: disable=invalid-name, arguments-differ
+        """Method to set weight on a DynamicGroup & Policy Relationship."""
+        form_data = dict(request.POST)
+        form_data.pop("csrfmiddlewaretoken", None)
+        for group, weight in form_data.items():
+            m2m = models.PolicyDynamicGroupM2M.objects.get(dynamic_group=group, policy=pk)
+            m2m.weight = weight[0]
+            m2m.validated_save()
+        return redirect(reverse("plugins:nautobot_firewall_models:policy", kwargs={"pk": pk}))
