7dcbcc723add689ef2ec1733d1ff04fd1b36b39a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:01 +0000

    Add package synaptogram/0.0.10/synaptogram-0.0.10-py3-none-any.whl

---
+++ packages/synaptogram/synaptogram-0.0.10-py3-none-any.whl/synaptogram/config.py
@@ -0,0 +1,15 @@
+CHANNEL_CONFIG = {
+    'CtBP2': { 'display_color': '#FF0000'},
+    'MyosinVIIa': {'display_color': '#0000FF'},
+    'GluR2': {'display_color': '#00FF00'},
+    'GlueR2': {'display_color': '#00FF00'},
+    'PMT': {'display_color': '#FFFFFF'},
+    'DAPI': {'display_color': '#FFFFFF', 'visible': False},
+
+    # Channels are tagged as unknown if there's difficulty parsing the channel
+    # information from the file.
+    'Unknown 1': {'display_color': '#FF0000'},
+    'Unknown 2': {'display_color': '#00FF00'},
+    'Unknown 3': {'display_color': '#0000FF'},
+    'Unknown 4': {'display_color': '#FFFFFF'},
+}
