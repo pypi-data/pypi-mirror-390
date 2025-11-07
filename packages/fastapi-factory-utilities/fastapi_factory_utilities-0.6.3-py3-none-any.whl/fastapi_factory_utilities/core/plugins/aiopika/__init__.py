523e3fe47df892fb3e31720e44ca6665f29c8676
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:11 +0000

    Add package fastapi-factory-utilities/0.6.3/fastapi_factory_utilities-0.6.3-py3-none-any.whl

---
+++ packages/fastapi-factory-utilities/fastapi_factory_utilities-0.6.3-py3-none-any.whl/fastapi_factory_utilities/core/plugins/aiopika/__init__.py
@@ -0,0 +1,25 @@
+"""Aiopika Plugin Module."""
+
+from .configs import AiopikaConfig
+from .depends import depends_aiopike_robust_connection
+from .exceptions import AiopikaPluginBaseError, AiopikaPluginConfigError
+from .exchange import Exchange
+from .listener import AbstractListener
+from .message import AbstractMessage, SenderModel
+from .plugins import AiopikaPlugin
+from .publisher import AbstractPublisher
+from .queue import Queue
+
+__all__: list[str] = [
+    "AbstractListener",
+    "AbstractMessage",
+    "AbstractPublisher",
+    "AiopikaConfig",
+    "AiopikaPlugin",
+    "AiopikaPluginBaseError",
+    "AiopikaPluginConfigError",
+    "Exchange",
+    "Queue",
+    "SenderModel",
+    "depends_aiopike_robust_connection",
+]
