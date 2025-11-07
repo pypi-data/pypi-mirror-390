03b00445e213de08ae83876469067bedda46fb99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:05:23 +0000

    Add package centrip-book/1.0.4/centrip_book-1.0.4.tar.gz

---
+++ packages/centrip-book/centrip_book-1.0.4.tar.gz/centrip_book-1.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="centrip-book",
+    version="1.0.4",
+    py_modules=["centrip_book"],
+    entry_points={"console_scripts": ["book=centrip_book:main"]},
+    author="Michael Sowerwine",
+    author_email="you@example.com",
+    description="A simple cross-system command bookmarking tool for your terminal.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/centrip-book",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+    ],
+    python_requires=">=3.6",
+)
