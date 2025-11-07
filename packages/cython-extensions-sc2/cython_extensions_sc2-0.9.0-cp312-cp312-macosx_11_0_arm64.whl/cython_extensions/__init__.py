5a31c4c7f0abf43a387789f72ad55e6282f79c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 17:04:39 +0000

    Add package cython-extensions-sc2/0.9.0/cython_extensions_sc2-0.9.0-cp312-cp312-macosx_11_0_arm64.whl

---
+++ packages/cython-extensions-sc2/cython_extensions_sc2-0.9.0-cp312-cp312-macosx_11_0_arm64.whl/cython_extensions/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.9.0"
+
+# bootstrap is the only module which
+# can be loaded with default Python-machinery
+# because the resulting extension is called `bootstrap`:
+from . import bootstrap
+
+# injecting our finders into sys.meta_path
+# after that all other submodules can be loaded
+bootstrap.bootstrap_cython_submodules()
+
+# Import configuration functions from the safe module
+from cython_extensions.type_checking import (
+    enable_safe_mode,
+    disable_safe_mode,
+    is_safe_mode_enabled,
+    get_safe_mode_status,
+    safe_mode_context,
+)
+
+# Import all functions from the safe wrappers module
+# These handle the conditional validation internally
+from cython_extensions.type_checking.wrappers import *
