8e2e5d6e76aa302a3ef6719e6bb670ce7093394d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:06:04 +0000

    Add package amd-hipcim/0.0.2/amd_hipcim-0.0.2-py3-none-any.whl

---
+++ packages/amd-hipcim/amd_hipcim-0.0.2-py3-none-any.whl/amd_hipcim/__init__.py
@@ -0,0 +1,6 @@
+"""amd-hipcim
+
+hipCIM - an extensible toolkit designed to provide GPU accelerated I/O, computer vision & image processing primitives for N-Dimensional images with a focus on biomedical imaging.
+"""
+
+__version__ = "0.0.1"
