5995201d6ba5c2b54c7f9aa7e239b8156dc14561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 15:04:36 +0000

    Add package deep-dive-logger/0.1.3/deep_dive_logger-0.1.3.tar.gz

---
+++ packages/deep-dive-logger/deep_dive_logger-0.1.3.tar.gz/deep_dive_logger-0.1.3/deep_dive_logger/__init__.py
@@ -0,0 +1,7 @@
+"""dive-logger: A colorful and feature-rich logging utility for Python."""
+
+__version__ = "0.1.2" 
+
+from .logger import Logger, ColorFormatter
+
+__all__ = ["Logger", "ColorFormatter"]
