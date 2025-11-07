7183373ab545894723011c80616379c2ea73dfc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:21 +0000

    Add package pypitzer/1.1.1/pypitzer-1.1.1.tar.gz

---
+++ packages/pypitzer/pypitzer-1.1.1.tar.gz/pypitzer-1.1.1/tests/test.py
@@ -0,0 +1,28 @@
+from pypitzer.Pitzer.models import FluidPitzer
+
+
+species = {
+    'Na+': 1, # always be 1 if Na is the internal standard
+    'K+': 2 ,  # K/Na = 2
+    # 'Mg+2':0.1
+}
+
+# create a fluid object with information from microthemometric and LA-ICP-MS data
+fluid = FluidPitzer(
+    # the initial guess
+    x0=(1,1),
+  
+    # species defined before
+    species=species,
+  
+    # the last melting solid phase
+    equilibrium='KCl(s) = K+(aq) + Cl-(aq)',
+  
+    # melting temperature of the last-melting solid phase, [°C]
+    t = 25,
+)
+
+result = fluid.solve()
+
+print(result)
+
