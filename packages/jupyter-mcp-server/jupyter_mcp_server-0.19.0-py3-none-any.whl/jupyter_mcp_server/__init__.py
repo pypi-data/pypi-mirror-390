c00c6dbc2927a27de0b2c7a9e6f636da82bb8b82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 16:05:56 +0000

    Add package jupyter-mcp-server/0.19.0/jupyter_mcp_server-0.19.0-py3-none-any.whl

---
+++ packages/jupyter-mcp-server/jupyter_mcp_server-0.19.0-py3-none-any.whl/jupyter_mcp_server/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+#
+# BSD 3-Clause License
+
+"""Jupyter MCP Server."""
+
+from jupyter_mcp_server.jupyter_extension.extension import _jupyter_server_extension_points
+
+
+__all__ = ["_jupyter_server_extension_points"]
