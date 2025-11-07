55a1ac5a50dcebfc33f208745aeef680901a90cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:05:37 +0000

    Add package faber/0.5.10/faber-0.5.10.tar.gz

---
+++ packages/faber/faber-0.5.10.tar.gz/faber-0.5.10/src/faber/__init__.py
@@ -0,0 +1,12 @@
+#
+# Copyright (c) 2016 Stefan Seefeld
+# All rights reserved.
+#
+# This file is part of Faber. It is made available under the
+# Boost Software License, Version 1.0.
+# (Consult LICENSE or http://www.boost.org/LICENSE_1_0.txt)
+
+from . import _version
+__version__ = _version.get_versions()['version']
+
+debug = False
