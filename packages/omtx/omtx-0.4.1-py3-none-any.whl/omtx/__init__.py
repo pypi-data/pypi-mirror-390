bac21741042811861efeaed82d0986787ff95ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:05 +0000

    Add package omtx/0.4.1/omtx-0.4.1-py3-none-any.whl

---
+++ packages/omtx/omtx-0.4.1-py3-none-any.whl/omtx/__init__.py
@@ -0,0 +1,19 @@
+"""OMTX Python SDK - Client for OM Gateway V2.
+
+Includes:
+- OMTXClient: Explicit client with automatic idempotency and typed methods
+"""
+
+from .client import OMTXClient, JobTimeoutError
+from .exceptions import OMTXError, InsufficientCreditsError, AuthenticationError
+
+# edit the version below here!
+__version__ = "0.4.1"
+
+__all__ = [
+    "OMTXClient",
+    "OMTXError",
+    "InsufficientCreditsError",
+    "AuthenticationError",
+    "JobTimeoutError",
+]
