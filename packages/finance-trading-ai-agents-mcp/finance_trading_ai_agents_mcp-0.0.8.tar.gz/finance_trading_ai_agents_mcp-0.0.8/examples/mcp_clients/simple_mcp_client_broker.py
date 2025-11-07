1aaed2c56ef0ac152ddf9f6952986e475da22bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 15:04:40 +0000

    Add package finance-trading-ai-agents-mcp/0.0.8/finance_trading_ai_agents_mcp-0.0.8.tar.gz

---
+++ packages/finance-trading-ai-agents-mcp/finance_trading_ai_agents_mcp-0.0.8.tar.gz/finance_trading_ai_agents_mcp-0.0.8/examples/mcp_clients/simple_mcp_client_broker.py
@@ -0,0 +1,34 @@
+import asyncio
+import json
+
+from aitrados_api.common_lib.contant import IntervalName
+from fastmcp import Client
+
+from finance_trading_ai_agents_mcp import analysis_department
+
+from finance_trading_ai_agents_mcp.assistive_tools.assistive_tools_utils import get_client_mcp_config
+from finance_trading_ai_agents_mcp.assistive_tools.mcp_tools_converter import LlmCallToolConverter
+
+departments=[
+ analysis_department.BROKER
+]
+mcp_config=get_client_mcp_config(departments,mcp_base_url="http://127.0.0.1:11999")
+print("Departments",list(mcp_config["mcpServers"].keys()))
+print("mcp",json.dumps(mcp_config,indent=2,ensure_ascii=False))
+
+async def main():
+    async with Client(mcp_config) as client:
+        tool_data=[]
+        for tool in await client.list_tools():
+            tool_data.append(tool.model_dump())
+
+        print(json.dumps(tool_data))
+
+        # Execute operations
+
+        print(await LlmCallToolConverter(client).call_tool("get_all_accounts" ))
+        print(await LlmCallToolConverter(client).call_tool("get_all_active_orders"))
+        print(await LlmCallToolConverter(client).call_tool("get_all_positions"))
+
+
+asyncio.run(main())
