232c011f4a577e6deadc0b496f639306bd6909d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:23:07 +0000

    Add package bus-202/1.18.5/bus_202-1.18.5-py3-none-any.whl

---
+++ packages/bus-202/bus_202-1.18.5-py3-none-any.whl/bus_202/__init__.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from pathlib import Path
+from .data_visualization.trim import trim
+from .data_visualization.boxplot import boxplot
+from .data_visualization.histogram import histogram
+from .data_visualization.scatter import scatter
+from .data import (
+    sp1500_cross_sectional, sp1500_panel, 
+    ceo_comp, a1, netflix_content, 
+    olympic_medals, world_cup_goals,
+    midterm, sweet_things, sweet_things_simple)
+from .stats.ci import ci
+from .stats.moe import moe
+from .stats.reg import reg
+
+__all__ = [
+    'trim', 'boxplot', 'histogram', 'ci', 'moe', 
+    'sp1500_cross_sectional', 'sp1500_panel', 
+    'ceo_comp', 'a1', 'netflix_content', 
+    'olympic_medals', 'world_cup_goals', 
+    'midterm', 'sweet_things_simple', 
+    'sweet_things']
