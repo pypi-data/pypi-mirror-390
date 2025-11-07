222332a0547d151b90494008e6c2baafba224f41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 05:04:41 +0000

    Add package mp-api/0.45.13/mp_api-0.45.13.tar.gz

---
+++ packages/mp-api/mp_api-0.45.13.tar.gz/mp_api-0.45.13/mp_api/client/routes/materials/similarity.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+from emmet.core.similarity import SimilarityDoc
+
+from mp_api.client.core import BaseRester
+from mp_api.client.core.utils import validate_ids
+
+
+class SimilarityRester(BaseRester):
+    suffix = "materials/similarity"
+    document_model = SimilarityDoc  # type: ignore
+    primary_key = "material_id"
+
+    def search(
+        self,
+        material_ids: str | list[str] | None = None,
+        num_chunks: int | None = None,
+        chunk_size: int = 1000,
+        all_fields: bool = True,
+        fields: list[str] | None = None,
+    ) -> list[SimilarityDoc] | list[dict]:
+        """Query similarity docs using a variety of search criteria.
+
+        Arguments:
+            material_ids (str, List[str]): A single Material ID string or list of strings
+                (e.g., mp-149, [mp-149, mp-13]).
+            num_chunks (int): Maximum number of chunks of data to yield. None will yield all possible.
+            chunk_size (int): Number of data entries per chunk.
+            all_fields (bool): Whether to return all fields in the document. Defaults to True.
+            fields (List[str]): List of fields in SimilarityDoc to return data for.
+                Default is material_id, last_updated, and formula_pretty if all_fields is False.
+
+        Returns:
+            ([SimilarityDoc], [dict]) List of similarity documents or dictionaries.
+        """
+        query_params = {}  # type: dict
+
+        if material_ids:
+            if isinstance(material_ids, str):
+                material_ids = [material_ids]
+
+            query_params.update({"material_ids": ",".join(validate_ids(material_ids))})
+
+        query_params = {
+            entry: query_params[entry]
+            for entry in query_params
+            if query_params[entry] is not None
+        }
+        return super()._search(
+            num_chunks=num_chunks,
+            chunk_size=chunk_size,
+            all_fields=all_fields,
+            fields=fields,
+            **query_params,
+        )
