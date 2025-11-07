d592e2890e0d6e19e9096dd896f0c0a0ffe34a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:52 +0000

    Add package centrip-book/1.1.5/centrip_book-1.1.5.tar.gz

---
+++ packages/centrip-book/centrip_book-1.1.5.tar.gz/centrip_book-1.1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="centrip-book",
+    version='1.1.5',
+    author="Centrip Studios",
+    description="A command bookmarking tool for your terminal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    py_modules=["centrip_book"],
+    entry_points={"console_scripts": ["book=centrip_book:main"]},
+    python_requires=">=3.6",
+)
