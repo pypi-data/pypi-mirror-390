d2e2ec9ca70ed4d542b5b99f1e98de109618d348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 03:23:05 +0000

    Add package pygeofilter-aeronet/0.3.0/pygeofilter_aeronet-0.3.0-py3-none-any.whl

---
+++ packages/pygeofilter-aeronet/pygeofilter_aeronet-0.3.0-py3-none-any.whl/pygeofilter_aeronet/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2025-present Fabrice Brito <fabrice.brito@terradue.com>
+#
+# SPDX-License-Identifier: MIT
+
+from pygeofilter_aeronet.evaluator import to_aeronet_api, http_invoke
+
+__all__ = ["to_aeronet_api", "http_invoke"]
