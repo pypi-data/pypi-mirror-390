02ad733bd429ef5e53a70f0e905f6046042f6bf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 04:05:15 +0000

    Add package mapminer/0.1.71/mapminer-0.1.71.tar.gz

---
+++ packages/mapminer/mapminer-0.1.71.tar.gz/mapminer-0.1.71/mapminer/models/__init__.py
@@ -0,0 +1,17 @@
+import torch
+from .nafnet import NAFNet
+from .convlstm import ConvLSTM
+from .dinov3 import DiNOV3
+from .dinochm import DINOCHM
+
+# Aliases for convenience
+DINOv3 = DiNOV3
+DINOV3 = DiNOV3
+DiNOv3 = DiNOV3
+
+
+try : 
+    from IPython.display import clear_output
+    clear_output()
+except : 
+    pass
