5223b672eb094842bff2262d57097f641687ee53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:41 +0000

    Add package sc-napalm/0.1.1/sc_napalm-0.1.1-py3-none-any.whl

---
+++ packages/sc-napalm/sc_napalm-0.1.1-py3-none-any.whl/custom_napalm/__init__.py
@@ -0,0 +1,28 @@
+from .iosxr import SCIOSXR
+from .junos import SCJunOS
+from .nxos import SCNXOS
+from .sros import SCNokiaSROSDriver
+from .srl import SCNokiaSRLDriver
+from .eos import SCEOSDriver
+from .nos import SCNOSDriver
+
+PLATFORM_MAP = {
+    "iosxr": SCIOSXR,
+    "nxos": SCNXOS,
+    "junos": SCJunOS,
+    "sros": SCNokiaSROSDriver,
+    "srl": SCNokiaSRLDriver,
+    "eos": SCEOSDriver,
+    "nos": SCNOSDriver,
+}
+
+
+def get_network_driver(platform: str):
+    """
+    Returns network driver based on platform string.
+    """
+    for valid_platform, driver in PLATFORM_MAP.items():
+        if valid_platform == platform:
+            return driver
+
+    raise NotImplementedError(f"Unsupported platform {platform}")
