d4811715e2fe36a76c4db68c5693b238666bd34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-07 14:04:50 +0000

    Add package HeroLIBmagnagomatheus/0.1.1/herolibmagnagomatheus-0.1.1.tar.gz

---
+++ packages/HeroLIBmagnagomatheus/herolibmagnagomatheus-0.1.1.tar.gz/herolibmagnagomatheus-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="HeroLIBmagnagomatheus",  # ⚡ Nome com hífen para o PyPI
+    version="0.1.1",
+    description="Biblioteca com model de Heros and Teams + generics and database in memory.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Matheus de Oliveira Magnago",
+    author_email="magnagomatheus7@gmail.com",
+    url="https://github.com/magnagomatheus/HeroLib.git",   
+    packages=[
+        'lib_hero',
+        'lib_hero.model',
+        'lib_hero.repository',
+        'lib_hero.service',
+        'lib_hero.util'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
