174a765e1818188c0ace45ad28075c290135e0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:35 +0000

    Add package conan-server/2.22.2/conan_server-2.22.2.tar.gz

---
+++ packages/conan-server/conan_server-2.22.2.tar.gz/conan_server-2.22.2/conan/__init__.py
@@ -0,0 +1,6 @@
+from conan.internal.model.conan_file import ConanFile
+from conan.internal.model.workspace import Workspace
+from conan.internal.model.version import Version
+
+__version__ = '2.22.2'
+conan_version = Version(__version__)
