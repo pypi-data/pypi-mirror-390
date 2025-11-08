8e4094941b4dd8ff75b40e8314e4d4e486b2346b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 15:05:24 +0000

    Add package nemo-library-etl/0.1.39/nemo_library_etl-0.1.39-py3-none-any.whl

---
+++ packages/nemo-library-etl/nemo_library_etl-0.1.39-py3-none-any.whl/nemo_library_etl/adapter/migman/enums.py
@@ -0,0 +1,20 @@
+from enum import Enum
+
+
+class MigManExtractAdapter(Enum):
+    GENERICODBC = "genericodbc"
+    INFORCOM = "inforcom"
+    SAPECC = "sapecc"
+    PROALPHA = "proalpha"
+    SAGEKHK = "sagekhk"
+
+class MigManLoadAdapter(Enum):
+    PROALPHA_MIGMAN = "proalpha_migman"
+    NEMO_BUSINESS_PROCESSES = "nemo_business_process"
+
+class MigManTransformStep(Enum):
+    JOINS = "10_joins"
+    MAPPINGS = "20_mappings"
+    DUPLICATES = "30_duplicates"
+    NEWNUMBERS = "40_newnumbers"
+    NONEMPTY = "50_nonempty"
