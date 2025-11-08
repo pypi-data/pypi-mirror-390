924aba15ec0bc6dc11eea8cc3baafe8abdaa93d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:44 +0000

    Add package viur-core/3.8.9/viur_core-3.8.9-py3-none-any.whl

---
+++ packages/viur-core/viur_core-3.8.9-py3-none-any.whl/viur/core/bones/raw.py
@@ -0,0 +1,35 @@
+import re
+from viur.core.bones.base import BaseBone, ReadFromClientError, ReadFromClientErrorSeverity
+
+SEARCH_TAGS = re.compile(r"[^\s]+")
+
+
+class RawBone(BaseBone):
+    """
+    Stores its data without applying any pre/post-processing or filtering. Can be used to store
+    non-html content.
+    Use the dot-notation like "raw.markdown" or similar to describe subsequent types.
+
+    ..Warning: Using this bone will lead to security vulnerabilities like reflected XSS unless the
+        data is either otherwise validated/stripped or from a trusted source! Don't use this unless
+        you fully understand it's implications!
+    """
+    type = "raw"
+
+    def singleValueFromClient(self, value, skel, bone_name, client_data):
+        err = self.isInvalid(value)
+        if err:
+            return self.getEmptyValue(), [ReadFromClientError(ReadFromClientErrorSeverity.Invalid, err)]
+        return value, None
+
+    def getSearchTags(self, skel: "SkeletonInstance", name: str) -> set[str]:
+        result = set()
+
+        for idx, lang, value in self.iter_bone_value(skel, name):
+            if not value:
+                continue
+
+            for tag in re.finditer(SEARCH_TAGS, str(value)):
+                result.add(tag.group())
+
+        return result
