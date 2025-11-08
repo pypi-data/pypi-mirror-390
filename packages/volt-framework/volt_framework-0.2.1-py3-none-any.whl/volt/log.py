cbd0ec0fcd1dd1658ca3cb08ddc3381a58a73e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 14:06:05 +0000

    Add package volt-framework/0.2.1/volt_framework-0.2.1-py3-none-any.whl

---
+++ packages/volt-framework/volt_framework-0.2.1-py3-none-any.whl/volt/log.py
@@ -0,0 +1,33 @@
+import logging
+from typing import override
+
+
+class ColorFormatter(logging.Formatter):
+    COLORS: dict[str, str] = {
+        "DEBUG": "\033[36m",  # Cyan
+        "INFO": "\033[32m",  # Green
+        "WARNING": "\033[33m",  # Yellow
+        "ERROR": "\033[31m",  # Red
+        "CRITICAL": "\033[35m",  # Magenta
+    }
+    RESET: str = "\033[0m"
+
+    @override
+    def format(self, record: logging.LogRecord):
+        log_color = self.COLORS.get(record.levelname, "")
+        record.levelname = f"{log_color}{record.levelname}{self.RESET}"
+        return super().format(record)
+
+
+# Setup logging configuration
+formatter = ColorFormatter(
+    "%(asctime)s [%(levelname)s] %(name)s - %(message)s", datefmt="%H:%M:%S"
+)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+volt_parent = logging.getLogger("volt")
+volt_parent.addHandler(handler)
+
+volt_parent.setLevel(logging.INFO)
+
