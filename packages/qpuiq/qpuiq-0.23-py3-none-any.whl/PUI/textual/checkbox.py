f986e8441ecc44773e531277c656520063772437
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 08:06:01 +0000

    Add package qpuiq/0.23/qpuiq-0.23-py3-none-any.whl

---
+++ packages/qpuiq/qpuiq-0.23-py3-none-any.whl/PUI/textual/checkbox.py
@@ -0,0 +1,21 @@
+from .. import *
+from .base import *
+
+class Checkbox(TBase):
+    def __init__(self, text, model):
+        super().__init__()
+        self.text = text
+        self.model = model
+
+    def update(self, prev):
+        if prev and prev.ui:
+            self.ui = prev.ui
+            self.ui.value = self.model.value
+        else:
+            self.ui = widgets.Checkbox(self.text, self.model.value)
+
+        self.ui.puinode = self
+        super().update(prev)
+
+    def _changed(self, value):
+        self.model.value = value
