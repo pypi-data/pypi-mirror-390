62e9959a0367fa9bac061b1af8fb27e34840bf4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 06:05:26 +0000

    Add package curvpyutils/0.0.22/curvpyutils-0.0.22.tar.gz

---
+++ packages/curvpyutils/curvpyutils-0.0.22.tar.gz/curvpyutils-0.0.22/src/curvpyutils/__init__.py
@@ -0,0 +1,15 @@
+"""Curv Python shared utilities package."""
+
+from . import version
+
+__all__ = [
+    "test_helpers",
+    "colors",
+    "file_utils",
+    "multi_progress",
+    "shellutils",
+    "system",
+    "str_utils",
+    "toml_utils",
+    "version",
+]
