a3b4169ea421b7761679c02609a2911d3569ce95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:27 +0000

    Add package zlmdb/25.10.2/zlmdb-25.10.2-cp311-cp311-win_amd64.whl

---
+++ packages/zlmdb/zlmdb-25.10.2-cp311-cp311-win_amd64.whl/zlmdb/flatbuffers/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) FlatBuffers Contributors, Apache License 2.0
+
+# Copied from (master branch, 05/29/2022):
+# * https://github.com/google/flatbuffers/blob/master/reflection/reflection.fbs
+# * https://github.com/google/flatbuffers/tree/master/python/flatbuffers/reflection
