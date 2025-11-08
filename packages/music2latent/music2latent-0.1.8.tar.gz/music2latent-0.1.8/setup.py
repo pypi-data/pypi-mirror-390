f96d5c8b0a0b9c1d4b96268ef229fe4cfbfa67d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 06:05:16 +0000

    Add package music2latent/0.1.8/music2latent-0.1.8.tar.gz

---
+++ packages/music2latent/music2latent-0.1.8.tar.gz/music2latent-0.1.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='music2latent',
+    version='0.1.8',
+    packages=find_packages(), 
+    description='Encode and decode audio samples to/from compressed representations!',
+    author='Sony Computer Science Laboratories Paris',
+    author_email='music@csl.sony.fr', 
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown', 
+    install_requires=[
+        'numpy',
+        'soundfile',
+        'huggingface_hub',
+        'torch',
+    ],
+    license='CC BY-NC 4.0',
+    url='https://github.com/SonyCSLParis/music2latent',
+    keywords='audio speech music compression generative-model autoencoder diffusion consistency',
+)
