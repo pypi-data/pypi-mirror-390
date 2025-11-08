c0dce557aad9c1d1c13d40f6c4e6cce29270b5d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:19 +0000

    Add package padie-extended/0.0.5/padie_extended-0.0.5.tar.gz

---
+++ packages/padie-extended/padie_extended-0.0.5.tar.gz/padie_extended-0.0.5/src/padie_extended/__init__.py
@@ -0,0 +1,8 @@
+# from padie.core.language import detect_language
+# from padie.core.intent import detect_intent
+
+# __all__ = ["detect_language", "detect_intent"]
+
+from .language_detector import LanguageDetector
+
+__all__ = ["LanguageDetector"]
