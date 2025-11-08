baf6a8163a1dfc06695ef9cacacb11ed3b733c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:05:03 +0000

    Add package pymod112/0.1.5/pymod112-0.1.5-py3-none-any.whl

---
+++ packages/pymod112/pymod112-0.1.5-py3-none-any.whl/pymod112/__init__.py
@@ -0,0 +1,13 @@
+"""
+pymod112
+一个可以离线检验公民身份号码是否合法的程序
+v 0.1.5
+by HoshsL
+"""
+
+from .mod112 import *
+
+__name__ = "pymod112"
+__title__ = "pymod112"
+__version__ = "0.1.5"
+__author__ = "HoshsL"
