475e8a954474c7b7308914c1c8adf1375de20640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:19 +0000

    Add package dl-myo/1.1.8/dl_myo-1.1.8.tar.gz

---
+++ packages/dl-myo/dl_myo-1.1.8.tar.gz/dl_myo-1.1.8/myo/__init__.py
@@ -0,0 +1,36 @@
+"""
+    dl-myo
+    ------
+    Yet another MyoConnect alternative without dongles.
+"""
+from __future__ import absolute_import, annotations
+
+from .core import Myo, MyoClient
+from .profile import Handle
+from .types import (
+    AggregatedData,
+    ClassifierEvent,
+    ClassifierMode,
+    EMGData,
+    EMGDataSingle,
+    EMGMode,
+    FVData,
+    FirmwareInfo,
+    FirmwareVersion,
+    IMUData,
+    IMUMode,
+    MotionEvent,
+    MotionEventType,
+    SleepMode,
+    UnlockType,
+    UserActionType,
+    VibrationType,
+)
+from .version import __version__
+
+__author__ = "Iori Mizutani"
+__copyright__ = "Copyright (c) 2023 Iori Mizutani"
+__email__ = "iomz@sazanka.io"
+__license__ = "GPLv3"
+__summary__ = "Yet another MyoConnect alternative without dongles"
+__uri__ = "https://github.com/iomz/dl-myo"
