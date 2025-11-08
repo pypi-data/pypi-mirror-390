3815f493f3059951323b7b28107e0572e388e509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 09:05:17 +0000

    Add package passphera-shell/0.4.0/passphera_shell-0.4.0.tar.gz

---
+++ packages/passphera-shell/passphera_shell-0.4.0.tar.gz/passphera_shell-0.4.0/passphera_shell/interfaces.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+from passphera_core.entities import Generator
+
+from passphera_shell.entities import Password
+
+
+class GeneratorRepository(ABC):
+    @abstractmethod
+    def save(self, generator: Generator) -> None:
+        pass
+
+    @abstractmethod
+    def get(self) -> Generator:
+        pass
+
+    @abstractmethod
+    def update(self, generator: Generator) -> None:
+        pass
+
+
+class VaultRepository(ABC):
+    @abstractmethod
+    def save(self, password: Password) -> None:
+        pass
+
+    @abstractmethod
+    def get(self, context: str) -> Password:
+        pass
+
+    @abstractmethod
+    def update(self, password: Password) -> None:
+        pass
+
+    @abstractmethod
+    def delete(self, password: Password) -> None:
+        pass
+
+    @abstractmethod
+    def list(self) -> list[Password]:
+        pass
+
+    @abstractmethod
+    def flush(self) -> None:
+        pass
+
+
+class CryptoService(ABC):
+    @abstractmethod
+    def encrypt(self, plaintext: str) -> tuple[str, bytes]:
+        """Encrypt plaintext and return (ciphertext, salt)."""
+        pass
+
+    @abstractmethod
+    def decrypt(self, ciphertext: str, salt: bytes) -> str:
+        """Decrypt ciphertext using salt and return plaintext."""
+        pass
