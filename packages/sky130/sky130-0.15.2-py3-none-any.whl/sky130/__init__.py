b5ecf3b69c25821ad682404dd629b4fa480683ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:54 +0000

    Add package sky130/0.15.2/sky130-0.15.2-py3-none-any.whl

---
+++ packages/sky130/sky130-0.15.2-py3-none-any.whl/sky130/__init__.py
@@ -0,0 +1,36 @@
+"""Sky130 - skywater gdsfactory pdk"""
+
+import gdsfactory as gf
+from gdsfactory.get_factories import get_cells
+from gdsfactory.pdk import Pdk
+
+from sky130 import components, pcells
+from sky130.layers import LAYER, LAYER_STACK, LAYER_VIEWS, connectivity
+from sky130.tech import cross_sections, routing_strategies
+
+__version__ = "0.15.2"
+
+gf.CONF.allow_layer_mismatch = True
+gf.CONF.allow_width_mismatch = True
+
+cells = get_cells([components, pcells])
+PDK = Pdk(
+    name="sky130",
+    cells=cells,
+    cross_sections=cross_sections,
+    layers=LAYER,
+    layer_stack=LAYER_STACK,
+    layer_views=LAYER_VIEWS,
+    routing_strategies=routing_strategies,
+    connectivity=connectivity,
+)
+PDK.activate()
+
+__all__ = [
+    "cells",
+    "PDK",
+    "components",
+    "pcells",
+    "LAYER",
+    "LAYER_STACK",
+]
