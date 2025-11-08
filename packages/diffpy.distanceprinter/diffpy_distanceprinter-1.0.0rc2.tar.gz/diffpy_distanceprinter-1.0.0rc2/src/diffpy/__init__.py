da6cdf09c0524f774789216f2f34c031d600e173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:42 +0000

    Add package diffpy.distanceprinter/1.0.0rc2/diffpy_distanceprinter-1.0.0rc2.tar.gz

---
+++ packages/diffpy.distanceprinter/diffpy_distanceprinter-1.0.0rc2.tar.gz/diffpy_distanceprinter-1.0.0rc2/src/diffpy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+##############################################################################
+#
+# (c) 2013-2025 The Trustees of Columbia University in the City of New York.
+# All rights reserved.
+#
+# File coded by: Xiaohao Yang and Billinge Group members
+#
+# See GitHub contributions for a more detailed list of contributors.
+# https://github.com/diffpy/diffpy.distanceprinter/graphs/contributors
+# noqa: E501
+#
+# See LICENSE.rst for license information.
+#
+##############################################################################
