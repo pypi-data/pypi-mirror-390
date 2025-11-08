bc7844b149fc6dbeada9422f81a4551568f22a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:26 +0000

    Add package text-python/0.1/text_python-0.1.tar.gz

---
+++ packages/text-python/text_python-0.1.tar.gz/text_python-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text_python",
+    version="0.1",
+    author="YourName",
+    description="Ngon ngu Viet khong dau ket hop Python va pygame",
+    packages=find_packages(),
+    install_requires=["pygame"],
+    python_requires=">=3.8",
+)
