31ce89bcad6c0588141b8178fdb4da7c9a99ea64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 15:05:22 +0000

    Add package escapyst/0.0.2/escapyst-0.0.2-py3-none-any.whl

---
+++ packages/escapyst/escapyst-0.0.2-py3-none-any.whl/escapyst/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 vi:noet
+# SPDX-FileCopyrightText: 2025 Jérôme Carretero <cJ-escapyst@zougloub.eu> & contributors
+# SPDX-License-Identifier: MIT
+
+"""Package implementing Binary<->String codecs based on escape sequences"""
+
+__version__ = "0.0.2"
