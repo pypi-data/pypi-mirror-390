67381885d6e3adfc4a317ca0f6830de3b822d477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 08:05:36 +0000

    Add package aws-sdk-python/0.1.2/aws_sdk_python-0.1.2-py3-none-any.whl

---
+++ packages/aws-sdk-python/aws_sdk_python-0.1.2-py3-none-any.whl/aws_sdk_python/__init__.py
@@ -0,0 +1,6 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+__version__ = "0.1.2"
+
+
+# TODO: Consider adding relative imports for services from the top level namespace?
