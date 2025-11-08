9337db00ee82438be67d0cf380d668443756d5ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:05:14 +0000

    Add package rocketTestSoufiane/1.0.0/rockettestsoufiane-1.0.0.tar.gz

---
+++ packages/rocketTestSoufiane/rockettestsoufiane-1.0.0.tar.gz/rockettestsoufiane-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='rocketTestSoufiane',
+   version='1.0.0',
+   author='Bensetallah soufiane',
+   author_email='rtsof91@gmail.com',
+   packages=['rocketTestSoufiane'],
+   url='http://pypi.python.org/pypi/rocketTestSoufiane/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[]
+)
