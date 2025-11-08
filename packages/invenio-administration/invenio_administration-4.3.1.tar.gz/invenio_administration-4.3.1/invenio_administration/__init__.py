1228fcafabb391219e126fe0d8347cb7e30a696a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 04:07:43 +0000

    Add package invenio-administration/4.3.1/invenio_administration-4.3.1.tar.gz

---
+++ packages/invenio-administration/invenio_administration-4.3.1.tar.gz/invenio_administration-4.3.1/invenio_administration/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2022-2024 CERN.
+# Copyright (C) 2024-2025 Graz University of Technology.
+#
+# invenio-administration is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Invenio module providing extensible administration panel."""
+
+from .ext import InvenioAdministration
+
+__version__ = "4.3.1"
+
+__all__ = ["InvenioAdministration"]
