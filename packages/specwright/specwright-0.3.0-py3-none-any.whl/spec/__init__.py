0b321f8562b4c947437b829dd202a3f7a0e3ed4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:04:50 +0000

    Add package specwright/0.3.0/specwright-0.3.0-py3-none-any.whl

---
+++ packages/specwright/specwright-0.3.0-py3-none-any.whl/spec/__init__.py
@@ -0,0 +1,3 @@
+"""Specwright: Meta-engineering orchestration for Agentic Implementation Plans."""
+
+__version__ = "0.3.0"
---
+++ packages/specwright/specwright-0.3.0-py3-none-any.whl/spec/cli/__init__.py
@@ -0,0 +1,5 @@
+"""CLI module for Specwright."""
+
+from spec.cli.spec import app
+
+__all__ = ["app"]
