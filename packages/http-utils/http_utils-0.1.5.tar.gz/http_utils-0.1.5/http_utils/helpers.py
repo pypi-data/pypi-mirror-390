c8542c8bcfb17d9db2b02a3d7745b27c3df83814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 08:05:07 +0000

    Add package http-utils/0.1.5/http_utils-0.1.5.tar.gz

---
+++ packages/http-utils/http_utils-0.1.5.tar.gz/http_utils-0.1.5/http_utils/helpers.py
@@ -0,0 +1,34 @@
+"""
+Helpers module for handling HTTP Response and Header objects.
+"""
+
+from requests import Response
+
+
+def convert_header_to_meta_key(header: str | None) -> str | None:
+    """
+    Django Http Request has a dictionary called META with the request_session headers.
+    But it replace the header name following the rules:
+      - all characters to uppercase
+      - replacing any hyphens with underscores
+      - adding an HTTP_ prefix to the name.
+    So, for example, a header called X-Bender would be mapped to the META key HTTP_X_BENDER.
+    """
+    if header is None:
+        return None
+
+    return f"HTTP_{header.replace('-', '_')}".upper()
+
+
+def get_response_body(response: Response) -> dict:
+    """
+    Deserialize the response returning a deserialized json dict or a text.
+
+    :param response:
+        Response object from `requests` module.
+    """
+    try:
+        response_body = response.json()
+    except ValueError:
+        response_body = response.text
+    return response_body
