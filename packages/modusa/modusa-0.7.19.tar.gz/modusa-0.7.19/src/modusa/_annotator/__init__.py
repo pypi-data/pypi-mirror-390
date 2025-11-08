44e6f2b3264570bc897e36c2ffb6f278b1863e7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 15:05:22 +0000

    Add package modusa/0.7.19/modusa-0.7.19.tar.gz

---
+++ packages/modusa/modusa-0.7.19.tar.gz/modusa-0.7.19/src/modusa/_annotator/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+#---------------------------------
+# Author: Ankit Anand
+# Date: 07/11/25
+# Email: ankit0.anand0@gmail.com
+#---------------------------------
+
---
+++ packages/modusa/modusa-0.7.19.tar.gz/modusa-0.7.19/src/modusa/_mediarecorder/_mediarecorder.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+#---------------------------------
+# Author: Ankit Anand
+# Date: 06/11/25
+# Email: ankit0.anand0@gmail.com
+#---------------------------------
+
+class MediaRecorder:
+	
+	def __init(self):
+		pass
+		
+	def mic():
+		"""
+		Create a UI to record audio in jupyter notebook, the 
+		recorded signal is available as array.
+	
+		.. code-block:: python
+			
+			import modusa as ms
+			result = ms.record()
+			y, sr, title = result() # Keep it in the next cell
+	
+		Returns
+		-------
+		Callable
+			A lambda function that returns y(audio signal), sr(sampling rate), title(title set in the UI)
+		"""
+		
+		from ._mic import MicRecorder
+		
+		rec = MicRecorder.record()
+		
+		return rec
