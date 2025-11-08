6baf3071cd441f2887f238c97c52fc9e3198623c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 09:05:04 +0000

    Add package masterframework/0.4.0/masterframework-0.4.0-py3-none-any.whl

---
+++ packages/masterframework/masterframework-0.4.0-py3-none-any.whl/masterframework/augmentation/augmentation.py
@@ -0,0 +1,51 @@
+from abc import ABC
+from pathlib import Path
+
+from src.masterframework.data_loader import Task
+
+
+class Augmentation(ABC):
+    """Augmentations are used to modify the prompt that will be provided to a model"""
+
+    def apply(self, task: Task) -> str:
+        return task.prompt
+
+    def apply_before(self, task: Task) -> str | None:
+        return None
+
+    def apply_after(self, task: Task) -> str | None:
+        return None
+
+
+class SecurityAugmentation(Augmentation):
+    """Basic augmentation to add security related information to the prompt"""
+
+    def __init__(self, prefix, suffix=None):
+        self.prefix = prefix
+        self.suffix = suffix
+
+    def apply_before(self, task: Task) -> str | None:
+        return self.prefix
+
+    def apply_after(self, task: Task) -> str | None:
+        return self.suffix
+
+    @staticmethod
+    def from_template(template: str | Path) -> 'SecurityAugmentation':
+        """Read the template file and return a SecurityAugmentation object"""
+        # check if the template is a file
+        if isinstance(template, Path):
+            template_content = template.read_text(encoding='utf-8')
+        else:
+            template_content = template
+        
+        if '{PROMPT}' in template_content:
+            parts = template_content.split('{PROMPT}')
+            prefix = parts[0].strip() if parts[0].strip() else None
+            suffix = parts[1].strip() if len(parts) > 1 and parts[1].strip() else None
+        else:
+            prefix = template_content.strip()
+            suffix = None
+        
+        return SecurityAugmentation(prefix, suffix)
+
