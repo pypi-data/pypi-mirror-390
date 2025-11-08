3fe5727f4dd17a550db6c76a0f0e4bd2486bd912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:34 +0000

    Add package dfcon/0.2.12/dfcon-0.2.12-py3-none-any.whl

---
+++ packages/dfcon/dfcon-0.2.12-py3-none-any.whl/dfcon/__init__.py
@@ -0,0 +1,22 @@
+"""Initialize published package."""
+
+from .directory import Directory
+from .path_filter import FileFilter, DircFilter
+
+############## EDIT THESE INFORMATION ###############
+AUTHOR = "Tamon Mikawa"
+EMAIL = "mtamon.engineering@gmail.com"
+YEAR = "2023"
+GIT_URL = "https://github.com/MTamon/dataFileController.git"
+VERSION = "0.2.12"
+LICENCE = "MIT License"
+#####################################################
+
+__copyright__ = f"Copyright (C) {YEAR} {AUTHOR}"
+__version__ = VERSION
+__license__ = LICENCE
+__author__ = AUTHOR
+__author_email__ = EMAIL
+__url__ = GIT_URL
+
+__all__ = ["path_filter", "directory"]
