0fc4e7d348560ff3c3f276c5154e7590d8cf921d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:28 +0000

    Add package pyfixest/0.40.1/pyfixest-0.40.1-cp313-cp313t-musllinux_1_2_i686.whl

---
+++ packages/pyfixest/pyfixest-0.40.1-cp313-cp313t-musllinux_1_2_i686.whl/pyfixest/report/__init__.py
@@ -0,0 +1,21 @@
+from pyfixest.report.summarize import (
+    dtable,
+    etable,
+    make_table,
+    summary,
+)
+from pyfixest.report.visualize import (
+    coefplot,
+    iplot,
+    qplot,
+)
+
+__all__ = [
+    "coefplot",
+    "dtable",
+    "etable",
+    "iplot",
+    "make_table",
+    "qplot",
+    "summary",
+]
