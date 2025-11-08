14410082566fd4408f5b4163cbc0fe585e1c5565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 14:05:48 +0000

    Add package microsoft-agents-m365copilot/0.1.0rc2/microsoft_agents_m365copilot-0.1.0rc2-py3-none-any.whl

---
+++ packages/microsoft-agents-m365copilot/microsoft_agents_m365copilot-0.1.0rc2-py3-none-any.whl/microsoft_agents_m365copilot/_version.py
@@ -0,0 +1,9 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.  All Rights Reserved.  
+# Licensed under the MIT License.  See License in the project root for license information.
+# ------------------------------------------------------------------------------
+
+# The SDK version
+# x-release-please-start-version
+VERSION: str = '0.1.0-preview.2'
+# x-release-please-end
