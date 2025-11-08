2ac0572776a53a53e35e18b89ae5d50333f0e20f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:33 +0000

    Add package awsreplicate/0.1.1/awsreplicate-0.1.1-py3-none-any.whl

---
+++ packages/awsreplicate/awsreplicate-0.1.1-py3-none-any.whl/awsreplicate/utils/logging.py
@@ -0,0 +1,34 @@
+"""Logging configuration."""
+
+import structlog
+import logging
+
+
+def setup_logger(log_level: str = "INFO") -> None:
+    """
+    Configure structured logging.
+
+    Args:
+        log_level: Logging level (DEBUG, INFO, WARNING, ERROR)
+    """
+    logging.basicConfig(
+        format="%(message)s",
+        level=getattr(logging, log_level.upper()),
+    )
+
+    structlog.configure(
+        processors=[
+            structlog.stdlib.filter_by_level,
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.processors.StackInfoRenderer(),
+            structlog.processors.format_exc_info,
+            structlog.processors.UnicodeDecoder(),
+            structlog.dev.ConsoleRenderer(),
+        ],
+        wrapper_class=structlog.stdlib.BoundLogger,
+        context_class=dict,
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
