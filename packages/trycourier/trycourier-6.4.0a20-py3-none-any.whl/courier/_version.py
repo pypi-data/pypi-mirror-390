22258def525853677cf7f845f4f181cdd3e4159e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 09:04:56 +0000

    Add package trycourier/6.4.0a20/trycourier-6.4.0a20-py3-none-any.whl

---
+++ packages/trycourier/trycourier-6.4.0a20-py3-none-any.whl/courier/_version.py
@@ -0,0 +1,4 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+__title__ = "courier"
+__version__ = "6.4.0-alpha20"  # x-release-please-version
---
+++ packages/trycourier/trycourier-6.4.0a20-py3-none-any.whl/courier/resources/tenants/__init__.py
@@ -0,0 +1,47 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .tenants import (
+    TenantsResource,
+    AsyncTenantsResource,
+    TenantsResourceWithRawResponse,
+    AsyncTenantsResourceWithRawResponse,
+    TenantsResourceWithStreamingResponse,
+    AsyncTenantsResourceWithStreamingResponse,
+)
+from .templates import (
+    TemplatesResource,
+    AsyncTemplatesResource,
+    TemplatesResourceWithRawResponse,
+    AsyncTemplatesResourceWithRawResponse,
+    TemplatesResourceWithStreamingResponse,
+    AsyncTemplatesResourceWithStreamingResponse,
+)
+from .tenant_default_preferences import (
+    TenantDefaultPreferencesResource,
+    AsyncTenantDefaultPreferencesResource,
+    TenantDefaultPreferencesResourceWithRawResponse,
+    AsyncTenantDefaultPreferencesResourceWithRawResponse,
+    TenantDefaultPreferencesResourceWithStreamingResponse,
+    AsyncTenantDefaultPreferencesResourceWithStreamingResponse,
+)
+
+__all__ = [
+    "TenantDefaultPreferencesResource",
+    "AsyncTenantDefaultPreferencesResource",
+    "TenantDefaultPreferencesResourceWithRawResponse",
+    "AsyncTenantDefaultPreferencesResourceWithRawResponse",
+    "TenantDefaultPreferencesResourceWithStreamingResponse",
+    "AsyncTenantDefaultPreferencesResourceWithStreamingResponse",
+    "TemplatesResource",
+    "AsyncTemplatesResource",
+    "TemplatesResourceWithRawResponse",
+    "AsyncTemplatesResourceWithRawResponse",
+    "TemplatesResourceWithStreamingResponse",
+    "AsyncTemplatesResourceWithStreamingResponse",
+    "TenantsResource",
+    "AsyncTenantsResource",
+    "TenantsResourceWithRawResponse",
+    "AsyncTenantsResourceWithRawResponse",
+    "TenantsResourceWithStreamingResponse",
+    "AsyncTenantsResourceWithStreamingResponse",
+]
