bb17434b6f44911dfb0eb7e2ae0f820ff4084648
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 15:05:39 +0000

    Add package beekeeper-monitors-watsonx/1.0.6/beekeeper_monitors_watsonx-1.0.6.tar.gz

---
+++ packages/beekeeper-monitors-watsonx/beekeeper_monitors_watsonx-1.0.6.tar.gz/beekeeper_monitors_watsonx-1.0.6/beekeeper/monitors/watsonx/__init__.py
@@ -0,0 +1,21 @@
+from beekeeper.monitors.watsonx.base import (
+    CloudPakforDataCredentials,
+    IntegratedSystemCredentials,
+    WatsonxCustomMetric,
+    WatsonxExternalPromptMonitor,
+    WatsonxLocalMetric,
+    WatsonxMetric,
+    WatsonxMetricThreshold,
+    WatsonxPromptMonitor,
+)
+
+__all__ = [
+    "CloudPakforDataCredentials",
+    "IntegratedSystemCredentials",
+    "WatsonxCustomMetric",
+    "WatsonxExternalPromptMonitor",
+    "WatsonxLocalMetric",
+    "WatsonxMetric",
+    "WatsonxMetricThreshold",
+    "WatsonxPromptMonitor",
+]
