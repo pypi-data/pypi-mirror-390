8ba6059b33c2f49f5dd06c7ab767a03159267219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:05:11 +0000

    Add package edx-codejail/4.1.0/edx_codejail-4.1.0.tar.gz

---
+++ packages/edx-codejail/edx_codejail-4.1.0.tar.gz/edx_codejail-4.1.0/codejail/jail_code.py
@@ -0,0 +1,384 @@
+"""Run code in a jail."""
+
+import logging
+import os
+import os.path
+import resource
+import shutil
+import sys
+
+from .proxy import run_subprocess_through_proxy
+from .subproc import run_subprocess
+from .util import temp_directory
+
+log = logging.getLogger("codejail")
+
+# TODO: limit too much stdout data?  # pylint: disable=fixme
+
+# Configure the commands
+
+# COMMANDS is a map from an abstract command name to a list of command-line
+# pieces, such as subprocess.Popen wants.
+COMMANDS = {}
+
+
+def configure(command, bin_path, user=None):
+    """
+    Configure a command for `jail_code` to use.
+
+    `command` is the abstract command you're configuring, such as "python" or
+    "node".  `bin_path` is the path to the binary.  `user`, if provided, is
+    the user name to run the command under.
+
+    """
+    cmd_argv = [bin_path]
+
+    # Command-specific arguments
+    if command == "python":
+        # -E means ignore the environment variables PYTHON*
+        # -B means don't try to write .pyc files.
+        cmd_argv.extend(['-E', '-B'])
+
+    COMMANDS[command] = {
+        # The start of the command line for this program.
+        'cmdline_start': cmd_argv,
+        # The user to run this as, perhaps None.
+        'user': user,
+    }
+
+
+def is_configured(command):
+    """
+    Has `jail_code` been configured for `command`?
+
+    Returns true if the abstract command `command` has been configured for use
+    in the `jail_code` function.
+
+    """
+    return command in COMMANDS
+
+
+# By default, look where our current Python is, and maybe there's a
+# python-sandbox alongside.  Only do this if running in a virtualenv.
+# The check for sys.real_prefix covers virtualenv
+# the equality of non-empty sys.base_prefix with sys.prefix covers venv
+running_in_virtualenv = (
+    hasattr(sys, 'real_prefix') or
+    (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix)
+)
+
+if running_in_virtualenv:
+    # In test environment
+    sandbox_user = os.getenv('CODEJAIL_TEST_USER')
+    sandbox_env = os.getenv('CODEJAIL_TEST_VENV')
+    if sandbox_env and sandbox_user:
+        configure("python", f"{sandbox_env}/bin/python", sandbox_user)
+    # or fall back to defaults
+    elif os.path.isdir(sys.prefix + "-sandbox"):
+        configure("python", sys.prefix + "-sandbox/bin/python", "sandbox")
+
+
+# The resource limits that we unless otherwise configured.
+DEFAULT_LIMITS = {
+    # CPU seconds, defaulting to 1.
+    "CPU": 1,
+    # Real time, defaulting to 1 second.
+    "REALTIME": 1,
+    # Total process virutal memory, in bytes, defaulting to unlimited.
+    "VMEM": 0,
+    # Size of files creatable, in bytes, defaulting to nothing can be written.
+    "FSIZE": 0,
+    # The number of processes and threads to allow for the sandbox user (total
+    # across entire host).
+    "NPROC": 15,
+    # Whether to use a proxy process or not.  None means use an environment
+    # variable to decide. NOTE: using a proxy process is NOT THREAD-SAFE, only
+    # one thread can use CodeJail at a time if you are using a proxy process.
+    "PROXY": None,
+}
+
+# Configured resource limits.
+# Modified by calling `set_limit`.
+LIMITS = DEFAULT_LIMITS.copy()
+
+# Map from limit_overrides_contexts (strings) to dictionaries in the shape of LIMITS.
+# Modified by calling `override_limit`.
+LIMIT_OVERRIDES = {}
+
+
+def set_limit(limit_name, value):
+    """
+    Set a limit for `jail_code`.
+
+    `limit_name` is a string, the name of the limit to set. `value` is the
+    value to use for that limit.  The type, meaning, default, and range of
+    accepted values depend on `limit_name`.
+
+    These limits are available:
+
+        * `"CPU"`: the maximum number of CPU seconds the jailed code can use.
+            The value is an integer, defaulting to 1.
+
+        * `"REALTIME"`: the maximum number of seconds the jailed code can run,
+            in real time.  The default is 1 second.
+
+        * `"VMEM"`: the total virtual memory available to the jailed code, in
+            bytes.  The default is 0 (no memory limit).
+
+        * `"FSIZE"`: the maximum size of files creatable by the jailed code,
+            in bytes.  The default is 0 (no files may be created).
+
+        * `"NPROC"`: the maximum number of process or threads allowed for
+            jailed code across the entire host (combined across all instances
+            in all containers). This includes processes owned by the same UID
+            in containers where that UID is mapped to a different username.
+            The default is 15.
+
+        * `"PROXY"`: 1 to use a proxy process, 0 to not use one. This isn't
+            really a limit, sorry about that.
+
+    Limits are process-wide, and will affect all future calls to jail_code.
+    Providing a limit of 0 will disable that limit, unless otherwise specified.
+
+    """
+    LIMITS[limit_name] = value
+
+
+def get_effective_limits(overrides_context=None):
+    """
+    Calculate the effective limits dictionary.
+
+    Arguments:
+        overrides_context (str|None): Identifies which set of overrides to use.
+            If None or missing from `LIMIT_OVERRIDES`, then just return `LIMITS` as-is.
+    """
+    overrides = LIMIT_OVERRIDES.get(overrides_context, {}) if overrides_context else {}
+    return {**LIMITS, **overrides}
+
+
+def override_limit(limit_name, value, limit_overrides_context):
+    """
+    Override a limit for `jail_code`, but only in the context of `limit_overrides_context`.
+
+    See `set_limit` for the meaning of `limit_name` and `value`.
+
+    All limits may be overriden except PROXY. Having this setting be different between
+    executions of code is not supported. If one attempts to override PROXY, the override
+    will be ignored and the globally-configured value will be used instead.
+    """
+    if limit_name == 'PROXY' and LIMITS['PROXY'] != value:
+        log.error(
+            'Tried to override value of PROXY to %s. '
+            'Overriding PROXY on a per-context basis is not permitted. '
+            'Will use globally-configured value instead: %s.',
+            value,
+            LIMITS['PROXY'],
+        )
+        return
+    if limit_overrides_context not in LIMIT_OVERRIDES:
+        LIMIT_OVERRIDES[limit_overrides_context] = {}
+    LIMIT_OVERRIDES[limit_overrides_context][limit_name] = value
+
+
+class JailResult:
+    """
+    A passive object for us to return from jail_code.
+    """
+    def __init__(self):
+        self.stdout = self.stderr = self.status = None
+
+
+# pylint: disable=too-many-positional-arguments
+def jail_code(command, code=None, files=None, extra_files=None, argv=None,
+              stdin=None, limit_overrides_context=None, slug=None):
+    """
+    Run code in a jailed subprocess.
+
+    `command` is an abstract command ("python", "node", ...) that must have
+    been configured using `configure`.
+
+    `code` is a string containing the code to run.  If no code is supplied,
+    then the code to run must be in one of the `files` copied, and must be
+    named in the `argv` list.
+
+    `files` is a list of file paths, they are all copied to the jailed
+    directory.  Note that no check is made here that the files don't contain
+    sensitive information.  The caller must somehow determine whether to allow
+    the code access to the files.  Symlinks will be copied as symlinks.  If the
+    linked-to file is not accessible to the sandbox, the symlink will be
+    unreadable as well.
+
+    `extra_files` is a list of pairs, each pair is a filename and a bytestring
+    of contents to write into that file.  These files will be created in the
+    temp directory and cleaned up automatically.  No subdirectories are
+    supported in the filename.
+
+    `argv` is the command-line arguments to supply.
+
+    `stdin` is a string, the data to provide as the stdin for the process.
+
+    `limit_overrides_context` is an optional string to use as a key against the
+    configured limit overrides contexts. If omitted or if no such limit override context
+    has been configured, then use the default limits.
+
+    `slug` is an arbitrary string, a description that's meaningful to the
+    caller, that will be used in log messages.
+
+    Return an object with:
+
+        .stdout: stdout of the program, a string
+        .stderr: stderr of the program, a string
+        .status: exit status of the process: an int, 0 for success
+
+    """
+    # pylint: disable=too-many-statements
+
+    if not is_configured(command):
+        # pylint: disable=broad-exception-raised
+        raise Exception("jail_code needs to be configured for %r" % command)
+
+    # We make a temp directory to serve as the home of the sandboxed code.
+    # It has a writable "tmp" directory within it for temp files.
+
+    with temp_directory() as homedir:
+
+        # Make directory readable by other users ('sandbox' user needs to be
+        # able to read it).
+        os.chmod(homedir, 0o775)
+
+        # Make a subdir to use for temp files, world-writable so that the
+        # sandbox user can write to it.
+        tmptmp = os.path.join(homedir, "tmp")
+        os.mkdir(tmptmp)
+        os.chmod(tmptmp, 0o777)
+
+        argv = argv or []
+
+        # All the supporting files are copied into our directory.
+        for filename in files or ():
+            dest = os.path.join(homedir, os.path.basename(filename))
+            if os.path.islink(filename):
+                os.symlink(os.readlink(filename), dest)
+            elif os.path.isfile(filename):
+                shutil.copy(filename, homedir)
+            else:
+                shutil.copytree(filename, dest, symlinks=True)
+
+        # Create the main file.
+        if code:
+            with open(os.path.join(homedir, "jailed_code"), "wb") as jailed:
+                code_bytes = bytes(code, 'utf8')
+                jailed.write(code_bytes)
+
+            argv = ["jailed_code"] + argv
+
+        # Create extra files requested by the caller:
+        for name, content in extra_files or ():
+            with open(os.path.join(homedir, name), "wb") as extra:
+                extra.write(content)
+
+        cmd = []
+        rm_cmd = []
+
+        # Build the command to run.
+        user = COMMANDS[command]['user']
+        if user:
+            # Run as the specified user
+            cmd.extend(['sudo', '-u', user])
+            rm_cmd.extend(['sudo', '-u', user])
+
+        # Point TMPDIR at our temp directory.
+        # FIXME: This breaks command execution unless user param has been set.
+        #   Issue: https://github.com/openedx/codejail/issues/162
+        cmd.extend(['TMPDIR=tmp'])
+        # Start with the command line dictated by "python" or whatever.
+        cmd.extend(COMMANDS[command]['cmdline_start'])
+
+        # Add the code-specific command line pieces.
+        cmd.extend(argv)
+
+        # Determine effective resource limits.
+        effective_limits = get_effective_limits(limit_overrides_context)
+        if slug:
+            log.info(
+                "Preparing to execute jailed code %r "
+                "(overrides context = %r, resource limits = %r).",
+                slug,
+                limit_overrides_context,
+                effective_limits,
+            )
+
+        # Use the configuration and maybe an environment variable to determine
+        # whether to use a proxy process.
+        use_proxy = effective_limits["PROXY"]
+        if use_proxy is None:
+            use_proxy = int(os.environ.get("CODEJAIL_PROXY", "0"))
+        if use_proxy:
+            run_subprocess_fn = run_subprocess_through_proxy
+        else:
+            run_subprocess_fn = run_subprocess
+
+        if stdin:
+            stdin = bytes(stdin, 'utf-8')
+
+        # Run the subprocess.
+        status, stdout, stderr = run_subprocess_fn(
+            cmd=cmd, cwd=homedir, env={}, slug=slug,
+            stdin=stdin,
+            realtime=effective_limits["REALTIME"],
+            rlimits=create_rlimits(effective_limits),
+            )
+
+        result = JailResult()
+        result.status = status
+        result.stdout = stdout
+        result.stderr = stderr
+
+        # Remove the tmptmp directory as the sandbox user since the sandbox
+        # user may have written files that the application user can't delete.
+        rm_cmd.extend([
+            '/usr/bin/find', tmptmp,
+            '-mindepth', '1', '-maxdepth', '1',
+            '-exec', 'rm', '-rf', '{}', ';'
+        ])
+
+        # Run the rm command subprocess.
+        run_subprocess_fn(rm_cmd, cwd=homedir)
+
+    return result
+
+
+def create_rlimits(effective_limits):
+    """
+    Create a list of resource limits for our jailed processes.
+
+    Arguments:
+        effective_limits (dict)
+    """
+    rlimits = []
+
+    # Allow a small number of subprocess and threads.  One limit controls both,
+    # and at least OpenBLAS (imported by numpy) requires threads.
+    nproc = effective_limits["NPROC"]
+    if nproc:
+        rlimits.append((resource.RLIMIT_NPROC, (nproc, nproc)))
+
+    # CPU seconds, not wall clock time.
+    cpu = effective_limits["CPU"]
+    if cpu:
+        # Set the soft limit and the hard limit differently.  When the process
+        # reaches the soft limit, a SIGXCPU will be sent, which should kill the
+        # process.  If you set the soft and hard limits the same, then the hard
+        # limit is reached, and a SIGKILL is sent, which is less distinctive.
+        rlimits.append((resource.RLIMIT_CPU, (cpu, cpu+1)))
+
+    # Total process virtual memory.
+    vmem = effective_limits["VMEM"]
+    if vmem:
+        rlimits.append((resource.RLIMIT_AS, (vmem, vmem)))
+
+    # Size of written files.  Can be zero (nothing can be written).
+    fsize = effective_limits["FSIZE"]
+    rlimits.append((resource.RLIMIT_FSIZE, (fsize, fsize)))
+
+    return rlimits
---
+++ packages/edx-codejail/edx_codejail-4.1.0.tar.gz/edx_codejail-4.1.0/codejail/safe_exec.py
@@ -0,0 +1,313 @@
+"""Safe execution of untrusted Python code."""
+
+import inspect
+import logging
+import os.path
+import shutil
+import sys
+import textwrap
+
+from codejail import jail_code
+from codejail.util import change_directory, temp_directory
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+
+log = logging.getLogger("codejail")
+
+
+# Flags to let developers temporarily change some behavior in this file.
+
+# Set this to True to log all the code and globals being executed.
+LOG_ALL_CODE = False
+
+# Set this to True to run submitted code with no confinement and no sandbox.
+#
+# WARNING: This is deeply dangerous; anyone who can submit code can take
+# over the computer immediately and entirely.
+#
+# The only purpose of this setting is for local debugging.
+ALWAYS_BE_UNSAFE = False
+
+
+class SafeExecException(Exception):
+    """
+    Python code running in the sandbox has failed.
+
+    The message will be the stdout of the sandboxed process, which will usually
+    contain the original exception message.
+
+    """
+
+
+# pylint: disable=too-many-positional-arguments
+def safe_exec(
+        code,
+        globals_dict,
+        files=None,
+        python_path=None,
+        limit_overrides_context=None,
+        slug=None,
+        extra_files=None,
+):
+    """
+    Execute code as "exec" does, but safely.
+
+    `code` is a string of Python code.  `globals_dict` is used as the globals
+    during execution.  Modifications the code makes to `globals_dict` are
+    reflected in the dictionary on return.
+
+    `files` is a list of file paths, either files or directories.  They will be
+    copied into the temp directory used for execution.  No attempt is made to
+    determine whether the file is appropriate or safe to copy.  The caller must
+    determine which files to provide to the code.
+
+    `python_path` is a list of directory or file paths.  These names will be
+    added to `sys.path` so that modules they contain can be imported.  Only
+    directories and zip files are supported.  If the name is not provided in
+    `extras_files`, it will be copied just as if it had been listed in `files`.
+
+    `limit_overrides_context` is an optional string to use as a key against the
+    configured limit overrides contexts. If omitted or if no such limit override context
+    has been configured, then use the default limits.
+
+    `slug` is an arbitrary string, a description that's meaningful to the
+    caller, that will be used in log messages.
+
+    `extra_files` is a list of pairs, each pair is a filename and a bytestring
+    of contents to write into that file.  These files will be created in the
+    temp directory and cleaned up automatically.  No subdirectories are
+    supported in the filename.
+
+    Returns None.  Changes made by `code` are visible in `globals_dict`.  If
+    the code raises an exception, this function will raise `SafeExecException`
+    with the stderr of the sandbox process, which usually includes the original
+    exception message and traceback.
+    """
+    if ALWAYS_BE_UNSAFE:
+        not_safe_exec(
+            code,
+            globals_dict,
+            files=files,
+            python_path=python_path,
+            limit_overrides_context=limit_overrides_context,
+            slug=slug,
+            extra_files=extra_files,
+        )
+        return
+
+    if not jail_code.is_configured('python'):
+        raise RuntimeError("safe_exec has not been configured for Python")
+
+    the_code = []
+
+    files = list(files or ())
+    extra_files = extra_files or ()
+    python_path = python_path or ()
+
+    extra_names = {name for name, contents in extra_files}
+
+    the_code.append(textwrap.dedent(
+        """
+        import sys
+        import six
+        try:
+            import simplejson as json
+        except ImportError:
+            import json
+        """
+        # We need to prevent the sandboxed code from printing to stdout,
+        # or it will pollute the json we print there.  This isn't a
+        # security concern (they can put any values in the json output
+        # anyway, either by writing to sys.__stdout__, or just by defining
+        # global values), but keeps accidents from happening.
+        """
+        class DevNull(object):
+            def write(self, *args, **kwargs):
+                pass
+
+            def flush(self, *args, **kwargs):
+                pass
+        sys.stdout = DevNull()
+        """
+        # Read the code and the globals from the stdin.
+        """
+        code, g_dict = json.load(sys.stdin)
+        """))
+
+    for pydir in python_path:
+        pybase = os.path.basename(pydir)
+        the_code.append("sys.path.append(%r)\n" % pybase)
+        if pybase not in extra_names:
+            files.append(pydir)
+
+    the_code.append(textwrap.dedent(
+        # Execute the sandboxed code.
+        """
+        exec(code, g_dict)
+        """))
+
+    the_code.append(inspect.getsource(json_safe))
+
+    the_code.append(textwrap.dedent(
+        """
+        g_dict = json_safe(g_dict)
+        """
+        # Write the globals back to the calling process.
+        """
+        json.dump(g_dict, sys.__stdout__)
+        """))
+
+    stdin = json.dumps([code, json_safe(globals_dict)])
+    jailed_code = "".join(the_code)
+
+    # Turn this on to see what's being executed.
+    if LOG_ALL_CODE:        # pragma: no cover
+        log.debug("Jailed code: %s", jailed_code)
+        log.debug("Exec: %s", code)
+        log.debug("Stdin: %s", stdin)
+
+    res = jail_code.jail_code(
+        "python", code=jailed_code, stdin=stdin, files=files,
+        limit_overrides_context=limit_overrides_context,
+        slug=slug, extra_files=extra_files,
+    )
+
+    if LOG_ALL_CODE:
+        log.debug("Status: %s", res.status)
+        log.debug("Stdout: %s", res.stdout)
+        log.debug("Stderr: %s", res.stderr)
+
+    if res.status != 0:
+        raise SafeExecException((
+            "Couldn't execute jailed code: stdout: {res.stdout!r}, "
+            "stderr: {res.stderr!r} with status code: {res.status}"
+        ).format(res=res))
+    globals_dict.update(json.loads(res.stdout.decode('utf-8')))
+
+
+def json_safe(d):
+    """
+    Return only the JSON-safe part of d(a python dict object).
+
+    Used to emulate reading data through a serialization straw.
+
+    """
+    # pylint: disable=invalid-name
+
+    # six.binary_type is here because bytes are sometimes ok if they represent valid utf8
+    # so we consider them valid for now and try to decode them with decode_object.  If that
+    # doesn't work they'll get dropped later in the process.
+    ok_types = (type(None), int, float, bytes, str, list, tuple, dict)
+
+    def decode_object(obj):
+        """
+        Convert an object to a JSON serializable form by decoding all byte strings.
+
+        In particular if we find any byte strings try to convert them to
+        utf-8 strings.  If we run into byte strings that can't be decoded as utf8 strings
+        throw an exception.
+
+        For other non-serializable objects we return them as is.
+
+        raises: Exception
+        """
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        if isinstance(obj, (list, tuple)):
+            new_list = []
+            for i in obj:
+                new_obj = decode_object(i)
+                new_list.append(new_obj)
+            return new_list
+        if isinstance(obj, dict):
+            new_dict = {}
+            for k, v in obj.items():
+                new_key = decode_object(k)
+                new_value = decode_object(v)
+                new_dict[new_key] = new_value
+            return new_dict
+        return obj
+
+    bad_keys = ("__builtins__",)
+    jd = {}
+    for k, v in d.items():
+        if not isinstance(v, ok_types):
+            continue
+        if k in bad_keys:
+            continue
+        try:
+            # Python's JSON encoder will produce output that
+            # the JSON decoder cannot parse if the input string
+            # contains unicode "unpaired surrogates" (only on Linux)
+            # To test for this, we try decoding the output and check
+            # for a ValueError
+            v = json.loads(json.dumps(decode_object(v)))
+
+            # Also ensure that the keys encode/decode correctly
+            k = json.loads(json.dumps(decode_object(k)))
+        except Exception:  # pylint: disable=broad-except
+            continue
+        else:
+            jd[k] = v
+    return json.loads(json.dumps(jd))
+
+
+# pylint: disable=too-many-positional-arguments
+def not_safe_exec(
+        code,
+        globals_dict,
+        files=None,
+        python_path=None,
+        limit_overrides_context=None,  # pylint: disable=unused-argument
+        slug=None,  # pylint: disable=unused-argument
+        extra_files=None,
+):
+    """
+    Another implementation of `safe_exec`, but not safe.
+
+    This can be swapped in for debugging problems in sandboxed Python code.
+
+    This is not thread-safe, due to temporarily changing the current directory
+    and modifying sys.path.
+
+    Note that `limit_overrides_context` is ignored here, because resource limits
+    are not applied.
+    """
+    # Because it would be bad if this function were used in production,
+    # let's log a warning when it is used.  Developers can live with
+    # one more log line.
+    log.warning("DANGER: Executing code with `not_safe_exec` for %s", slug)
+
+    g_dict = json_safe(globals_dict)
+
+    with temp_directory() as tmpdir:
+        with change_directory(tmpdir):
+            # pylint: disable=invalid-name
+            # Copy the files here.
+            for filename in files or ():
+                dest = os.path.join(tmpdir, os.path.basename(filename))
+                shutil.copyfile(filename, dest)
+            for filename, contents in extra_files or ():
+                dest = os.path.join(tmpdir, filename)
+                with open(dest, "wb") as f:
+                    f.write(contents)
+
+            original_path = sys.path
+            if python_path:
+                sys.path.extend(python_path)
+            try:
+                exec(code, g_dict)  # pylint: disable=exec-used
+            except Exception as e:
+                # Wrap the exception in a SafeExecException, but we don't
+                # try here to include the traceback, since this is just a
+                # substitute implementation.
+                msg = "{0.__class__.__name__}: {0!s}".format(e)
+                raise SafeExecException(msg)  # pylint: disable=raise-missing-from
+            finally:
+                sys.path = original_path
+
+    globals_dict.update(json_safe(g_dict))
