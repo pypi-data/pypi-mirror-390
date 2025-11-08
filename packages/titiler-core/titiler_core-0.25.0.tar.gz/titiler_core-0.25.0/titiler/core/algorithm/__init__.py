40cd60444bfb6d6d9b023058fa8d610ab0a0ec8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:06:32 +0000

    Add package titiler-core/0.25.0/titiler_core-0.25.0.tar.gz

---
+++ packages/titiler-core/titiler_core-0.25.0.tar.gz/titiler_core-0.25.0/titiler/core/algorithm/__init__.py
@@ -0,0 +1,102 @@
+"""titiler.core.algorithm."""
+
+import json
+from copy import copy
+from typing import Dict, List, Literal, Optional, Type
+
+import attr
+from fastapi import HTTPException, Query
+from pydantic import ValidationError
+from typing_extensions import Annotated
+
+from titiler.core.algorithm.base import (  # noqa
+    AlgorithmMetadata,
+    AlgorithmtList,
+    BaseAlgorithm,
+)
+from titiler.core.algorithm.dem import Contours, HillShade, Slope, TerrainRGB, Terrarium
+from titiler.core.algorithm.image import ToBitonal, ToGrayScale
+from titiler.core.algorithm.index import NormalizedIndex
+from titiler.core.algorithm.math import _Max, _Mean, _Median, _Min, _Std, _Sum, _Var
+from titiler.core.algorithm.ops import CastToInt, Ceil, Floor
+
+default_algorithms: Dict[str, Type[BaseAlgorithm]] = {
+    "hillshade": HillShade,
+    "slope": Slope,
+    "contours": Contours,
+    "normalizedIndex": NormalizedIndex,
+    "terrarium": Terrarium,
+    "terrainrgb": TerrainRGB,
+    "cast": CastToInt,
+    "ceil": Ceil,
+    "floor": Floor,
+    "min": _Min,
+    "max": _Max,
+    "median": _Median,
+    "mean": _Mean,
+    "std": _Std,
+    "var": _Var,
+    "sum": _Sum,
+    "grayscale": ToGrayScale,
+    "bitonal": ToBitonal,
+}
+
+
+@attr.s(frozen=True)
+class Algorithms:
+    """Algorithms."""
+
+    data: Dict[str, Type[BaseAlgorithm]] = attr.ib()
+
+    def get(self, name: str) -> BaseAlgorithm:
+        """Fetch a TMS."""
+        if name not in self.data:
+            raise KeyError(f"Invalid name: {name}")
+
+        return self.data[name]
+
+    def list(self) -> List[str]:
+        """List registered Algorithm."""
+        return list(self.data.keys())
+
+    def register(
+        self,
+        algorithms: Dict[str, BaseAlgorithm],
+        overwrite: bool = False,
+    ) -> "Algorithms":
+        """Register Algorithm(s)."""
+        for name, _algo in algorithms.items():
+            if name in self.data and not overwrite:
+                raise Exception(f"{name} is already a registered. Use overwrite=True.")
+
+        return Algorithms({**self.data, **algorithms})
+
+    @property
+    def dependency(self):
+        """FastAPI PostProcess dependency."""
+
+        def post_process(
+            algorithm: Annotated[
+                Literal[tuple(self.data.keys())],
+                Query(description="Algorithm name"),
+            ] = None,
+            algorithm_params: Annotated[
+                Optional[str],
+                Query(description="Algorithm parameter"),
+            ] = None,
+        ) -> Optional[BaseAlgorithm]:
+            """Data Post-Processing options."""
+            kwargs = json.loads(algorithm_params) if algorithm_params else {}
+            if algorithm:
+                try:
+                    return self.get(algorithm)(**kwargs)
+
+                except ValidationError as e:
+                    raise HTTPException(status_code=400, detail=str(e)) from e
+
+            return None
+
+        return post_process
+
+
+algorithms = Algorithms(copy(default_algorithms))  # noqa
---
+++ packages/titiler-core/titiler_core-0.25.0.tar.gz/titiler_core-0.25.0/titiler/core/models/responses.py
@@ -0,0 +1,62 @@
+"""TiTiler response models."""
+
+from typing import Dict, List, Optional, Union
+
+from geojson_pydantic.features import Feature, FeatureCollection
+from geojson_pydantic.geometries import Geometry, MultiPolygon, Polygon
+from pydantic import BaseModel
+from rio_tiler.models import BandStatistics, Info
+
+from titiler.core.models.common import Link
+
+
+class Point(BaseModel):
+    """
+    Point model.
+
+    response model for `/point` endpoints
+
+    """
+
+    coordinates: List[float]
+    values: List[Optional[float]]
+    band_names: List[str]
+
+
+InfoGeoJSON = Feature[Union[Polygon, MultiPolygon], Info]
+Statistics = Dict[str, BandStatistics]
+
+
+class StatisticsInGeoJSON(BaseModel):
+    """Statistics model in geojson response."""
+
+    statistics: Statistics
+
+    model_config = {"extra": "allow"}
+
+
+StatisticsGeoJSON = Union[
+    FeatureCollection[Feature[Geometry, StatisticsInGeoJSON]],
+    Feature[Geometry, StatisticsInGeoJSON],
+]
+
+# MultiBase Models
+MultiBaseInfo = Dict[str, Info]
+MultiBaseInfoGeoJSON = Feature[Union[Polygon, MultiPolygon], MultiBaseInfo]
+
+MultiBaseStatistics = Dict[str, Statistics]
+MultiBaseStatisticsGeoJSON = StatisticsGeoJSON
+
+
+class ColorMapRef(BaseModel):
+    """ColorMapRef model."""
+
+    id: str
+    title: Optional[str] = None
+    links: List[Link]
+
+
+class ColorMapList(BaseModel):
+    """Model for colormap list."""
+
+    colormaps: List[ColorMapRef]
