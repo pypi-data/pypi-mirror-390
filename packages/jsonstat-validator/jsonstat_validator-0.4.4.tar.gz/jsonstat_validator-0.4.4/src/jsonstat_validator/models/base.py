a63f390c375cc092a69d6d475a7ad485d2c31c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 03:19:06 +0000

    Add package jsonstat-validator/0.4.4/jsonstat_validator-0.4.4.tar.gz

---
+++ packages/jsonstat-validator/jsonstat_validator-0.4.4.tar.gz/jsonstat_validator-0.4.4/src/jsonstat_validator/models/base.py
@@ -0,0 +1,46 @@
+"""Base model for JSON-stat models."""
+
+from __future__ import annotations
+
+from pydantic import AnyUrl, BaseModel, ConfigDict, Field, RootModel, field_serializer
+
+
+class JSONStatBaseModel(BaseModel):
+    """Base model for all JSON-stat models with common configuration."""
+
+    def model_dump(
+        self, *, exclude_none: bool = True, by_alias: bool | None = True, **kwargs
+    ) -> dict:
+        """Override model_dump to set exclude_none=True by default."""
+        return super().model_dump(
+            exclude_none=exclude_none, by_alias=by_alias, **kwargs
+        )
+
+    @field_serializer("href", check_fields=False, return_type=str)
+    def serialize_any_url(self, href: AnyUrl | None) -> str | None:
+        """Convert AnyUrl to string, if it exists."""
+        return str(href) if href else None
+
+    model_config = ConfigDict(extra="forbid", serialize_by_alias=True)
+
+
+class JSONStatSchema(RootModel):
+    """JSON-stat response."""
+
+    root: Dataset | Dimension | Collection = Field(  # noqa: F821
+        ...,
+        discriminator="class_",
+    )
+
+    def model_dump(
+        self, *, exclude_none: bool = True, by_alias: bool | None = True, **kwargs
+    ) -> dict:
+        """Override model_dump to set exclude_none=True by default."""
+        return super().model_dump(
+            exclude_none=exclude_none, by_alias=by_alias, **kwargs
+        )
+
+    @field_serializer("href", check_fields=False, return_type=str)
+    def serialize_any_url(self, href: AnyUrl | None) -> str | None:
+        """Convert AnyUrl to string, if it exists."""
+        return str(href) if href else None
