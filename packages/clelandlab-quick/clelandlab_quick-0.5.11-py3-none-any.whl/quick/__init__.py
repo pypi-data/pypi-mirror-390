71ea08bf961aaec54676f986359289782b85d9df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 05:05:45 +0000

    Add package clelandlab-quick/0.5.11/clelandlab_quick-0.5.11-py3-none-any.whl

---
+++ packages/clelandlab-quick/clelandlab_quick-0.5.11-py3-none-any.whl/quick/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+def get_version():
+    with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version_file:
+        return version_file.read().strip()
+
+__version__ = get_version()
+
+from .helper import *
+from .mercator import Mercator
+from .ai import AI
+import quick.experiment as experiment
+import quick.auto as auto
