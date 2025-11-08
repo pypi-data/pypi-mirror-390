872bfce94c0469818294767386c09b7bef0ce1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 08:05:24 +0000

    Add package pytest-jsonschema/1.0.0/pytest_jsonschema-1.0.0-py3-none-any.whl

---
+++ packages/pytest-jsonschema/pytest_jsonschema-1.0.0-py3-none-any.whl/pytest_jsonschema/types.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from typing import Protocol
+
+
+StrPath = str | Path
+DataStructure = dict | list
+
+
+class FileValidator(Protocol):
+    def __call__(
+        self, path: StrPath, schema_name: str, file_type: str = ""
+    ) -> bool: ...
+
+
+class StrValidator(Protocol):
+    def __call__(self, data: str, schema_name: str, file_type: str = "") -> bool: ...
+
+
+class Validator(Protocol):
+    def __call__(self, data: DataStructure, schema_name: str) -> bool: ...
