49bed21b279d9094a0f336058dae46635d6c02b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 14:05:18 +0000

    Add package types-pytz/2025.2.0.20251108/types_pytz-2025.2.0.20251108.tar.gz

---
+++ packages/types-pytz/types_pytz-2025.2.0.20251108.tar.gz/types_pytz-2025.2.0.20251108/setup.py
@@ -0,0 +1,4 @@
+
+from setuptools import setup
+
+setup(package_data={'pytz-stubs': ['__init__.pyi', 'exceptions.pyi', 'lazy.pyi', 'reference.pyi', 'tzfile.pyi', 'tzinfo.pyi', 'METADATA.toml', 'py.typed']})
