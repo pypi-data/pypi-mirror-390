e5ba6c96a94c099e26dda6022b875d8d2557d7cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 03:19:24 +0000

    Add package basedpyright/1.33.0/basedpyright-1.33.0.tar.gz

---
+++ packages/basedpyright/basedpyright-1.33.0.tar.gz/basedpyright-1.33.0/packages/pyright-internal/src/languageService/navigationUtils.ts
@@ -0,0 +1,41 @@
+/*
+ * navigationUtils.ts
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ *
+ * Helper functions for navigating files.
+ */
+import { Location } from 'vscode-languageserver-types';
+import { DocumentRange } from '../common/docRange';
+import { ReadOnlyFileSystem } from '../common/fileSystem';
+import { Uri } from '../common/uri/uri';
+import { LanguageServerInterface } from '../common/languageServerInterface';
+
+export function canNavigateToFile(fs: ReadOnlyFileSystem, path: Uri): boolean {
+    return !fs.isInZip(path);
+}
+
+export function convertDocumentRangesToLocation(
+    ls: LanguageServerInterface,
+    fs: ReadOnlyFileSystem,
+    ranges: DocumentRange[],
+    converter: (
+        ls: LanguageServerInterface,
+        fs: ReadOnlyFileSystem,
+        range: DocumentRange
+    ) => Location | undefined = convertDocumentRangeToLocation
+): Location[] {
+    return ranges.map((range) => converter(ls, fs, range)).filter((loc) => !!loc) as Location[];
+}
+
+export function convertDocumentRangeToLocation(
+    ls: LanguageServerInterface,
+    fs: ReadOnlyFileSystem,
+    range: DocumentRange
+): Location | undefined {
+    if (!canNavigateToFile(fs, range.uri)) {
+        return undefined;
+    }
+
+    return Location.create(ls.convertUriToLspUriString(fs, range.uri), range.range);
+}
