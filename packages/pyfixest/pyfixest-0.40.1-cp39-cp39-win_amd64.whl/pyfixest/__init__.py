d031b81fe5d51585844922955693d9e5a0689a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 16:05:32 +0000

    Add package pyfixest/0.40.1/pyfixest-0.40.1-cp39-cp39-win_amd64.whl

---
+++ packages/pyfixest/pyfixest-0.40.1-cp39-cp39-win_amd64.whl/pyfixest/__init__.py
@@ -0,0 +1,69 @@
+# Import modules
+from pyfixest import (
+    did,
+    errors,
+    estimation,
+    report,
+    utils,
+)
+from pyfixest.did import (
+    SaturatedEventStudy,
+    did2s,
+    event_study,
+    lpdid,
+    panelview,
+)
+
+# Import frequently used functions and classes
+from pyfixest.estimation import (
+    bonferroni,
+    feglm,
+    feols,
+    fepois,
+    quantreg,
+    rwolf,
+    wyoung,
+)
+from pyfixest.report import coefplot, dtable, etable, iplot, make_table, qplot, summary
+from pyfixest.utils import (
+    get_data,
+    get_ssc,
+    ssc,
+)
+
+__all__ = [
+    "SaturatedEventStudy",
+    "bonferroni",
+    "coefplot",
+    "did",
+    "did2s",
+    "dtable",
+    "errors",
+    "estimation",
+    "etable",
+    "event_study",
+    "feglm",
+    "feols",
+    "fepois",
+    "get_data",
+    "get_ssc",
+    "iplot",
+    "lpdid",
+    "make_table",
+    "panelview",
+    "qplot",
+    "quantreg",
+    "report",
+    "rwolf",
+    "ssc",
+    "summary",
+    "utils",
+    "wyoung",
+]
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pyfixest")
+except PackageNotFoundError:
+    __version__ = "unknown"
---
+++ packages/pyfixest/pyfixest-0.40.1-cp39-cp39-win_amd64.whl/pyfixest/did/__init__.py
@@ -0,0 +1,11 @@
+from pyfixest.did.estimation import (
+    did2s,
+    event_study,
+    lpdid,
+)
+from pyfixest.did.saturated_twfe import SaturatedEventStudy
+from pyfixest.did.visualize import (
+    panelview,
+)
+
+__all__ = ["SaturatedEventStudy", "did2s", "event_study", "lpdid", "panelview"]
