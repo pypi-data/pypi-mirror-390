45a19ad7dc0ee83d1afb06a1ded305dae625e71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 08:05:46 +0000

    Add package jl-ecms-server/0.2.4/jl_ecms_server-0.2.4.tar.gz

---
+++ packages/jl-ecms-server/jl_ecms_server-0.2.4.tar.gz/jl_ecms_server-0.2.4/mirix/orm/enums.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class ToolType(str, Enum):
+    CUSTOM = "custom"
+    MIRIX_CORE = "mirix_core"
+    MIRIX_CODER_CORE = "mirix_coder_core"
+    MIRIX_MEMORY_CORE = "mirix_memory_core"
+    MIRIX_EXTRA = "mirix_extra"
+    MIRIX_MCP = "mirix_mcp"
+    MIRIX_MULTI_AGENT_CORE = "mirix_multi_agent_core"
+    USER_DEFINED = "user_defined"
+
+
+class JobType(str, Enum):
+    JOB = "job"
+    RUN = "run"
+
+
+class ToolSourceType(str, Enum):
+    """Defines what a tool was derived from"""
+
+    python = "python"
+    json = "json"
