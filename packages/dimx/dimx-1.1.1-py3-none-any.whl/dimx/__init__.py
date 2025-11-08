ed59561b74d173920e03be0047b1010cf2c7172a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-08 03:19:43 +0000

    Add package dimx/1.1.1/dimx-1.1.1-py3-none-any.whl

---
+++ packages/dimx/dimx-1.1.1-py3-none-any.whl/dimx/__init__.py
@@ -0,0 +1,9 @@
+'''dimx package'''
+
+# import local modules
+from .MDE             import MDE
+from .CrossMapColumns import CrossMapColumns
+from .Evaluate        import Evaluate
+
+__version__ = "1.1.1"
+__date__    = "2025-11-07"
