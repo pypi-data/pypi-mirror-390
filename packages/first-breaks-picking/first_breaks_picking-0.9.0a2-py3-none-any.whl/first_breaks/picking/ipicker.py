4dc8c4cd49ab8b289e1ccbf241f5b4d314998e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:17 +0000

    Add package first-breaks-picking/0.9.0a2/first_breaks_picking-0.9.0a2-py3-none-any.whl

---
+++ packages/first-breaks-picking/first_breaks_picking-0.9.0a2-py3-none-any.whl/first_breaks/picking/ipicker.py
@@ -0,0 +1,39 @@
+from typing import Any, Optional
+
+from tqdm.auto import tqdm
+
+from first_breaks.picking.task import Task
+
+
+class IPicker:
+    def __init__(self, show_progressbar: bool = True, *args: Any, **kwargs: Any):
+        self.show_progressbar = show_progressbar
+        self.progressbar: Optional[tqdm] = None
+        self.need_interrupt = False
+
+    def change_settings(self, *args: Any, **kwargs: Any) -> None:
+        pass
+
+    def process_task(self, task: Task) -> Task:
+        raise NotImplementedError
+
+    def callback_interrupt(self) -> None:
+        self.need_interrupt = True
+
+    def interrupt_if_need(self) -> None:
+        if self.need_interrupt:
+            self.need_interrupt = False
+            raise InterruptedError("Processing interrupted")
+
+    def callback_processing_started(self, length: int) -> Any:
+        if self.show_progressbar:
+            self.progressbar = tqdm(desc="Picking", total=length)
+
+    def callback_processing_finished(self) -> Any:
+        if self.show_progressbar:
+            self.progressbar.close()
+
+    def callback_step_finished(self, finished_step: int) -> Any:
+        if self.show_progressbar:
+            interval = finished_step - self.progressbar.n
+            self.progressbar.update(interval)
