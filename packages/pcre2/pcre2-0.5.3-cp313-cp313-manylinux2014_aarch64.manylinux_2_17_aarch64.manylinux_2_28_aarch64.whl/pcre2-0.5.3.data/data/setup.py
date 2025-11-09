9d275bb70427cb61e3dd86725efaf2b900f6e255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:16 +0000

    Add package pcre2/0.5.3/pcre2-0.5.3-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/pcre2/pcre2-0.5.3-cp313-cp313-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/pcre2-0.5.3.data/data/setup.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+
+import os
+import skbuild
+import setuptools
+
+
+def get_long_desciption():
+    cwd = os.path.abspath(os.path.dirname(__file__))
+    filename = os.path.join(cwd, "README.md")
+    with open(filename) as f:
+        long_description = f.read()
+
+    return long_description
+
+
+skbuild.setup(
+    name = "pcre2",
+    version = "0.5.3",
+    description = "Python bindings for the PCRE2 regular expression library",
+    long_description = get_long_desciption(),
+    long_description_content_type = "text/markdown",
+    license = "BSD 3-Clause License",
+    author = "Garrett Tetrault",
+    url = "https://github.com/grtetrault/pcre2.py",
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: C",
+        "Programming Language :: Cython",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows"
+    ],
+    include_package_data=True,
+    packages = setuptools.find_packages("src"),
+    package_dir = {"": "src"},
+    cmake_languages = "C",
+)
