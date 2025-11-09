0bfc73fc8ad8236825113ba879dd507edfba09ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:11 +0000

    Add package apppy-logger/0.9.0/apppy_logger-0.9.0.tar.gz

---
+++ packages/apppy-logger/apppy_logger-0.9.0.tar.gz/apppy_logger-0.9.0/src/apppy/logger/__init__.py
@@ -0,0 +1,59 @@
+import logging
+
+from apppy.logger.filter import SuppressApiLoggingFilter
+from apppy.logger.format import ExtraLoggingFormatter
+from apppy.logger.parser import LogRecordParser
+from apppy.logger.storage import LoggingStorage
+
+
+def bootstrap_global_logging(level):
+    """
+    Utility to apply custom logging configuration
+    to application loggers. It should be called very
+    early in the application boot process.
+    """
+    if isinstance(level, str):
+        level = logging._nameToLevel[level.upper()]
+
+    logging.root.setLevel(level)
+
+    logging.getLogger("httpcore").setLevel(logging.INFO)
+    logging.getLogger("httpcore.connection").setLevel(logging.INFO)
+    logging.getLogger("httpcore.http11").setLevel(logging.INFO)
+
+    # Setup global singletons
+    LogRecordParser.set_global()
+    LoggingStorage.set_global()
+    # Apply formatters to all loggers so that their
+    # output is uniform
+    ExtraLoggingFormatter.apply(logging.root)
+    ExtraLoggingFormatter.apply_all()
+    # Setup the filter to add Logging Storage state
+    # to each log record
+    LoggingStorage.apply_all()
+    # A /health endpoint is typically called a lot in deployed environments
+    # as part of load balancer checks. This bloats the application logs
+    # so this filter allows health checks to be skipped in logs.
+    SuppressApiLoggingFilter.apply(logging.getLogger("uvicorn.access"), "/health")
+
+
+class WithLogger:
+    """
+    Class decorator that injects a _logger object using a very standard
+    naming convention. The convention is based on the class name so that
+    log lines can be easily associated with a given class. For example:
+
+    class MyService(WithLogger):
+        ...
+        def do_something(self):
+            # This logger will be named with the module and class names
+            # e.g. 'module.path.to.MyService'
+            self._logger.info("I am doing something")
+        ...
+    """
+
+    _logger: logging.Logger
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls._logger = logging.getLogger(f"{cls.__module__}.{cls.__qualname__}")
