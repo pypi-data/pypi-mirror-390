cbdb133fb1cd4c945c3c28770bf435f9d46f0a24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:08 +0000

    Add package akipy/1.3.5/akipy-1.3.5.tar.gz

---
+++ packages/akipy/akipy-1.3.5.tar.gz/akipy-1.3.5/akipy/dicts.py
@@ -0,0 +1,63 @@
+HEADERS = {
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) snap Chromium/81.0.4044.92 "
+    "Chrome/81.0.4044.92 Safari/537.36",
+    "x-requested-with": "XMLHttpRequest",
+}
+
+LANG_MAP = {
+    "english": "en",
+    "arabic": "ar",
+    "chinese": "cn",
+    "german": "de",
+    "spanish": "es",
+    "french": "fr",
+    "hebrew": "il",
+    "italian": "it",
+    "japanese": "jp",
+    "korean": "kr",
+    "dutch": "nl",
+    "polish": "pl",
+    "portuguese": "pt",
+    "russian": "ru",
+    "turkish": "tr",
+    "indonesian": "id",
+}
+
+THEME_ID = {"c": 1, "a": 14, "o": 2}
+
+"""
+c - characters
+a - animals
+o - objects
+"""
+
+THEMES = {
+    "en": ["c", "a", "o"],
+    "ar": ["c"],
+    "cn": ["c"],
+    "de": ["c", "a"],
+    "es": ["c", "a"],
+    "fr": ["c", "a", "o"],
+    "il": ["c"],
+    "it": ["c", "a"],
+    "jp": ["c", "a"],
+    "kr": ["c"],
+    "nl": ["c"],
+    "pl": ["c"],
+    "pt": ["c"],
+    "ru": ["c"],
+    "tr": ["c"],
+    "id": ["c"],
+}
+
+ANSWERS = {
+    0: ["yes", "y", "0"],
+    1: ["no", "n", "1"],
+    2: ["i", "idk", "i dont know", "i don't know", "2"],
+    3: ["p", "probably", "3"],
+    4: ["pn", "probably not", "4"],
+}
+ANSWER_MAP = {a: key for key, values in ANSWERS.items() for a in values}
