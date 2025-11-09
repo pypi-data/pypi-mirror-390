c05ba1ffc3a570d284be53f94f98640046032e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:08 +0000

    Add package osonbot/1.1.8/osonbot-1.1.8-py3-none-any.whl

---
+++ packages/osonbot/osonbot-1.1.8-py3-none-any.whl/osonbot/__init__.py
@@ -0,0 +1,9 @@
+from .bot import Bot, Photo, Video, Audio, Voice, Sticker, Document, KeyboardButton, RemoveKeyboardButton, InlineKeyboardButton, URLKeyboardButton
+from . import botbuilder
+
+__all__ = [
+    "Bot",
+    "Photo", "Video", "Audio", "Voice", "Sticker", "Document",
+    "KeyboardButton", "RemoveKeyboardButton", "InlineKeyboardButton", "URLKeyboardButton",
+    "botbuilder"
+]
