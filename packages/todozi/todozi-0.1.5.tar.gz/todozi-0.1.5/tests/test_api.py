489187eb5816e886a2b3625367ee12839ad36b91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 14:06:38 +0000

    Add package todozi/0.1.5/todozi-0.1.5.tar.gz

---
+++ packages/todozi/todozi-0.1.5.tar.gz/todozi-0.1.5/tests/test_api.py
@@ -0,0 +1,201 @@
+"""
+Tests for api module.
+Auto-generated test file.
+"""
+
+import pytest
+
+import todozi.api  # noqa: E402
+
+# Import available items from module
+try:
+    from todozi.api import ApiKey, ApiKeyCollection, ApiKeyManager, TodoziError, activate_api_key, activate_key, add_key, check_api_key_auth, create_api_key, create_api_key_with_user_id, deactivate_api_key, deactivate_key, get_active_keys, get_all_keys, get_api_key, get_api_key_by_public, get_key, get_key_by_public, get_storage_dir, is_admin
+    from todozi.api import list_active_api_keys, list_api_keys, load_api_key_collection, matches, new, remove_api_key, remove_key, save_api_key_collection, with_user_id
+except ImportError:
+    # Some items may not be available, import module instead
+    import todozi.api as api
+
+# ========== Class Tests ==========
+
+def test_apikey_creation():
+    """Test ApiKey class creation."""
+    # TODO: Implement test
+    pass
+
+
+def test_apikeycollection_creation():
+    """Test ApiKeyCollection class creation."""
+    # TODO: Implement test
+    pass
+
+
+def test_apikeymanager_creation():
+    """Test ApiKeyManager class creation."""
+    # TODO: Implement test
+    pass
+
+
+def test_todozierror_creation():
+    """Test TodoziError class creation."""
+    # TODO: Implement test
+    pass
+
+
+# ========== Function Tests ==========
+
+def test_activate_api_key():
+    """Test activate_api_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_activate_key():
+    """Test activate_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_add_key():
+    """Test add_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_check_api_key_auth():
+    """Test check_api_key_auth function."""
+    # TODO: Implement test
+    pass
+
+
+def test_create_api_key():
+    """Test create_api_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_create_api_key_with_user_id():
+    """Test create_api_key_with_user_id function."""
+    # TODO: Implement test
+    pass
+
+
+def test_deactivate_api_key():
+    """Test deactivate_api_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_deactivate_key():
+    """Test deactivate_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_active_keys():
+    """Test get_active_keys function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_all_keys():
+    """Test get_all_keys function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_api_key():
+    """Test get_api_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_api_key_by_public():
+    """Test get_api_key_by_public function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_key():
+    """Test get_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_key_by_public():
+    """Test get_key_by_public function."""
+    # TODO: Implement test
+    pass
+
+
+def test_get_storage_dir():
+    """Test get_storage_dir function."""
+    # TODO: Implement test
+    pass
+
+
+def test_is_admin():
+    """Test is_admin function."""
+    # TODO: Implement test
+    pass
+
+
+def test_list_active_api_keys():
+    """Test list_active_api_keys function."""
+    # TODO: Implement test
+    pass
+
+
+def test_list_api_keys():
+    """Test list_api_keys function."""
+    # TODO: Implement test
+    pass
+
+
+def test_load_api_key_collection():
+    """Test load_api_key_collection function."""
+    # TODO: Implement test
+    pass
+
+
+def test_matches():
+    """Test matches function."""
+    # TODO: Implement test
+    pass
+
+
+def test_new():
+    """Test new function."""
+    # TODO: Implement test
+    pass
+
+
+def test_remove_api_key():
+    """Test remove_api_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_remove_key():
+    """Test remove_key function."""
+    # TODO: Implement test
+    pass
+
+
+def test_save_api_key_collection():
+    """Test save_api_key_collection function."""
+    # TODO: Implement test
+    pass
+
+
+def test_with_user_id():
+    """Test with_user_id function."""
+    # TODO: Implement test
+    pass
+
+
+# ========== Integration Tests ==========
+
+def test_module_import():
+    """Test that the module can be imported."""
+    import todozi.api as mod
+    assert mod is not None
