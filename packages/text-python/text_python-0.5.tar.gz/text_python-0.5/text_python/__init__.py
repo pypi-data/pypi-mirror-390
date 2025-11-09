5619cef6df66fbdad3fc328f0bd0339c3cd3e736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:01 +0000

    Add package text-python/0.5/text_python-0.5.tar.gz

---
+++ packages/text-python/text_python-0.5.tar.gz/text_python-0.5/text_python/__init__.py
@@ -0,0 +1,5 @@
+# Import toàn bộ cho người dùng
+from text_python.core import *
+
+# Thay đổi cú pháp import * thành import all
+all = globals()
---
+++ packages/text-python/text_python-0.5.tar.gz/text_python-0.5/text_python/__main__.py
@@ -0,0 +1,8 @@
+from text_python import all
+
+def main():
+    all.in_ra("Xin chao tu text_python 0.5!")
+
+if __name__ == "__main__":
+    main()
+
