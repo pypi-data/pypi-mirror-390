b9bd4e2a203bb48e2056c9e80e1bad15d3d9e706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:14 +0000

    Add package xpander-sdk/2.0.90/xpander_sdk-2.0.90-py3-none-any.whl

---
+++ packages/xpander-sdk/xpander_sdk-2.0.90-py3-none-any.whl/xpander_sdk/modules/backend/frameworks/dispatch.py
@@ -0,0 +1,33 @@
+from typing import Any, Callable, Dict, List, Optional
+
+from xpander_sdk.models.frameworks import Framework
+from xpander_sdk.modules.agents.sub_modules.agent import Agent
+from xpander_sdk.modules.tasks.sub_modules.task import Task
+
+async def dispatch_get_args(
+    agent: Agent,
+    task: Optional[Task] = None,
+    override: Optional[Dict[str, Any]] = None,
+    tools: Optional[List[Callable]] = None,
+) -> Dict[str, Any]:
+    """
+    Dispatch to the correct framework-specific argument resolver.
+
+    Args:
+        agent (Agent): The agent to generate args for.
+        task (Optional[Task]): Optional runtime task.
+        override (Optional[Dict[str, Any]]): Dict of override values.
+        tools (Optional[List[Callable]]): Optional additional tools to be added to the agent arguments.
+
+    Returns:
+        Dict[str, Any]: Arguments for instantiating the framework agent.
+    """
+    match agent.framework:
+        case Framework.Agno:
+            from .agno import build_agent_args
+            return await build_agent_args(xpander_agent=agent, task=task, override=override, tools=tools)
+        # case Framework.Langchain: # PLACEHOLDER
+        #     from .langchain import build_agent_args
+        #     return await build_agent_args(xpander_agent=agent, task=task, override=override)
+        case _:
+            raise NotImplementedError(f"Framework '{agent.framework.value}' is not supported.")
