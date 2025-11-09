c2620384be89a7df55a2f4dbd8442d15b4a1b25d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 05:05:40 +0000

    Add package mactools/1.8.0/mactools-1.8.0-py3-none-any.whl

---
+++ packages/mactools/mactools-1.8.0-py3-none-any.whl/mactools/macaddress.py
@@ -0,0 +1,37 @@
+# MacTools MAC Address Class
+
+# Python Modules
+from typing import Union
+
+# Local Modules
+from mactools.basemac import BaseMac, MacNotation
+
+
+class MacAddress(BaseMac):
+    """
+    Final class that merges `OUICache` instance into the `BaseMac` for
+    look-ups automatically on creation and prevents circular dependencies.
+    """
+
+    def __init__(
+        self,
+        mac: Union[str, int],
+        format: MacNotation = MacNotation.COLON,
+        *args,
+        **kwargs,
+    ):
+
+        # Keyword arguments for cache instances to override default global cache
+        input_cache = None
+        for keyword in ["cache", "oui_cache"]:
+            input_cache = kwargs.get(keyword)
+            if input_cache:
+                break
+
+        if input_cache is None:
+            from mactools.oui_cache import get_oui_cache
+
+            oui_cache = get_oui_cache()
+
+        init_cache = oui_cache if input_cache is None else input_cache
+        super().__init__(mac, format, oui_cache=init_cache)
---
+++ packages/mactools/mactools-1.8.0-py3-none-any.whl/mactools/oui_cache/oui_core.py
@@ -0,0 +1,32 @@
+# OUI Database
+
+# Python Modules
+from typing import Dict, Optional
+
+# Local Modules
+from mactools.oui_cache.oui_classes import OUICache
+from mactools.oui_cache.oui_common import create_oui_dict
+
+
+def get_oui_cache(regenerate: bool = False) -> OUICache:
+    """
+    Gets the IEEE OUI info, creates, and pickles the cache
+    """
+    if OUICache._instance is not None and not regenerate:
+        return OUICache._instance
+
+    return OUICache(create_oui_dict())
+
+
+def get_oui_record(input_mac: str) -> Optional[Dict[str, str]]:
+    """
+    Gets the record of a MAC or OUI
+    """
+    return get_oui_cache().get_record(input_mac)
+
+
+def get_oui_vendor(input_mac: str) -> str:
+    """
+    Gets the vendor names of a MAC or OUI
+    """
+    return get_oui_cache().get_vendor(input_mac)
