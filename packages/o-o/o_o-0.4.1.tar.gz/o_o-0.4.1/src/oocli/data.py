8765dc31335a7e125c7c9000eb8f8a4b660766a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:02 +0000

    Add package o-o/0.4.1/o_o-0.4.1.tar.gz

---
+++ packages/o-o/o_o-0.4.1.tar.gz/o_o-0.4.1/src/oocli/data.py
@@ -0,0 +1,45 @@
+"""Common data classes"""
+
+from pydantic import BaseModel, ByteSize, constr
+from pydantic_extra_types.pendulum_dt import DateTime
+
+
+class DataStore(BaseModel):
+    provider: constr(to_lower=True)
+    bucket: str
+    region: str | None = None
+    default: bool = False
+
+
+class Environment(BaseModel):
+    provider: constr(to_lower=True)
+    image: str
+    machinetype: str
+    region: str
+    default: bool = False
+    size: ByteSize = ByteSize(20_000_000_000)
+
+
+class LogMessage(BaseModel):
+    message: str
+    timestamp: DateTime
+
+
+class Run(BaseModel):
+    command: list[str]
+    commit_sha: str | None = None
+    creator: str
+    datastore: DataStore
+    environment: Environment
+    message: str
+    project: str
+    started: DateTime
+    ended: DateTime | None = None
+    exit_status: int | None = None
+    sha: str | None = None
+    tags: list[str]
+    inputs: list["Run"] | None = None
+
+    @property
+    def short_sha(self):
+        return self.sha[:10]
