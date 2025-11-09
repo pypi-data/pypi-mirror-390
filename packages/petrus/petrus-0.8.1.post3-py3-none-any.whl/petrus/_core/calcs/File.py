2e3cafb0c82134ada1f4c1e071a55af7f9cb4d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 05:05:34 +0000

    Add package petrus/0.8.1.post3/petrus-0.8.1.post3-py3-none-any.whl

---
+++ packages/petrus/petrus-0.8.1.post3-py3-none-any.whl/petrus/_core/calcs/File.py
@@ -0,0 +1,60 @@
+import os
+from typing import *
+
+from petrus._core import utils
+from petrus._core.calcs.Calc import Calc
+
+
+class File(Calc):
+    def _calc_core(self: Self) -> Any:
+        n = self.prog.project.name
+        return os.path.join("src", n, "core", "__init__.py")
+
+    def _calc_gitignore(self: Self) -> str:
+        return ".gitignore"
+
+    def _calc_license(self: Self) -> Any:
+        ans = self.prog.pp.get("project", "license", "file")
+        if type(ans) is str:
+            return ans
+        return self._find("LICENSE.txt")
+
+    def _calc_main(self: Self) -> Any:
+        n = self.prog.project.name
+        return os.path.join("src", n, "__main__.py")
+
+    def _calc_init(self: Self) -> Any:
+        n = self.prog.project.name
+        return os.path.join("src", n, "__init__.py")
+
+    def _calc_manifest(self: Self) -> Any:
+        return "MANIFEST.in"
+
+    def _calc_pp(self: Self) -> Any:
+        return "pyproject.toml"
+
+    def _calc_readme(self: Self) -> Any:
+        ans = self.prog.pp.get("project", "readme")
+        if type(ans) is str and os.path.exists(ans):
+            return ans
+        return self._find("README.rst")
+
+    def _calc_setup(self: Self) -> str:
+        return "setup.cfg"
+
+    def exists(self: Self, name: Any) -> bool:
+        f = getattr(self, name)
+        return os.path.exists(f)
+
+    @staticmethod
+    def _find(file: Any):
+        if utils.isfile(file):
+            return file
+        t = os.path.splitext(file)[0]
+        l = os.listdir()
+        l = list(l)
+        l.sort(reverse=True)
+        for x in l:
+            if t == os.path.splitext(x)[0]:
+                return x
+        return file
