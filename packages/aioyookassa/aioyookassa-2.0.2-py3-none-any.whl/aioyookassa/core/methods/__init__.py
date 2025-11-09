ecb79fbd8b86cf6fce2fc1bdff8e5846fb57e8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:22 +0000

    Add package aioyookassa/2.0.2/aioyookassa-2.0.2-py3-none-any.whl

---
+++ packages/aioyookassa/aioyookassa-2.0.2-py3-none-any.whl/aioyookassa/core/methods/__init__.py
@@ -0,0 +1,34 @@
+from .base import APIMethod, BaseAPIMethod
+from .invoices import CreateInvoice, GetInvoice
+from .me import GetMe
+from .payment_methods import CreatePaymentMethod, GetPaymentMethod
+from .payments import (
+    CancelPayment,
+    CapturePayment,
+    CreatePayment,
+    GetPayment,
+    GetPayments,
+)
+from .receipts import CreateReceipt, GetReceipt, GetReceipts
+from .refunds import CreateRefund, GetRefund, GetRefunds
+
+__all__ = [
+    "APIMethod",
+    "BaseAPIMethod",
+    "CreatePayment",
+    "GetPayments",
+    "GetPayment",
+    "CapturePayment",
+    "CancelPayment",
+    "CreatePaymentMethod",
+    "GetPaymentMethod",
+    "CreateInvoice",
+    "GetInvoice",
+    "CreateRefund",
+    "GetRefunds",
+    "GetRefund",
+    "CreateReceipt",
+    "GetReceipts",
+    "GetReceipt",
+    "GetMe",
+]
