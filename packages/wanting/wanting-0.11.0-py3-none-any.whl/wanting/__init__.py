591bec9432f9cc70e863e4bcfd0bb49da0e27a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:06:58 +0000

    Add package wanting/0.11.0/wanting-0.11.0-py3-none-any.whl

---
+++ packages/wanting/wanting-0.11.0-py3-none-any.whl/wanting/__init__.py
@@ -0,0 +1,32 @@
+"""The Wanting package."""
+
+from wanting.wanting import (
+    FieldInfoEx,
+    IncExMapping,
+    Json,
+    Unavailable,
+    Unmapped,
+    Wanting,
+    WantingValues,
+    wanting_fields,
+    wanting_incex,
+    wanting_values,
+)
+
+__authors__ = ["Narvin Singh"]
+__project__ = "Wanting"
+__version__ = "0.11.0"
+
+__all__ = [
+    "FieldInfoEx",
+    "IncExMapping",
+    "Json",
+    "Unavailable",
+    "Unmapped",
+    "Wanting",
+    "WantingValues",
+    "__version__",
+    "wanting_fields",
+    "wanting_incex",
+    "wanting_values",
+]
