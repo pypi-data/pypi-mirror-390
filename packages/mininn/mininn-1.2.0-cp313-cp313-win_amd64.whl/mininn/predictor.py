f76f09d92003f6b7f5ce1068e5ebe5b64fcc9c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 14:06:50 +0000

    Add package mininn/1.2.0/mininn-1.2.0-cp313-cp313-win_amd64.whl

---
+++ packages/mininn/mininn-1.2.0-cp313-cp313-win_amd64.whl/mininn/predictor.py
@@ -0,0 +1,29 @@
+from mininn import mininn_capi
+
+
+class Predictor:
+    def __init__(self, filename):
+        graph = mininn_capi.Graph()
+        mininn_capi.load_model(filename, graph)
+        self.Predictor = mininn_capi.Predictor(graph)
+
+    def get_input(self):
+        inputs = self.Predictor.get_input_tensors()
+        return inputs
+
+    def set_data(self, data):
+        input = self.Predictor.get_input_tensors()
+        for i in range(0, len(input)):
+            input[i].set_data(data[i])
+
+    def run(self):
+        self.Predictor.prepare()
+        self.Predictor.run()
+
+    def get_output(self):
+        outputs = self.Predictor.get_output_tensors()
+        return outputs
+
+    def dump_all_outputs(self):
+        outputs = self.Predictor.dump_all_outputs()
+        return outputs
