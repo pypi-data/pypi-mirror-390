0816497f762a284f633fdfa7a5976d6a482da76c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:05 +0000

    Add package zedline/1.0.0/zedline-1.0.0.tar.gz

---
+++ packages/zedline/zedline-1.0.0.tar.gz/zedline-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="zedline",
+    version="1.0.0",
+    author="ZedLine Co.",
+    author_email="no-reply@zedline.ir",
+    description="ZedLine: The first Iranian free web service toolkit for developers",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://zedline.ir",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
