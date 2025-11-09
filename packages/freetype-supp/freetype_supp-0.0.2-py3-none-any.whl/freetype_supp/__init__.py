d14da1544ef96e7221f2e7f768012598e416c25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:07 +0000

    Add package freetype-supp/0.0.2/freetype_supp-0.0.2-py3-none-any.whl

---
+++ packages/freetype-supp/freetype_supp-0.0.2-py3-none-any.whl/freetype_supp/__init__.py
@@ -0,0 +1,9 @@
+__all__ = [
+    "truetype_engine_type",
+    "get_truetype_engine_type",
+    "load_sfnt_table",
+]
+from .get_truetype_engine_type import (
+    truetype_engine_type, get_truetype_engine_type
+)
+from .load_sfnt_table import load_sfnt_table
