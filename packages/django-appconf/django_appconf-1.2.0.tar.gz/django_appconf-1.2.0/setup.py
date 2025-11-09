85756dcaffa6667da252c29463a78049142635df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:16 +0000

    Add package django-appconf/1.2.0/django_appconf-1.2.0.tar.gz

---
+++ packages/django-appconf/django_appconf-1.2.0.tar.gz/django_appconf-1.2.0/setup.py
@@ -0,0 +1,57 @@
+import codecs
+import re
+from os import path
+from setuptools import setup
+
+
+def read(*parts):
+    file_path = path.join(path.dirname(__file__), *parts)
+    return codecs.open(file_path, encoding='utf-8').read()
+
+
+def find_version(*parts):
+    version_file = read(*parts)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name='django-appconf',
+    version=find_version('appconf', '__init__.py'),
+    description='A helper class for handling configuration defaults '
+                'of packaged apps gracefully.',
+    long_description=read('README.rst'),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    license='BSD',
+    url='https://django-appconf.readthedocs.io/',
+    packages=['appconf'],
+    python_requires='>=3.9',
+    install_requires=['django'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
+        'Framework :: Django :: 6.0',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Utilities',
+    ],
+    project_urls={
+        'Source': 'https://github.com/django-compressor/django-appconf',
+    }
+)
