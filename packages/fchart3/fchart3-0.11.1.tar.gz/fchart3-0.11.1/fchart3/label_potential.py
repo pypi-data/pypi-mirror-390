e8735d28eaf319802839258fd8b136ac6af8da1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:33 +0000

    Add package fchart3/0.11.1/fchart3-0.11.1.tar.gz

---
+++ packages/fchart3/fchart3-0.11.1.tar.gz/fchart3-0.11.1/fchart3/label_potential.py
@@ -0,0 +1,64 @@
+#    fchart3 draws beautiful deepsky charts in vector formats
+#    Copyright (C) 2005-2025 fchart3 authors
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+import math
+
+import numpy as np
+
+
+class LabelPotential:
+    def __init__(self, field_radius):
+        """
+        field_radius in mm
+        deepskylist [(x,y,size), (x,y,size),...]
+        x,y, size in mm
+        """
+        self.field_radius  = field_radius
+        self.positions = np.empty((0, 2))
+        self.sizes = np.empty(0)
+
+    def add_deepsky_list(self, deepskylist):
+        N = len(self.sizes)
+        newpos = np.zeros((N+len(deepskylist),2))
+        newpos[0:N,:] = self.positions
+        newsize = np.zeros(N+len(deepskylist))
+        newsize[0:N] = self.sizes
+
+        for i in range(len(deepskylist)):
+            _, x,y,s = deepskylist[i]
+            if s <= 0:
+                s = 1
+            newsize[N + i] = math.sqrt(s)
+            newpos[N + i,:] = [x, y]
+
+        self.positions = newpos
+        self.sizes = newsize
+
+    def add_position(self, x, y, size):
+        self.positions = np.append(self.positions, [[x, y]], axis=0)
+        self.sizes = np.append(self.sizes, math.sqrt(size))
+
+    def compute_potential(self, x, y, edge_opt=False):
+        r2 = (self.positions[:,0]-x)**2 + (self.positions[:,1]-y)**2
+        sr = (r2+0.1)**(-1)
+        p = self.sizes*sr
+        if edge_opt:
+            ss = np.sum(self.sizes)
+            rf = ((x**2+y**2)**0.5 - self.field_radius)**-3
+            value = np.sum(p) + ss*rf
+        else:
+            value = np.sum(p)
+        return value
---
+++ packages/fchart3/fchart3-0.11.1.tar.gz/fchart3-0.11.1/fchart3/widgets/widget_map_scale.py
@@ -0,0 +1,85 @@
+#    fchart3 draws beautiful deepsky charts in vector formats
+#    Copyright (C) 2005-2025 fchart3 authors
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+import numpy as np
+
+from .widget_base import WidgetBase
+from ..graphics import DrawMode
+
+
+class WidgetMapScale(WidgetBase):
+
+    def __init__(self, sky_map_engine, alloc_space_spec, drawingscale, maxlength, legend_fontsize, legend_linewidth, color=(0, 0, 0)):
+        super().__init__(sky_map_engine=sky_map_engine, alloc_space_spec=alloc_space_spec)
+        self.drawingscale = drawingscale
+        self.maxlength = maxlength
+        self.legend_fontsize = legend_fontsize
+        self.legend_linewidth = legend_linewidth
+        self.alloc_space_spec = alloc_space_spec
+        self.color = color
+        self._initialize()
+
+    def _initialize(self):
+        # Determine a suitable scale ruler
+        allowed_ruler = np.array([1, 5, 10, 30, 60, 120, 300.0, 600.0, 1200.0]) # arcminutes
+        allowed_labels = ['1\'', '5\'', '10\'', '30\'', '1°', '2°', '5°', '10°', '20°']
+
+        ruler_mm = allowed_ruler*np.pi/(180.0*60.0)*self.drawingscale
+        self.ruler_label = ''
+        self.ruler_length = 0.0
+
+        for i in range(len(allowed_ruler)):
+            if ruler_mm[-(i+1)] <= self.maxlength:
+                self.ruler_label = allowed_labels[-(i+1)]
+                self.ruler_length = ruler_mm[-(i+1)]
+                break
+
+        fh = self.legend_fontsize * 0.66
+        self.width, self.height = self.ruler_length + 2*fh, 2.2 * self.legend_fontsize
+
+    def draw(self, graphics, fill_background):
+        if self.x is None or self.y is None:
+            return
+        fh = self.legend_fontsize * 0.66
+
+        x = self.x + self.width - fh
+        y = self.y - self.height + fh + fh/2
+
+        graphics.set_solid_line()
+        graphics.set_pen_rgb(self.color)
+        graphics.set_linewidth(self.legend_linewidth)
+
+        lw = graphics.gi_linewidth
+
+        if fill_background and graphics.gi_background_rgb:
+            graphics.save()
+            graphics.set_fill_background()
+            graphics.rectangle(self.x, self.y, self.width, self.height, DrawMode.FILL)
+            graphics.restore()
+
+        graphics.line(x, y, x - self.ruler_length, y)
+        graphics.line(x - lw/2.0, y - 0.5*fh,
+                      x - lw/2.0, y + 0.5*fh)
+        graphics.line(x - self.ruler_length + lw/2.0, y - 0.5*fh,
+                      x - self.ruler_length + lw/2.0, y + 0.5*fh)
+        old_fontsize = graphics.gi_font_size
+        graphics.set_font(graphics.gi_font, fh)
+        graphics.text_centred(x - self.ruler_length/2.0, y + graphics.gi_font_size*2/3.0, self.ruler_label)
+
+        graphics.set_font(graphics.gi_font, old_fontsize)
+
+        self.draw_bounding_rect(graphics)
