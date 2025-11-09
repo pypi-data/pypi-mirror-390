708de0a700cf753349a40133c960cf8dc277e24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:01 +0000

    Add package text-python/0.6/text_python-0.6-py3-none-any.whl

---
+++ packages/text-python/text_python-0.6-py3-none-any.whl/text_python/__init__.py
@@ -0,0 +1,2 @@
+# Import tất cả từ core để người dùng dùng trực tiếp
+from text_python.core import *
---
+++ packages/text-python/text_python-0.6-py3-none-any.whl/text_python/__main__.py
@@ -0,0 +1,13 @@
+from text_python import *
+
+def main():
+    in_ra("Xin chao tu text_python 0.5!");
+    code = """
+int x = 5;
+for (int i=0; i<x; i++)
+    in_ra(i);
+"""
+    chay_code_c_gian_don(code);
+
+if __name__ == "__main__":
+    main();
