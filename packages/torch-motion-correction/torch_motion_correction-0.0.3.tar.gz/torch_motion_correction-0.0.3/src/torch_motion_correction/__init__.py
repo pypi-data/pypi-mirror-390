af27a214bac0d7827de93844c09d72f61446e8f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 05:05:37 +0000

    Add package torch-motion-correction/0.0.3/torch_motion_correction-0.0.3.tar.gz

---
+++ packages/torch-motion-correction/torch_motion_correction-0.0.3.tar.gz/torch_motion_correction-0.0.3/src/torch_motion_correction/__init__.py
@@ -0,0 +1,40 @@
+"""Motion estimation and correction in PyTorch."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("torch-motion-correction")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Alister Burt"
+__email__ = "alisterburt@gmail.com"
+
+from torch_motion_correction.correct_motion import (
+    correct_motion,
+    correct_motion_fast,
+    correct_motion_slow,
+)
+from torch_motion_correction.data_io import (
+    read_deformation_field_from_csv,
+    write_deformation_field_to_csv,
+)
+from torch_motion_correction.deformation_field_utils import (
+    evaluate_deformation_field,
+)
+from torch_motion_correction.estimate_motion_xc import (
+    estimate_global_motion,
+    estimate_motion_cross_correlation_patches,
+)
+from torch_motion_correction.estimate_motion_optimizer import estimate_local_motion
+
+__all__ = [
+    "estimate_local_motion",
+    "correct_motion",
+    "correct_motion_fast",
+    "correct_motion_slow",
+    "evaluate_deformation_field",
+    "estimate_global_motion",
+    "estimate_motion_cross_correlation_patches",
+    "write_deformation_field_to_csv",
+    "read_deformation_field_from_csv",
+]
