711dd527dabf10f59de74dc6c33a06928101d2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 05:05:39 +0000

    Add package maxclientapi/2.7.0/maxclientapi-2.7.0.tar.gz

---
+++ packages/maxclientapi/maxclientapi-2.7.0.tar.gz/maxclientapi-2.7.0/README.md
@@ -0,0 +1,21 @@
+# maxclientapi
+A Python library for working with the 'MAX Messenger' WebSocket API
+
+## Install the library
+```bash
+pip install maxclientapi
+```
+
+## Example code
+[Example code](example_code/example.py)
+
+## How to get token and deviceId?
+Go to https://web.max.ru, press F12 and find the websocket 
+![Step 1](.readme_photos/1.png)
+then go to messages 
+![Step 2](.readme_photos/2.png)
+and find the first two messages 
+![Step 3](.readme_photos/3.png)
+then do as shown in these two photos to copy the values 
+![Step 4](.readme_photos/4.png)
+![Step 5](.readme_photos/5.png)
---
+++ packages/maxclientapi/maxclientapi-2.7.0.tar.gz/maxclientapi-2.7.0/setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="maxclientapi",
+    version="2.7.0",
+    packages=find_packages(),
+    install_requires=["websocket-client"], 
+    author="arsrus721",
+    description="A Python library for working with the 'MAX Messenger' WebSocket API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/arsrus721/maxclientapi", 
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
+
+
+
+
