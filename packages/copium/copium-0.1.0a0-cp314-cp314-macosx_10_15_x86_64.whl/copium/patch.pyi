0920604db97b1d85ee3d26e07fb0b30e684d28a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:06:55 +0000

    Add package copium/0.1.0a0/copium-0.1.0a0-cp314-cp314-macosx_10_15_x86_64.whl

---
+++ packages/copium/copium-0.1.0a0-cp314-cp314-macosx_10_15_x86_64.whl/copium/patch.pyi
@@ -0,0 +1,16 @@
+__all__ = ["enable", "disable", "enabled"]
+
+def enable() -> bool:
+    """
+    :return: True if copium was enabled, False if it was already enabled.
+    """
+
+def disable() -> bool:
+    """
+    :return: True if copium was disabled, False if it was already disabled.
+    """
+
+def enabled() -> bool:
+    """
+    :return: Whether copium is currently enabled.
+    """
