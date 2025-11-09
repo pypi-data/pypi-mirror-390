532ef154e17de33201646a9f90a58b32f0d2fd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:16 +0000

    Add package instaui-tdesign/0.5.6/instaui_tdesign-0.5.6-py3-none-any.whl

---
+++ packages/instaui-tdesign/instaui_tdesign-0.5.6-py3-none-any.whl/instaui_tdesign/components/breadcrumb.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+import typing
+
+from instaui_tdesign.components._icon_param_utils import make_icon_for_str
+from ._base_element import BaseElement
+from instaui.event.event_mixin import EventMixin
+from typing_extensions import TypedDict, Unpack
+from ._utils import handle_props, handle_event_from_props
+
+
+class Breadcrumb(BaseElement):
+    def __init__(
+        self,
+        **kwargs: Unpack[TBreadcrumbProps],
+    ):
+        super().__init__("t-breadcrumb")
+
+        self.props(handle_props(kwargs))  # type: ignore
+        handle_event_from_props(self, kwargs)  # type: ignore
+
+
+class BreadcrumbItem(BaseElement):
+    def __init__(
+        self,
+        content: typing.Optional[str] = None,
+        *,
+        icon: typing.Optional[str] = None,
+        **kwargs: Unpack[TBreadcrumbItemProps],
+    ):
+        super().__init__("t-breadcrumb-item")
+        self.props({"content": content})
+        make_icon_for_str(self, icon)
+        self.props(handle_props(kwargs))  # type: ignore
+        handle_event_from_props(self, kwargs)  # type: ignore
+
+    def on_click(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "click",
+            handler,
+            extends=extends,
+        )
+        return self
+
+
+class TBreadcrumbProps(TypedDict, total=False):
+    ellipsis: str
+    items_after_collapse: float
+    items_before_collapse: float
+    max_item_width: str
+    max_items: float
+    options: typing.List
+    separator: str
+    theme: typing.Literal["light"]
+
+
+class TBreadcrumbItemProps(TypedDict, total=False):
+    disabled: bool
+    href: str
+    max_width: str
+    replace: bool
+    router: typing.Dict
+    target: typing.Literal["_blank", "_self", "_parent", "_top"]
+    to: typing.Literal["Route"]
+    on_click: EventMixin
---
+++ packages/instaui-tdesign/instaui_tdesign-0.5.6-py3-none-any.whl/instaui_tdesign/components/tabs.py
@@ -0,0 +1,126 @@
+from __future__ import annotations
+import typing
+from instaui import custom
+from instaui.event.event_mixin import EventMixin
+from typing_extensions import TypedDict, Unpack
+
+from ._utils import handle_props, handle_event_from_props, try_setup_vmodel
+
+
+class Tabs(custom.element):
+    def __init__(
+        self,
+        value: typing.Optional[str] = None,
+        **kwargs: Unpack[TTabsProps],
+    ):
+        super().__init__("t-tabs")
+        custom.configure_slot_without_slot_prop(self, slot_names=["default"])
+        try_setup_vmodel(self, value)
+        self.props(handle_props(kwargs))  # type: ignore
+        handle_event_from_props(self, kwargs)  # type: ignore
+
+    def on_add(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "add",
+            handler,
+            extends=extends,
+        )
+        return self
+
+    def on_change(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "change",
+            handler,
+            extends=extends,
+        )
+        return self
+
+    def on_drag_sort(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "drag-sort",
+            handler,
+            extends=extends,
+        )
+        return self
+
+    def on_remove(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "remove",
+            handler,
+            extends=extends,
+        )
+        return self
+
+
+class TabPanel(custom.element):
+    def __init__(
+        self,
+        **kwargs: Unpack[TTabPanelProps],
+    ):
+        super().__init__("t-tab-panel")
+        custom.configure_slot_without_slot_prop(self, slot_names=["default"])
+        self.props(handle_props(kwargs))  # type: ignore
+        handle_event_from_props(self, kwargs)  # type: ignore
+
+    def on_remove(
+        self,
+        handler: EventMixin,
+        *,
+        extends: typing.Optional[typing.List] = None,
+    ):
+        self.on(
+            "remove",
+            handler,
+            extends=extends,
+        )
+        return self
+
+
+class TTabsProps(TypedDict, total=False):
+    action: typing.Literal["TNode"]
+    addable: bool
+    disabled: bool
+    drag_sort: bool
+    list: typing.List[TTabPanelProps]
+    placement: typing.Literal["left", "top", "bottom", "right"]
+    scroll_position: typing.Literal["auto", "start", "center", "end"]
+    size: typing.Literal["medium", "large"]
+    theme: typing.Literal["normal", "card"]
+    default_value: str
+    on_add: EventMixin
+    on_change: EventMixin
+    on_drag_sort: EventMixin
+    on_remove: EventMixin
+
+
+class TTabPanelProps(TypedDict, total=False):
+    default: str
+    destroy_on_hide: bool
+    disabled: bool
+    draggable: bool
+    label: str
+    lazy: bool
+    panel: str
+    removable: bool
+    value: str
+    on_remove: EventMixin
