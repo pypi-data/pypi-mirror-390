d5d8dcb93e5348a8e6fbac0686bc9f35e4190034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 14:06:14 +0000

    Add package sheet-stream/0.1.0/sheet_stream-0.1.0-py3-none-any.whl

---
+++ packages/sheet-stream/sheet_stream-0.1.0-py3-none-any.whl/sheet_stream/erros/__init__.py
@@ -0,0 +1,53 @@
+
+
+class InvalidFileSheetError(Exception):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class InvalidFileCsvError(InvalidFileSheetError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class InvalidFileExcelError(InvalidFileSheetError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class InvalidFileOdsError(InvalidFileSheetError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class ListTableError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class ListTableLargeError(ListTableError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class ListTableShortError(ListTableError):
+
+    def __init__(self, *args):
+        super().__init__(*args)
+
+
+class RowLargeError(ListTableError):
+
+    def __init__(self, *args):
+        pass
+
+
+class RowShortError(ListTableError):
+
+    def __init__(self, *args):
+        pass
