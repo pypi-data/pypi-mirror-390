6c1ee3fb702f231a2351717b773764e97b4b4841
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:55 +0000

    Add package FindMy/0.9.5/findmy-0.9.5-py3-none-any.whl

---
+++ packages/FindMy/findmy-0.9.5-py3-none-any.whl/findmy/errors.py
@@ -0,0 +1,25 @@
+"""Exception classes."""
+
+
+class InvalidCredentialsError(Exception):
+    """Raised when credentials are incorrect."""
+
+
+class UnauthorizedError(Exception):
+    """Raised when an authorization error occurs."""
+
+
+class UnhandledProtocolError(RuntimeError):
+    """
+    Raised when an unexpected error occurs while communicating with Apple servers.
+
+    This is almost always a bug, so please report it.
+    """
+
+
+class InvalidStateError(RuntimeError):
+    """
+    Raised when a method is used that is in conflict with the internal account state.
+
+    For example: calling :meth:`BaseAppleAccount.login` while already logged in.
+    """
