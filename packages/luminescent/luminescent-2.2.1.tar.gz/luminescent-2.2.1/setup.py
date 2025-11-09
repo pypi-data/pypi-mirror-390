833405c0f0109e59876b0928c41601e5eb20d9bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:14 +0000

    Add package luminescent/2.2.1/luminescent-2.2.1.tar.gz

---
+++ packages/luminescent/luminescent-2.2.1.tar.gz/luminescent-2.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luminescent",  # Your package name
+    version="2.2.1",  # Your package version
+    description="GPU-accelerated fully differentiable FDTD for photonics and RF",
+    author="Paul Shen",
+    author_email="pxshen@alumni.stanford.edu",
+    packages=find_packages(),  # Automatically find your package(s)
+    install_requires=[
+        "gdsfactory",
+        "pymeshfix",
+        "electromagneticpython",
+        "sortedcontainers",
+        "scikit-rf",
+        "opencv-python",
+        "femwell",
+        "rasterio",
+        "rtree",
+        "gmsh",
+    ],
+)
+# cd C:\Users\pxshe\OneDrive\Desktop\beans\Luminescent.jl\luminescent\luminescent
+# cd luminescent
+# python -m build
+# twine upload dist/*
+
+# pip install gdsfactory pillow pymeshfix electromagneticpython sortedcontainers scikit-rf
+#
