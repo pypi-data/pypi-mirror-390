dc17494a389d314c053a9adf53945eae3125f2fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:06:43 +0000

    Add package omnicart-pipeline-campeon/0.1.5/omnicart_pipeline_campeon-0.1.5-py3-none-any.whl

---
+++ packages/omnicart-pipeline-campeon/omnicart_pipeline_campeon-0.1.5-py3-none-any.whl/omnicart_pipeline/cli.py
@@ -0,0 +1,14 @@
+from  omnicart_pipeline.pipeline import Pipeline
+
+def main():
+    print('..initializing the main program')
+    pipeline = Pipeline()
+    pipeline.run()
+  
+
+
+if __name__ == '__main__':
+    main()
+    
+  
+  
