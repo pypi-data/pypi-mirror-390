46caea3c3ed2a95ffad305f04d16d5d84bdec6b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:16 +0000

    Add package dedalus-labs/0.1.0/dedalus_labs-0.1.0-py3-none-any.whl

---
+++ packages/dedalus-labs/dedalus_labs-0.1.0-py3-none-any.whl/dedalus_labs/types/shared/__init__.py
@@ -0,0 +1,4 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from .dedalus_model import DedalusModel as DedalusModel
+from .dedalus_model_choice import DedalusModelChoice as DedalusModelChoice
