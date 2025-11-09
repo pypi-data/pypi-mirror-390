89b9dcfbc11a106954417af564a6ec66957593a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:56 +0000

    Add package brtp/0.0.16/brtp-0.0.16.tar.gz

---
+++ packages/brtp/brtp-0.0.16.tar.gz/brtp-0.0.16/brtp/math/aggregation/__init__.py
@@ -0,0 +1,1 @@
+from ._means import geo_mean, mean, ordered_weighted_geo_mean, ordered_weighted_mean, weighted_geo_mean, weighted_mean
---
+++ packages/brtp/brtp-0.0.16.tar.gz/brtp-0.0.16/brtp/math/sampling/__init__.py
@@ -0,0 +1,1 @@
+from ._deterministic import linspace, logspace
