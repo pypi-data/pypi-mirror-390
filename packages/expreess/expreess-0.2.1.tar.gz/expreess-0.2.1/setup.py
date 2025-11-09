c854b7402ebf9e98233106e53662330c61c38e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:24 +0000

    Add package expreess/0.2.1/expreess-0.2.1.tar.gz

---
+++ packages/expreess/expreess-0.2.1.tar.gz/expreess-0.2.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="expreess",
+    version="0.2.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Aayush Singh",
+    author_email="youremail@example.com",
+    description="A simple greeting module called expreess",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/expreess",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
