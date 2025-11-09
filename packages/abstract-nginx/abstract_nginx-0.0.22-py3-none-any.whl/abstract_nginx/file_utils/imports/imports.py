28c42244fbf078deaeedfd6abead1824eb778690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:47 +0000

    Add package abstract-nginx/0.0.22/abstract_nginx-0.0.22-py3-none-any.whl

---
+++ packages/abstract-nginx/abstract_nginx-0.0.22-py3-none-any.whl/abstract_nginx/file_utils/imports/imports.py
@@ -0,0 +1,17 @@
+import os,time,random,hashlib,shutil
+from flask import (Blueprint,
+                   request,
+                   render_template_string,
+                   url_for,
+                   jsonify
+                   )
+from abstract_utilities import SingletonMeta
+from abstract_pandas import (get_df,
+                             safe_excel_save,
+                             is_file)
+from werkzeug.datastructures import FileStorage
+from werkzeug.utils import secure_filename
+
+import os, re, json
+from abstract_utilities import get_any_value,eatAll,read_from_file,write_to_file,safe_dump_to_json
+from abstract_utilities.file_utils import *
