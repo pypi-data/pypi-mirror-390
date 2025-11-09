270671d61920e209ace018fa85d3a8e8db870c88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:46 +0000

    Add package django-request-in/0.0.4/django_request_in-0.0.4-py3-none-any.whl

---
+++ packages/django-request-in/django_request_in-0.0.4-py3-none-any.whl/django_request_in/__init__.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+from django_request_in.fields import Field, AnyField, IntegerField, StringField, BooleanField, DictField, SourceEnum
+from django_request_in.validators import greater_than_zero
+from django_request_in.requests import Request
+
+from django_request_in.decorators import view_decorator, function_decorator
+from django_request_in.schema_ins import BaseSchemaIn, SchemaIn
+
+__all__ = [
+    'Field',
+    'AnyField',
+    'IntegerField',
+    'StringField',
+    'BooleanField',
+    'DictField',
+    'SourceEnum',
+    'greater_than_zero',
+    'view_decorator',
+    'function_decorator',
+    'BaseSchemaIn',
+    'SchemaIn',
+    'Request'
+]
+
+__version__ = "0.0.4"
