32fb7414fc3120f3fac82e5cfe5ea8d116c7faec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:47 +0000

    Add package Hypercorn/0.18.0/hypercorn-0.18.0.tar.gz

---
+++ packages/Hypercorn/hypercorn-0.18.0.tar.gz/hypercorn-0.18.0/tests/helpers.py
@@ -0,0 +1,109 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from copy import deepcopy
+from json import dumps
+from socket import AF_INET
+from typing import cast
+
+from hypercorn.typing import ASGIReceiveCallable, ASGISendCallable, Scope, WWWScope
+
+SANITY_BODY = b"Hello Hypercorn"
+
+
+class MockSocket:
+    family = AF_INET
+
+    def getsockname(self) -> tuple[str, int]:
+        return ("162.1.1.1", 80)
+
+    def getpeername(self) -> tuple[str, int]:
+        return ("127.0.0.1", 80)
+
+
+async def empty_framework(scope: Scope, receive: Callable, send: Callable) -> None:
+    pass
+
+
+class SlowLifespanFramework:
+    def __init__(self, delay: float, sleep: Callable) -> None:
+        self.delay = delay
+        self.sleep = sleep
+
+    async def __call__(
+        self, scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+    ) -> None:
+        await self.sleep(self.delay)
+
+
+async def echo_framework(
+    input_scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+) -> None:
+    input_scope = cast(WWWScope, input_scope)
+    scope = deepcopy(input_scope)
+    scope["query_string"] = scope["query_string"].decode()  # type: ignore
+    scope["raw_path"] = scope["raw_path"].decode()  # type: ignore
+    scope["headers"] = [
+        (name.decode(), value.decode()) for name, value in scope["headers"]  # type: ignore
+    ]
+
+    body = bytearray()
+    while True:
+        event = await receive()
+        if event["type"] in {"http.disconnect", "websocket.disconnect"}:
+            break
+        elif event["type"] == "http.request":
+            body.extend(event.get("body", b""))
+            if not event.get("more_body", False):
+                response = dumps({"scope": scope, "request_body": body.decode()}).encode()
+                content_length = len(response)
+                await send(
+                    {
+                        "type": "http.response.start",
+                        "status": 200,
+                        "headers": [(b"content-length", str(content_length).encode())],
+                    }
+                )
+                await send({"type": "http.response.body", "body": response, "more_body": False})
+                break
+        elif event["type"] == "websocket.connect":
+            await send({"type": "websocket.accept"})  # type: ignore
+        elif event["type"] == "websocket.receive":
+            await send({"type": "websocket.send", "text": event["text"], "bytes": event["bytes"]})
+
+
+async def sanity_framework(
+    scope: Scope, receive: ASGIReceiveCallable, send: ASGISendCallable
+) -> None:
+    body = b""
+    if scope["type"] == "websocket":
+        await send({"type": "websocket.accept"})  # type: ignore
+
+    while True:
+        event = await receive()
+        if event["type"] in {"http.disconnect", "websocket.disconnect"}:
+            break
+        elif event["type"] == "lifespan.startup":
+            assert "state" in scope
+            await send({"type": "lifspan.startup.complete"})  # type: ignore
+        elif event["type"] == "lifespan.shutdown":
+            await send({"type": "lifspan.shutdown.complete"})  # type: ignore
+        elif event["type"] == "http.request" and event.get("more_body", False):
+            body += event["body"]
+        elif event["type"] == "http.request" and not event.get("more_body", False):
+            body += event["body"]
+            assert body == SANITY_BODY
+            response = b"Hello & Goodbye"
+            content_length = len(response)
+            await send(
+                {
+                    "type": "http.response.start",
+                    "status": 200,
+                    "headers": [(b"content-length", str(content_length).encode())],
+                }
+            )
+            await send({"type": "http.response.body", "body": response, "more_body": False})
+            break
+        elif event["type"] == "websocket.receive":
+            assert event["bytes"] == SANITY_BODY
+            await send({"type": "websocket.send", "text": "Hello & Goodbye"})  # type: ignore
---
+++ packages/Hypercorn/hypercorn-0.18.0.tar.gz/hypercorn-0.18.0/tests/test_app_wrappers.py
@@ -0,0 +1,208 @@
+from __future__ import annotations
+
+import asyncio
+from collections.abc import Callable
+from functools import partial
+from typing import Any
+
+import pytest
+import trio
+
+from hypercorn.app_wrappers import _build_environ, InvalidPathError, WSGIWrapper
+from hypercorn.typing import ASGIReceiveEvent, ASGISendEvent, ConnectionState, HTTPScope
+
+
+def echo_body(environ: dict, start_response: Callable) -> list[bytes]:
+    status = "200 OK"
+    output = environ["wsgi.input"].read()
+    headers = [
+        ("Content-Type", "text/plain; charset=utf-8"),
+        ("Content-Length", str(len(output))),
+    ]
+    start_response(status, headers)
+    return [output]
+
+
+@pytest.mark.trio
+async def test_wsgi_trio() -> None:
+    app = WSGIWrapper(echo_body, 2**16)
+    scope: HTTPScope = {
+        "http_version": "1.1",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/",
+        "root_path": "/",
+        "query_string": b"a=b",
+        "raw_path": b"/",
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    send_channel, receive_channel = trio.open_memory_channel[ASGIReceiveEvent](1)
+    await send_channel.send({"type": "http.request"})  # type: ignore
+
+    messages = []
+
+    async def _send(message: ASGISendEvent) -> None:
+        messages.append(message)
+
+    await app(scope, receive_channel.receive, _send, trio.to_thread.run_sync, trio.from_thread.run)
+    assert messages == [
+        {
+            "headers": [(b"content-type", b"text/plain; charset=utf-8"), (b"content-length", b"0")],
+            "status": 200,
+            "type": "http.response.start",
+        },
+        {"body": bytearray(b""), "type": "http.response.body", "more_body": True},
+        {"body": bytearray(b""), "type": "http.response.body", "more_body": False},
+    ]
+
+
+async def _run_app(app: WSGIWrapper, scope: HTTPScope, body: bytes = b"") -> list[ASGISendEvent]:
+    queue: asyncio.Queue = asyncio.Queue()
+    await queue.put({"type": "http.request", "body": body})
+
+    messages = []
+
+    async def _send(message: ASGISendEvent) -> None:
+        messages.append(message)
+
+    event_loop = asyncio.get_running_loop()
+
+    def _call_soon(func: Callable, *args: Any) -> Any:
+        future = asyncio.run_coroutine_threadsafe(func(*args), event_loop)
+        return future.result()
+
+    await app(scope, queue.get, _send, partial(event_loop.run_in_executor, None), _call_soon)
+    return messages
+
+
+@pytest.mark.asyncio
+async def test_wsgi_asyncio() -> None:
+    app = WSGIWrapper(echo_body, 2**16)
+    scope: HTTPScope = {
+        "http_version": "1.1",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/",
+        "root_path": "/",
+        "query_string": b"a=b",
+        "raw_path": b"/",
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    messages = await _run_app(app, scope)
+    assert messages == [
+        {
+            "headers": [(b"content-type", b"text/plain; charset=utf-8"), (b"content-length", b"0")],
+            "status": 200,
+            "type": "http.response.start",
+        },
+        {"body": bytearray(b""), "type": "http.response.body", "more_body": True},
+        {"body": bytearray(b""), "type": "http.response.body", "more_body": False},
+    ]
+
+
+@pytest.mark.asyncio
+async def test_max_body_size() -> None:
+    app = WSGIWrapper(echo_body, 4)
+    scope: HTTPScope = {
+        "http_version": "1.1",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/",
+        "root_path": "/",
+        "query_string": b"a=b",
+        "raw_path": b"/",
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    messages = await _run_app(app, scope, b"abcde")
+    assert messages == [
+        {"headers": [], "status": 400, "type": "http.response.start"},
+        {"body": bytearray(b""), "type": "http.response.body", "more_body": False},
+    ]
+
+
+def no_start_response(environ: dict, start_response: Callable) -> list[bytes]:
+    return [b"result"]
+
+
+@pytest.mark.asyncio
+async def test_no_start_response() -> None:
+    app = WSGIWrapper(no_start_response, 2**16)
+    scope: HTTPScope = {
+        "http_version": "1.1",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/",
+        "root_path": "/",
+        "query_string": b"a=b",
+        "raw_path": b"/",
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    with pytest.raises(RuntimeError):
+        await _run_app(app, scope)
+
+
+def test_build_environ_encoding() -> None:
+    scope: HTTPScope = {
+        "http_version": "1.0",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/中/文",
+        "root_path": "/中",
+        "query_string": b"bar=baz",
+        "raw_path": "/中/文".encode(),
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    environ = _build_environ(scope, b"")
+    assert environ["SCRIPT_NAME"] == "/中".encode().decode("latin-1")
+    assert environ["PATH_INFO"] == "/文".encode().decode("latin-1")
+
+
+def test_build_environ_root_path() -> None:
+    scope: HTTPScope = {
+        "http_version": "1.0",
+        "asgi": {},
+        "method": "GET",
+        "headers": [],
+        "path": "/中文",
+        "root_path": "/中国",
+        "query_string": b"bar=baz",
+        "raw_path": "/中文".encode(),
+        "scheme": "http",
+        "type": "http",
+        "client": ("localhost", 80),
+        "server": None,
+        "extensions": {},
+        "state": ConnectionState({}),
+    }
+    with pytest.raises(InvalidPathError):
+        _build_environ(scope, b"")
