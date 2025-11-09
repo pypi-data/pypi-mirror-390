49d12bc091bcd410fcfc2810e59de42294fd561e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 14:06:07 +0000

    Add package fbdfile/2025.11.8/fbdfile-2025.11.8.tar.gz

---
+++ packages/fbdfile/fbdfile-2025.11.8.tar.gz/fbdfile-2025.11.8/tests/conftest.py
@@ -0,0 +1,24 @@
+# fbdfile/tests/conftest.py
+
+import os
+import sys
+
+if os.environ.get('VSCODE_CWD'):
+    # work around pytest not using PYTHONPATH in VSCode
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    )
+
+
+def pytest_report_header(config):
+    try:
+        pyversion = f'Python {sys.version.splitlines()[0]}'
+        import fbdfile
+
+        return '{}\npackagedir: {}\nversion: fbdfile {}'.format(
+            pyversion,
+            fbdfile.__path__[0],
+            fbdfile.__version__,
+        )
+    except Exception as exc:
+        return f'pytest_report_header failed: {exc!s}'
