8c876335452166386e729634796fe5cf0d8d711b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 05:05:35 +0000

    Add package ace-framework/0.4.0/ace_framework-0.4.0-py3-none-any.whl

---
+++ packages/ace-framework/ace_framework-0.4.0-py3-none-any.whl/ace/__init__.py
@@ -0,0 +1,83 @@
+"""Agentic Context Engineering (ACE) reproduction framework."""
+
+from typing import Optional
+from .playbook import Bullet, Playbook
+from .delta import DeltaOperation, DeltaBatch
+from .llm import LLMClient, DummyLLMClient, TransformersLLMClient
+from .roles import (
+    Generator,
+    ReplayGenerator,
+    Reflector,
+    Curator,
+    GeneratorOutput,
+    ReflectorOutput,
+    CuratorOutput,
+)
+from .adaptation import (
+    OfflineAdapter,
+    OnlineAdapter,
+    Sample,
+    TaskEnvironment,
+    SimpleEnvironment,
+    EnvironmentResult,
+    AdapterStepResult,
+)
+
+# Import optional feature detection
+from .features import has_opik, has_litellm
+
+# Import observability components if available
+if has_opik():
+    try:
+        from .observability import OpikIntegration as _OpikIntegration
+
+        OpikIntegration: Optional[type] = _OpikIntegration
+        OBSERVABILITY_AVAILABLE = True
+    except ImportError:
+        OpikIntegration: Optional[type] = None  # type: ignore
+        OBSERVABILITY_AVAILABLE = False
+else:
+    OpikIntegration: Optional[type] = None  # type: ignore
+    OBSERVABILITY_AVAILABLE = False
+
+# Import production LLM clients if available
+if has_litellm():
+    try:
+        from .llm_providers import LiteLLMClient as _LiteLLMClient
+
+        LiteLLMClient: Optional[type] = _LiteLLMClient
+        LITELLM_AVAILABLE = True
+    except ImportError:
+        LiteLLMClient: Optional[type] = None  # type: ignore
+        LITELLM_AVAILABLE = False
+else:
+    LiteLLMClient: Optional[type] = None  # type: ignore
+    LITELLM_AVAILABLE = False
+
+__all__ = [
+    "Bullet",
+    "Playbook",
+    "DeltaOperation",
+    "DeltaBatch",
+    "LLMClient",
+    "DummyLLMClient",
+    "TransformersLLMClient",
+    "LiteLLMClient",
+    "Generator",
+    "ReplayGenerator",
+    "Reflector",
+    "Curator",
+    "GeneratorOutput",
+    "ReflectorOutput",
+    "CuratorOutput",
+    "OfflineAdapter",
+    "OnlineAdapter",
+    "Sample",
+    "TaskEnvironment",
+    "SimpleEnvironment",
+    "EnvironmentResult",
+    "AdapterStepResult",
+    "OpikIntegration",
+    "LITELLM_AVAILABLE",
+    "OBSERVABILITY_AVAILABLE",
+]
