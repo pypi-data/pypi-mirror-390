9e28399c91fe4a0c4f86da223ea09ab95893347b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:01 +0000

    Add package akipy/1.2.4/akipy-1.2.4.tar.gz

---
+++ packages/akipy/akipy-1.2.4.tar.gz/akipy-1.2.4/akipy/__init__.py
@@ -0,0 +1,22 @@
+"""
+akipy - A Python wrapper library for Akinator
+
+:copyright: (c) 2024, advnpzn
+:license: MIT, see LICENSE for more details.
+"""
+
+from .akinator import Akinator as Akinator
+from . import async_akinator as async_akinator
+from .exceptions import (
+    CantGoBackAnyFurther,
+    InvalidChoiceError,
+    InvalidLanguageError,
+)
+
+__all__ = [
+    "Akinator",
+    "async_akinator",
+    "CantGoBackAnyFurther",
+    "InvalidChoiceError",
+    "InvalidLanguageError",
+]
