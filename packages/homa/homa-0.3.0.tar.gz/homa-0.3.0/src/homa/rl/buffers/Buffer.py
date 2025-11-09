f79345d2780044db3ba25ce63741dd7a201b9c11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:06:38 +0000

    Add package homa/0.3.0/homa-0.3.0.tar.gz

---
+++ packages/homa/homa-0.3.0.tar.gz/homa-0.3.0/src/homa/rl/buffers/Buffer.py
@@ -0,0 +1,7 @@
+from .concerns import ResetsCollection, HasRecordAlternatives
+
+
+class Buffer(ResetsCollection, HasRecordAlternatives):
+    def __init__(self, capacity: int):
+        self.capacity: int = capacity
+        self.reset()
---
+++ packages/homa/homa-0.3.0.tar.gz/homa-0.3.0/src/homa/rl/buffers/__init__.py
@@ -0,0 +1,4 @@
+from .ActorCriticBuffer import ActorCriticBuffer
+from .ImageBuffer import ImageBuffer
+from .DiversityIsAllYouNeedBuffer import DiversityIsAllYouNeedBuffer
+from .Buffer import Buffer
