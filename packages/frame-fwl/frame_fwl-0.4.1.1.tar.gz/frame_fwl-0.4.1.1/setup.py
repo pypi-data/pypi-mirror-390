58f833e567b368a04389166bdb0cfab6b3ea3d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:08 +0000

    Add package frame-fwl/0.4.1.1/frame_fwl-0.4.1.1.tar.gz

---
+++ packages/frame-fwl/frame_fwl-0.4.1.1.tar.gz/frame_fwl-0.4.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='frame-fwl', # frame-framework-lib
+    version='0.4.1.1',
+    description='The Frame - multitool module for programming with advanced framing capabilities',
+    author='pt',
+    author_email='kvantorium73.int@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.10', 
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+        'Operating System :: OS Independent',
+    ],
+    keywords='frame, superglobal, context, context manager, framing, code generation, execution',
+    url='https://github.com/pt-main/frame'
+)
