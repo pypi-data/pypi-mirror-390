5dd118716d9516b773331081cb86b562f171aed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:51 +0000

    Add package text-python/0.3/text_python-0.3.tar.gz

---
+++ packages/text-python/text_python-0.3.tar.gz/text_python-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages  # <- dòng này rất quan trọng!
+
+setup(
+    name="text_python",
+    version="0.3",  # nhớ tăng version mới
+    packages=find_packages(),
+    install_requires=[
+        "pygame>=2.6.1"
+    ],
+    python_requires=">=3.8",
+)
+
+
