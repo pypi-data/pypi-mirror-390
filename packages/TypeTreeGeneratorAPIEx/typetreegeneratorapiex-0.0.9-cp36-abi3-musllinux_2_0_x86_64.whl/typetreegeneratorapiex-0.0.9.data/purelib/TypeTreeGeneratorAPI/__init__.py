8176650ac382b0bafc75e958f3ca1363a8b7642b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:18 +0000

    Add package TypeTreeGeneratorAPIEx/0.0.9/typetreegeneratorapiex-0.0.9-cp36-abi3-musllinux_2_0_x86_64.whl

---
+++ packages/TypeTreeGeneratorAPIEx/typetreegeneratorapiex-0.0.9-cp36-abi3-musllinux_2_0_x86_64.whl/typetreegeneratorapiex-0.0.9.data/purelib/TypeTreeGeneratorAPI/__init__.py
@@ -0,0 +1,12 @@
+from .TypeTreeGenerator import TypeTreeBackend, TypeTreeGenerator, TypeTreeNode, TypeTreeNodeNative
+
+version = __version__ = "0.0.9"
+
+__all__ = [
+    "__version__",
+    "version",
+    "TypeTreeBackend",
+    "TypeTreeGenerator",
+    "TypeTreeNode",
+    "TypeTreeNodeNative",
+]
