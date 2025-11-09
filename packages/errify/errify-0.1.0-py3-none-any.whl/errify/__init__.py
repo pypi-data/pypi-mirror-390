532854f5ff487376525b9a2618ea2336993a9574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:06:28 +0000

    Add package errify/0.1.0/errify-0.1.0-py3-none-any.whl

---
+++ packages/errify/errify-0.1.0-py3-none-any.whl/errify/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from .errify import install, uninstall, register_handler, _explainer
+install()  # auto-activate on import
+
+__version__ = "0.1.0"
+__author__ = "Divakar Babu M P"
+__email__ = "divakarbabu369@gmail.com"
