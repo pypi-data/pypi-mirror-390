b005ef8d5b483b00927ae3849603dd216cd6ede2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 04:07:37 +0000

    Add package pypomes-crypto/0.5.0/pypomes_crypto-0.5.0.tar.gz

---
+++ packages/pypomes-crypto/pypomes_crypto-0.5.0.tar.gz/pypomes_crypto-0.5.0/src/pypomes_crypto/crypto_pdf.py
@@ -0,0 +1,17 @@
+
+# from pathlib import Path
+# from pyhanko.sign.validation.pdf_embedded import EmbeddedPdfSignature
+# from pyhanko_certvalidator import ValidationContext
+# from pyhanko.keys import load_certs_from_pemder_data
+# from pyhanko.pdf_utils.reader import PdfFileReader
+# from pyhanko.sign.validation import validate_pdf_signature
+# from pyhanko.sign.validation.status import PdfSignatureStatus
+# from pypomes_core import file_get_data, exc_format, env_get_str, APP_PREFIX
+
+class CryptoPdf:
+    """
+    Python code to extract cryptographic data from a digitally signed PDF file.
+
+    instance attributes:
+
+    """
