177cb94d70a46ade4e47e3a1bfc9b48056951a97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:39 +0000

    Add package tkurses/0.3.3/tkurses-0.3.3.tar.gz

---
+++ packages/tkurses/tkurses-0.3.3.tar.gz/tkurses-0.3.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tkurses",
+    version="0.3.3",
+    author="Freeboardtortoise",
+    author_email="Freeboardtortoise@gmail.com",
+    description="Tkinter-like themed UI framework for curses",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://not-aplicable.com",
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
