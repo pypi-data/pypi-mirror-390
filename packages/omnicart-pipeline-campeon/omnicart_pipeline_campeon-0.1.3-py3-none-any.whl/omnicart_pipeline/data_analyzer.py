49123b95b245402b757a7644ea0f97ceb5a472a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:22 +0000

    Add package omnicart-pipeline-campeon/0.1.3/omnicart_pipeline_campeon-0.1.3-py3-none-any.whl

---
+++ packages/omnicart-pipeline-campeon/omnicart_pipeline_campeon-0.1.3-py3-none-any.whl/omnicart_pipeline/data_analyzer.py
@@ -0,0 +1,24 @@
+from typing import List, Dict, Any, Iterable
+import pandas as pd
+import logging
+
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(level=logging.INFO, format= "%(asctime)s - %(levelname)s : %(message)s")
+
+
+class DataAnalyzer:
+  """The class Analyzer that generate a summary for the api data that has been converted to dataframe"""
+  
+  
+  def analyze(self, df: pd.DataFrame) -> Dict[str, Dict[str, Any]]:
+    logging.info('analyzing data...')
+    agg_df = df.groupby('name').agg(
+    total_revenue=('revenue', 'sum'),
+    product_count=('id', 'count'),
+    average_price=('price', 'mean')
+    )
+    
+    logging.info('data analyzed and saved as seller_performance_report')
+    return agg_df.to_json('seller_performance_report.json',  orient='index', indent=4)#agg_df.to_dict()
+    
