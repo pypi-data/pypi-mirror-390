811e47fea32aeb8e7f052f98a24b838e6d4c62b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:37 +0000

    Add package retrodevem/0.7/retrodevem-0.7.tar.gz

---
+++ packages/retrodevem/retrodevem-0.7.tar.gz/retrodevem-0.7/doc/sourcing.md
@@ -0,0 +1,51 @@
+# Sourcing
+
+Everything was found on Farnell, except the plexiglass.  The prices
+are in Euros.
+
+## Connectors and components
+
+- 2x 2x5  Pin Headers
+- 1x 2x20 PCB Receptacle / Socket
+- 14x 15 KOhms resitors
+- 2x ULN2003AN
+
+| Count | Reference                   | Description       | Unit Price(+) | Total Price |
+|-------|-----------------------------|-------------------|---------------|-------------|
+| 2     | Multicomp Pro 2213S-10G     | 2x5 Pin Header    | 0.085         | 0.17        |
+| 1     | Multicomp Pro 2214S-40SG-85 | 2x20 PCB Socket   | 1.45          | 1.45        |
+| 14    | Multicomp Pro MP006750      | 15 KOhms resistor | 0.021         | 0.294       |
+| 2     | Texas Instruments ULN2003AN | Transistors Array | 0.392         | 0.784       |
+|       |                             | Total             |               | 2.698       |
+
+(+) On Farnell
+
+## Standoffs and feet
+
+| Count | Reference              | Description                  | Unit Price(+) | Total Price |
+|-------|------------------------|------------------------------|---------------|-------------|
+| 4     |                        | female/female M2.5, 12mm     |               |             |
+| 8     | Ettinger 005.12.103    | male/female M2.5, 10mm, 16mm | 0.319         | 2.55        |
+| 4     | Ettinger 017.52.080    | foot round 2.8mm * 8mm       | 0.30          | 1.2         |
+| 8     | Multicomp Pro MP007339 | vis M2.5 6mm                 | 0.10(++)      | 0.8         |
+|       |                        | Total                        |               |             |
+
+(+) On Farnell
+(++) Lot of 50 for 5.01 E (/ 5.01 50)
+
+## Cables
+
+- 2x 40 cm ribbon cable
+- 2x DB9 connector (Plastic)
+- 2x IDC connector
+
+| Count | Reference                     | Description   | Unit Price(+) | Total Price |
+|-------|-------------------------------|---------------|---------------|-------------|
+| 80 cm | Multicomp Pro R2651DTSY10SC85 | Ribbon cable  | 1.18          | 0.094       |
+| 2     | Amphenol L17DEFRB09S          | DB9 connector | 3.53          | 7.06        |
+| 2     | Amphenol T812110A101CEU       | IDC connector | 0.293         | 0.586       |
+|       |                               | Total         |               | 7.740       |
+
+## Plexiglass
+
+2mm transparent plexiglass plate (on Amazon) for a few Euros.
---
+++ packages/retrodevem/retrodevem-0.7.tar.gz/retrodevem-0.7/src/retrodevem/atarist_joystick.py
@@ -0,0 +1,133 @@
+import time
+import logging
+import click
+
+from gpiozero import LED
+from gpiozero.exc import BadPinFactory
+
+# From current package
+import inputdevice as idev
+
+logger = logging.getLogger(__name__)
+
+BOARDS_CONFIG = {
+    "v2.0": {
+        0: { # Port 0 (i.e connector J2)
+            "down" : 4,
+            "up"   : 17,
+            "left" : 3,
+            "right": 22,
+            "fire" : 27,
+        },
+        1: {
+            "down" : 0,
+            "up"   : 5,
+            "left" : 11,
+            "right": 13,
+            "fire" : 6,
+        },
+    },
+    "v2.1": {
+        0: { # Port 1 (i.e connector J3)
+            "down" : 27,
+            "up"   : 22,
+            "left" : 17,
+            "right": 4,
+            "fire" : 10,
+        },
+        1: {
+            "down" : 6,
+            "up"   : 13,
+            "left" : 5,
+            "right": 0,
+            "fire" : 19,
+        },
+    },
+}
+
+def rpi_init(board_version, port_id):
+    pins = BOARDS_CONFIG[board_version][port_id]
+    return {k:LED(v) for k,v in pins.items()}
+
+def process_input_events(input_device, board_signals):
+    dev = idev.InputDevice(input_device)
+    logger.info(f"Opened device: {input_device}")
+    while True:
+        _, _, ev_type, ev_code, ev_value = dev.get_event()
+        if  ev_type == idev.EV_ABS:
+
+            if   ev_code == idev.ABS_HAT0X:
+                logger.info(f"HAT0X move: {ev_value}")
+                if   ev_value == -1: board_signals["left"].on()
+                elif ev_value ==  1: board_signals["right"].on()
+                else: # ev_value == 0 when releasing a button
+                    board_signals["left"].off()
+                    board_signals["right"].off()
+
+            elif ev_code == idev.ABS_HAT0Y:
+                logger.info(f"HAT0Y move: {ev_value}")
+                if   ev_value == -1: board_signals["up"].on()
+                elif ev_value ==  1: board_signals["down"].on()
+                else: # ev_value == 0 when releasing a button
+                    board_signals["down"].off()
+                    board_signals["up"].off()
+
+            elif ev_code == idev.ABS_X:
+                if   ev_value <= 64 :
+                    logger.info(f"X (analog left) move: {ev_value}")
+                    board_signals["left"].on()
+                    board_signals["right"].off()
+                elif ev_value >= 192:
+                    logger.info(f"X (analog left) move: {ev_value}")
+                    board_signals["left"].off()
+                    board_signals["right"].on()
+                else: # 64 < ev_value < 192
+                    board_signals["left"].off()
+                    board_signals["right"].off()
+
+            elif ev_code == idev.ABS_Y:
+                if   ev_value <= 64 :
+                    logger.info(f"Y (analog left) move: {ev_value}")
+                    board_signals["up"].on()
+                    board_signals["down"].off()
+                elif ev_value >= 192:
+                    logger.info(f"Y (analog left) move: {ev_value}")
+                    board_signals["up"].off()
+                    board_signals["down"].on()
+                else: # 64 < ev_value < 192
+                    board_signals["up"].off()
+                    board_signals["down"].off()
+
+        elif ev_type == idev.EV_KEY and ev_code == idev.BTN_SOUTH:
+                logger.info(f"FIRE button: {ev_value}")
+                if ev_value == 1: board_signals["fire"].on()
+                else: board_signals["fire"].off()
+
+@click.command()
+@click.option("--board",  "-b", default="v2.1", type=click.Choice(['v2.0', 'v2.1']), help="Board revision.", show_default=True)
+@click.option("--device", "-d", default="/dev/input/event0", help="Input device to use.", show_default=True)
+@click.option("--port",   "-p", default='1', type=click.Choice(['0', '1']), help="Board/Atari ST port to connect the joystick to.", show_default=True)
+@click.option("--debug/--no-debug", help="Display debugging information.", show_default=True)
+def main(device, board, port, debug):
+    """Send joystick/gamepad events to an Atari ST connected to the
+    RetroDevEm board.  Usage example: atarist-joystick --board v2.0
+    --device /dev/input/event0 --port 0
+
+    """
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    try:
+        signals = rpi_init(board, int(port))
+        while True:
+            try:
+                process_input_events(device, signals)
+            except OSError as e:
+                # Sometimes gamepads get temporarily disconnected with crap cables
+                logger.warning(f"Error getting event: {e}")
+            time.sleep(0.2)
+    except BadPinFactory as e:
+        logger.error(f"Failed to initialize GPIO pins: {e}")
+        logger.error( "This program expects to be running on a Raspberry Pi. Is that the case ?")
+
+if __name__ == "__main__":
+    main()
