3773286c1776bac71de570195352e6fdedc4381e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 15:06:16 +0000

    Add package lfss/0.15.4/lfss-0.15.4-py3-none-any.whl

---
+++ packages/lfss/lfss-0.15.4-py3-none-any.whl/lfss/cli/user.py
@@ -0,0 +1,106 @@
+import argparse, asyncio, os
+from contextlib import asynccontextmanager
+from .cli import parse_permission, FileReadPermission
+from ..eng.utils import parse_storage_size, fmt_storage_size
+from ..eng.datatype import AccessLevel
+from ..eng.database import Database, FileReadPermission, transaction, UserConn, unique_cursor, FileConn
+from ..eng.connection_pool import global_entrance
+from ..eng.userman import UserCtl, parse_access_level
+
+@global_entrance(1)
+async def _main():
+    parser = argparse.ArgumentParser()
+    sp = parser.add_subparsers(dest='subparser_name', required=True)
+    sp_add = sp.add_parser('add')
+    sp_add.add_argument('username', type=str)
+    sp_add.add_argument('password', nargs='?', type=str, default=None)
+    sp_add.add_argument('--admin', action='store_true', help='Set user as admin')
+    sp_add.add_argument("--permission", type=parse_permission, default=FileReadPermission.UNSET, help="File fallback read permission, can be public, protected, private, or unset")
+    sp_add.add_argument('--max-storage', type=parse_storage_size, default="10G", help="Maximum storage size, e.g. 1G, 100M, 10K, default is 10G")
+    
+    sp_delete = sp.add_parser('delete')
+    sp_delete.add_argument('username', type=str)
+
+    def parse_bool(s):
+        if s.lower() == 'true':
+            return True
+        if s.lower() == 'false':
+            return False
+        raise ValueError('Not a boolean')
+    sp_set = sp.add_parser('set', help="Set user properties, refer to 'add' subcommand for details on each argument")
+    sp_set.add_argument('username', type=str)
+    sp_set.add_argument('-p', '--password', type=str, default=None)
+    sp_set.add_argument('-a', '--admin', type=parse_bool, default=None)
+    sp_set.add_argument('--permission', type=parse_permission, default=None)
+    sp_set.add_argument('--max-storage', type=parse_storage_size, default=None)
+
+    sp_list = sp.add_parser('list')
+    sp_list.add_argument("username", nargs='*', type=str, default=None)
+    sp_list.add_argument("-l", "--long", action="store_true", help="Show detailed information, including credential and peer users")
+    
+    sp_peer = sp.add_parser('set-peer')
+    sp_peer.add_argument('src_username', type=str)
+    sp_peer.add_argument('dst_username', type=str)
+    sp_peer.add_argument('--level', type=parse_access_level, default=AccessLevel.READ, help="Access level")
+    
+    args = parser.parse_args()
+    db = await Database().init()
+
+    @asynccontextmanager
+    async def get_uconn():
+        async with transaction() as conn:
+            yield UserConn(conn)
+    
+    if args.subparser_name == 'add':
+        user = await UserCtl.add(
+            username=args.username, 
+            password=args.password, 
+            admin=args.admin, 
+            max_storage=args.max_storage, 
+            permission=args.permission
+        )
+        print('User created, credential:', user.credential)
+    
+    if args.subparser_name == 'delete':
+        user = await UserCtl.delete(args.username)
+        print('User deleted')
+    
+    if args.subparser_name == 'set':
+        user = await UserCtl.update(
+            username=args.username,
+            password=args.password,
+            admin=args.admin,
+            max_storage=args.max_storage,
+            permission=args.permission
+        )
+        print('User updated, credential:', user.credential)
+    
+    if args.subparser_name == 'set-peer':
+        await UserCtl.set_peer(args.src_username, args.dst_username, args.level)
+        print(f"Peer set: [{args.src_username}] now have [{args.level.name}] access to [{args.dst_username}]")
+    
+    if args.subparser_name == 'list':
+        async with get_uconn() as uconn:
+            term_width = os.get_terminal_size().columns
+            async for user in uconn.all():
+                if args.username and not user.username in args.username:
+                    continue
+                print("\033[90m-\033[0m" * term_width)
+                print(user)
+                if args.long:
+                    async with unique_cursor() as c:
+                        fconn = FileConn(c)
+                        user_size_used = await fconn.user_size(user.id)
+                    print('- Credential: ', user.credential)
+                    print(f'- Storage: {fmt_storage_size(user_size_used)} / {fmt_storage_size(user.max_storage)}')
+                    for p in AccessLevel:
+                        if p > AccessLevel.NONE:
+                            usernames = [x.username for x in await uconn.list_peer_users(user.id, p)]
+                            if usernames:
+                                print(f'- Peers [{p.name}]: {", ".join(usernames)}')
+        
+def main():
+    asyncio.run(_main())
+
+if __name__ == '__main__':
+    main()
---
+++ packages/lfss/lfss-0.15.4-py3-none-any.whl/lfss/eng/userman.py
@@ -0,0 +1,119 @@
+"""
+High level user-management API.
+"""
+
+from typing import Optional
+from .utils import parse_storage_size
+from .datatype import UserRecord, FileReadPermission, AccessLevel
+from .connection_pool import transaction, unique_cursor
+from .database_conn import UserConn
+from .database import Database
+from .error import *
+
+async def _ensure_user_exists(u: str | int) -> UserRecord:
+    async with unique_cursor() as conn:
+        uconn = UserConn(conn)
+        if isinstance(u, str):
+            user = await uconn.get_user(u)
+        else:
+            user = await uconn.get_user_by_id(u)
+        if user is None:
+            raise UserNotFoundError(f"User {u} not found")
+    return user
+
+def parse_access_level(s: str) -> AccessLevel:
+    for p in AccessLevel:
+        if p.name.lower() == s.lower():
+            return p
+    raise ValueError(f"Invalid access level {s}")
+
+def parse_permission(s: str) -> FileReadPermission:
+    for p in FileReadPermission:
+        if p.name.lower() == s.lower():
+            return p
+    raise ValueError(f"Invalid file read permission {s}")
+
+class UserCtl:
+
+    @staticmethod
+    async def get_user(u: str | int) -> UserRecord:
+        return await _ensure_user_exists(u)
+
+    @staticmethod
+    async def add(
+        username: str, 
+        password: Optional[str] = None, 
+        admin: bool = False, 
+        max_storage: int | str = '100G', 
+        permission: FileReadPermission | str = 'unset'
+        ) -> UserRecord:
+        """
+        Add a new user to the system.
+        if password is None, a random password will be generated.
+        Returns the created UserRecord.
+        """
+        if password is None:
+            import secrets
+            password = secrets.token_urlsafe(16)
+        if isinstance(max_storage, str):
+            max_storage = parse_storage_size(max_storage)
+        if isinstance(permission, str):
+            permission = parse_permission(permission)
+        async with transaction() as conn:
+            uconn = UserConn(conn)
+            user_id = await uconn.create_user(username, password, admin, max_storage=max_storage, permission=permission)
+            user = await uconn.get_user_by_id(user_id)
+            assert user is not None
+            return user
+    
+    @staticmethod
+    async def delete(username: str) -> UserRecord:
+        """ Delete a user from the system"""
+        await _ensure_user_exists(username)
+        return await Database().delete_user(username)
+    
+    @staticmethod
+    async def update(
+        username: str, 
+        password: Optional[str] = None, 
+        admin: Optional[bool] = None, 
+        max_storage: Optional[int | str] = None, 
+        permission: Optional[FileReadPermission | str] = None
+        ) -> UserRecord:
+        """
+        Update user information.
+        Returns the updated UserRecord.
+        """
+        await _ensure_user_exists(username)
+        if isinstance(max_storage, str):
+            max_storage = parse_storage_size(max_storage)
+        if isinstance(permission, str):
+            permission = parse_permission(permission)
+        async with transaction() as conn:
+            uconn = UserConn(conn)
+            await uconn.update_user(
+                username=username, 
+                password=password, 
+                is_admin=admin, 
+                max_storage=max_storage, 
+                permission=permission
+            )
+            user = await uconn.get_user(username)
+            assert user is not None
+            return user
+    
+    @staticmethod
+    async def set_peer(src_username: str, dst_username: str, level: AccessLevel | str):
+        """
+        Set peer access level from src_username to dst_username.
+        So that [src_username] can do [level] to [dst_username].
+        """
+        src_user = await _ensure_user_exists(src_username)
+        dst_user = await _ensure_user_exists(dst_username)
+        if isinstance(level, str):
+            level = parse_access_level(level)
+        assert_or(level != AccessLevel.GUEST, lambda: InvalidInputError("Cannot set peer access level to GUEST"))
+        assert_or(level != AccessLevel.ALL, lambda: InvalidInputError("Cannot set peer access level to ALL"))
+        async with transaction() as conn:
+            uconn = UserConn(conn)
+            await uconn.set_peer_level(src_user.id, dst_user.id, level)
