68185e45ae02eaa8848901d5abda46b899c25a28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:24:45 +0000

    Add package vicutils/0.4/vicutils-0.4.tar.gz

---
+++ packages/vicutils/vicutils-0.4.tar.gz/vicutils-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vicutils",             # package name
+    version="0.4",
+    packages=find_packages(),    # finds vicutils folder automatically
+    install_requires=[],         # optional, for dependencies
+)
