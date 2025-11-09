a8e2e1319b0f5b098f1ef2eddd0a2423efd45c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-09 03:25:21 +0000

    Add package pywheels/0.8.1/pywheels-0.8.1-py3-none-any.whl

---
+++ packages/pywheels/pywheels-0.8.1-py3-none-any.whl/pywheels/typing.py
@@ -0,0 +1,24 @@
+from typing import Any
+from typing import Set
+from typing import Dict
+from typing import List
+from typing import Tuple
+from typing import Union
+from typing import TypeVar
+from typing import Literal
+from typing import Callable
+from typing import Optional
+
+
+__all__ = [
+    "Any",
+    "Set",
+    "Dict",
+    "List",
+    "Tuple",
+    "Union",
+    "TypeVar",
+    "Literal",
+    "Callable",
+    "Optional",
+]
