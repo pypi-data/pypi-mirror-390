4af0358b826566f739ed252a48006b5c26d3ecf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:05:51 +0000

    Add package pytest-cocotb/0.1.3/pytest_cocotb-0.1.3.tar.gz

---
+++ packages/pytest-cocotb/pytest_cocotb-0.1.3.tar.gz/pytest_cocotb-0.1.3/README.md
@@ -0,0 +1,9 @@
+<!-- SPDX-FileCopyrightText: 2025 Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com> -->
+<!-- SPDX-License-Identifier: Apache-2.0 -->
+
+# pytest-cocotb
+
+Pytest plugin that enables using [pytest][] as the regression manager for running [cocotb][] tests.
+
+[pytest]: https://docs.pytest.org
+[cocotb]: https://www.cocotb.org
---
+++ packages/pytest-cocotb/pytest_cocotb-0.1.3.tar.gz/pytest_cocotb-0.1.3/src/pytest_cocotb/plugin.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2025 Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
+# SPDX-License-Identifier: Apache-2.0
+
+"""Enabling the :py:mod:`cocotb_tools.pytest.plugin` that allows to use
+`pytest`_ as the regression manager for running `cocotb`_ tests.
+
+.. note::
+
+    Plugin will be atuomatically enabled via entry points defined by this package.
+
+.. _pytest: https://docs.pytest.org
+.. _cocotb: https://www.cocotb.org
+"""
