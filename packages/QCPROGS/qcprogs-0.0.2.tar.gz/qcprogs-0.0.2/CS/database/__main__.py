2e2b309eb1c93f1624a66ffeb7bca613fdceba02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:05:58 +0000

    Add package QCPROGS/0.0.2/qcprogs-0.0.2.tar.gz

---
+++ packages/QCPROGS/qcprogs-0.0.2.tar.gz/qcprogs-0.0.2/CS/database/__main__.py
@@ -0,0 +1,10 @@
+import sys, os
+sys.path.append(os.path.dirname(__file__)) 
+import flet as ft
+from UI import DataUI
+def main(page: ft.Page):
+    ui = DataUI(page)
+    page.title = "Data Query UI"
+    page.add(ui.build())
+def start():
+    ft.app(target=main)
