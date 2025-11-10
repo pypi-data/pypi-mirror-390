6415f1b01672b972dad547f4b122c0fcc9e40f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:10:32 +0000

    Add package Hydro-Quebec-API-Wrapper/4.1.0/hydro_quebec_api_wrapper-4.1.0-py3-none-any.whl

---
+++ packages/Hydro-Quebec-API-Wrapper/hydro_quebec_api_wrapper-4.1.0-py3-none-any.whl/hydroqc/peak/cpc/consts.py
@@ -0,0 +1,17 @@
+"""Constants module for CPC."""
+
+# from typing import Literal
+import datetime
+
+DEFAULT_MORNING_ANCHOR_START_OFFSET = 5
+DEFAULT_MORNING_ANCHOR_DURATION = 3
+DEFAULT_EVENING_ANCHOR_START_OFFSET = 4
+DEFAULT_EVENING_ANCHOR_DURATION = 2
+DEFAULT_EVENT_REFRESH_SECONDS = 300
+DEFAULT_PRE_HEAT_DURATION = 180
+DEFAULT_MORNING_PEAK_START = datetime.time(6, 0, 0)
+DEFAULT_MORNING_PEAK_END = datetime.time(10, 0, 0)
+DEFAULT_EVENING_PEAK_START = datetime.time(16, 0, 0)
+DEFAULT_EVENING_PEAK_END = datetime.time(20, 0, 0)
+# MORNING_EVENING: tuple[Literal["morning"], Literal["evening"]] = ("morning", "evening")
+MORNING_EVENING = ("morning", "evening")
