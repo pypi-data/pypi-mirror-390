59ec972c80d1838649ef7904856bed127f716882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:10:44 +0000

    Add package gi-loadouts/0.1.12/gi_loadouts-0.1.12-py3-none-any.whl

---
+++ packages/gi-loadouts/gi_loadouts-0.1.12-py3-none-any.whl/gi_loadouts/type/arti/pmod.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from .base import Artifact, MainStatType_PMOD, SecoStat
+from .stat import attack
+
+
+class PMOD(Artifact):
+    """
+    Artifact primitive for a "Plume of Death" artifact
+    """
+
+    stat_name: Optional[MainStatType_PMOD] = MainStatType_PMOD.attack
+    secostat_a: Optional[SecoStat] = SecoStat()
+    secostat_b: Optional[SecoStat] = SecoStat()
+    secostat_c: Optional[SecoStat] = SecoStat()
+    secostat_d: Optional[SecoStat] = SecoStat()
+
+    @property
+    def stat_data(self) -> float:
+        """
+        Calculate the statistics associated with the artifact mainstat based on the artifact quality and artifact level
+
+        :return:
+        """
+        return attack[self.rare]["init"] + sum(attack[self.rare]["diff"][0 : self.levl + 1])  # noqa : E501
+
+
+__revmap__ = {"ATK": MainStatType_PMOD.attack}
