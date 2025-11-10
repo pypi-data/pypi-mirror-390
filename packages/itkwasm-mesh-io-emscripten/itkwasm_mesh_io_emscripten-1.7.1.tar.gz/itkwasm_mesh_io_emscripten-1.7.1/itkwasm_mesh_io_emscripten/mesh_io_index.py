f58359eec0a7fbfb4c136c477158108c7e8ee492
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:11:27 +0000

    Add package itkwasm-mesh-io-emscripten/1.7.1/itkwasm_mesh_io_emscripten-1.7.1.tar.gz

---
+++ packages/itkwasm-mesh-io-emscripten/itkwasm_mesh_io_emscripten-1.7.1.tar.gz/itkwasm_mesh_io_emscripten-1.7.1/itkwasm_mesh_io_emscripten/mesh_io_index.py
@@ -0,0 +1,14 @@
+mesh_io_index = [
+    'vtk_poly_data',
+    'byu',
+    'free_surfer_ascii',
+    'free_surfer_binary',
+    'mz3',
+    'obj',
+    'off',
+    'stl',
+    'swc',
+    'wasm',
+    'wasm_zstd',
+]
+
