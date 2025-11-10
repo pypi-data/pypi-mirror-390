42b07985e01d050304b67b3b98581a00e87a527f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:16 +0000

    Add package import-linter/2.6/import_linter-2.6.tar.gz

---
+++ packages/import-linter/import_linter-2.6.tar.gz/import_linter-2.6/src/importlinter/adapters/printing.py
@@ -0,0 +1,21 @@
+import click
+
+from importlinter.application.ports.printing import Printer
+
+
+class ClickPrinter(Printer):
+    """
+    Console printer that uses Click's formatting helpers.
+    """
+
+    def print(
+        self,
+        text: str = "",
+        bold: bool = False,
+        color: str | None = None,
+        newline: bool = True,
+    ) -> None:
+        # click.secho(text, bold=bold, fg=color, nl=newline)
+        # A tricky solution to gh-267
+        # Remove when click reaches v9.0.0
+        print(click.style(text, bold=bold, fg=color), end=("\n" if newline else ""))
