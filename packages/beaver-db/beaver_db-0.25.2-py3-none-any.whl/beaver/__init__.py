12e75084a0e442052ba579eac6e7a6cce2901f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 05:07:44 +0000

    Add package beaver-db/0.25.2/beaver_db-0.25.2-py3-none-any.whl

---
+++ packages/beaver-db/beaver_db-0.25.2-py3-none-any.whl/beaver/__init__.py
@@ -0,0 +1,12 @@
+from .core import BeaverDB
+from .types import Model
+from .collections import Document, WalkDirection
+
+__version__ = "0.25.2"
+
+__all__ = [
+    "BeaverDB",
+    "Model",
+    "Document",
+    "WalkDirection",
+]
