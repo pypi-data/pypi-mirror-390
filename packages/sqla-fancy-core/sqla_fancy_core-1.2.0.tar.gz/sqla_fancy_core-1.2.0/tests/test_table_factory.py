963922e5f9857ff16dd110378de62bf8d4853da6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:42 +0000

    Add package sqla-fancy-core/1.2.0/sqla_fancy_core-1.2.0.tar.gz

---
+++ packages/sqla-fancy-core/sqla_fancy_core-1.2.0.tar.gz/sqla_fancy_core-1.2.0/tests/test_table_factory.py
@@ -0,0 +1,74 @@
+def test_table_factory():
+    import sqlalchemy as sa
+
+    from sqla_fancy_core import TableFactory
+
+    tf = TableFactory()
+
+    # Define a table
+    class Author:
+        id = tf.auto_id()
+        name = tf.string("name")
+        created_at = tf.created_at()
+        updated_at = tf.updated_at()
+
+        Table = tf("author")
+
+    # Or define it without losing type hints
+    class Book:
+        id = tf(sa.Column("id", sa.Integer, primary_key=True, autoincrement=True))
+        title = tf(sa.Column("title", sa.String(255), nullable=False))
+        author_id = tf(sa.Column("author_id", sa.Integer, sa.ForeignKey(Author.id)))
+        created_at = tf(
+            sa.Column(
+                "created_at",
+                sa.DateTime,
+                nullable=False,
+                server_default=sa.func.now(),
+            )
+        )
+        updated_at = tf(
+            sa.Column(
+                "updated_at",
+                sa.DateTime,
+                nullable=False,
+                server_default=sa.func.now(),
+                onupdate=sa.func.now(),
+            )
+        )
+
+        Table = tf(sa.Table("book", sa.MetaData()))
+
+    # Create the engine
+    engine = sa.create_engine("sqlite:///:memory:")
+
+    # Create the tables
+    tf.metadata.create_all(engine)
+
+    with engine.begin() as txn:
+        # Insert author
+        qry = (
+            sa.insert(Author.Table)
+            .values({Author.name: "John Doe"})
+            .returning(Author.id)
+        )
+        author = txn.execute(qry).mappings().one()
+        author_id = author[Author.id]
+        assert author_id == 1
+
+        # Insert book
+        qry = (
+            sa.insert(Book.Table)
+            .values({Book.title: "My Book", Book.author_id: author_id})
+            .returning(Book.id)
+        )
+        book = txn.execute(qry).mappings().one()
+        assert book[Book.id] == 1
+
+        # Query the data
+        qry = sa.select(Author.name, Book.title).join(
+            Book.Table,
+            Book.author_id == Author.id,
+        )
+        result = txn.execute(qry).all()
+        assert result == [("John Doe", "My Book")], result
