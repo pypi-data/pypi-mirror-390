72f9ce5c401d8d0ef480e0f85b3ccbe0071f88fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:20 +0000

    Add package remotivelabs-broker/0.10.0/remotivelabs_broker-0.10.0-py3-none-any.whl

---
+++ packages/remotivelabs-broker/remotivelabs_broker-0.10.0-py3-none-any.whl/remotivelabs/broker/__init__.py
@@ -0,0 +1,70 @@
+"""
+# RemotiveLabs Broker API
+This module handles communication with the RemotiveBroker.
+
+The RemotiveBroker can run locally or in RemotiveCloud. Connecting to a RemotiveCloud instance requires additional
+authentication parameters—see the `remotivelabs.broker.auth` module for details.
+
+## Installation
+
+.. code-block:: bash
+    pip install remotivelabs-broker
+
+## Logging
+
+This library uses Python's standard `logging` module. By default, the library does not configure any logging handlers,
+allowing applications to fully control their logging setup.
+
+To enable logs from this library in your application or tests, configure logging as follows:
+
+```python
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("remotivelabs.broker").setLevel(logging.DEBUG)
+```
+
+For more advanced configurations, refer to the [Python logging documentation](https://docs.python.org/3/library/logging.html).
+
+"""
+
+import logging
+
+from remotivelabs.broker import auth, exceptions, restbus
+from remotivelabs.broker.__about__ import __version__
+from remotivelabs.broker.client import BrokerClient
+from remotivelabs.broker.frame import Frame, FrameInfo, FrameName, FrameSubscription, Header
+from remotivelabs.broker.namespace import NamespaceInfo, NamespaceName
+from remotivelabs.broker.restbus import RestbusFrameConfig, RestbusSignalConfig
+from remotivelabs.broker.secoc import SecocCmac0, SecocFreshnessValue, SecocKey, SecocProperty, SecocTimeDiff
+from remotivelabs.broker.signal import Signal, SignalInfo, SignalName, SignalValue, WriteSignal
+
+__all__ = [
+    "__version__",
+    "BrokerClient",
+    "restbus",
+    "auth",
+    "exceptions",
+    "Frame",
+    "FrameInfo",
+    "FrameName",
+    "Header",
+    "FrameSubscription",
+    "Signal",
+    "SignalInfo",
+    "SignalName",
+    "SignalValue",
+    "WriteSignal",
+    "RestbusFrameConfig",
+    "RestbusSignalConfig",
+    "NamespaceInfo",
+    "NamespaceName",
+    "SecocCmac0",
+    "SecocFreshnessValue",
+    "SecocKey",
+    "SecocProperty",
+    "SecocTimeDiff",
+]
+
+
+logging.getLogger("remotivelabs.broker").addHandler(logging.NullHandler())
