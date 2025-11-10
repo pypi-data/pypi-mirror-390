840f487d66187b99e6c8a7928d274168fa2d63ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:11:17 +0000

    Add package polylith-cli/1.38.0/polylith_cli-1.38.0-py3-none-any.whl

---
+++ packages/polylith-cli/polylith_cli-1.38.0-py3-none-any.whl/polylith_cli/polylith/project/__init__.py
@@ -0,0 +1,5 @@
+from polylith_cli.polylith.project import interactive
+from polylith_cli.polylith.project.create import create_project
+from polylith_cli.polylith.project.get import get_packages_for_projects, get_project_name, get_project_template, get_toml
+from polylith_cli.polylith.project.parser import parse_package_paths
+__all__ = ['create_project', 'get_packages_for_projects', 'get_project_name', 'get_project_template', 'get_toml', 'interactive', 'parse_package_paths']
