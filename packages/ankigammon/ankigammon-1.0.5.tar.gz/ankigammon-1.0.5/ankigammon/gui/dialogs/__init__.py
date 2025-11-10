3ce86bd7cc37b4db6a4e58e8e86ade8f3d4be5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:11:17 +0000

    Add package ankigammon/1.0.5/ankigammon-1.0.5.tar.gz

---
+++ packages/ankigammon/ankigammon-1.0.5.tar.gz/ankigammon-1.0.5/ankigammon/gui/dialogs/__init__.py
@@ -0,0 +1,10 @@
+"""
+GUI dialogs package.
+"""
+
+__all__ = ['SettingsDialog', 'ExportDialog', 'InputDialog', 'ImportOptionsDialog']
+
+from .settings_dialog import SettingsDialog
+from .export_dialog import ExportDialog
+from .input_dialog import InputDialog
+from .import_options_dialog import ImportOptionsDialog
