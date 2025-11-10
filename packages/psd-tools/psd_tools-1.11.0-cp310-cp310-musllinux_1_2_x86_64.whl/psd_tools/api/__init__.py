979cd13c1fd070da294582d128819cfa1a62b1a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:10:54 +0000

    Add package psd-tools/1.11.0/psd_tools-1.11.0-cp310-cp310-musllinux_1_2_x86_64.whl

---
+++ packages/psd-tools/psd_tools-1.11.0-cp310-cp310-musllinux_1_2_x86_64.whl/psd_tools/api/__init__.py
@@ -0,0 +1,17 @@
+import functools
+import warnings
+
+
+def deprecated(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        warnings.simplefilter("always", DeprecationWarning)
+        warnings.warn(
+            "%r is deprecated" % (func.__name__),
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        warnings.simplefilter("default", DeprecationWarning)
+        return func(*args, **kwargs)
+
+    return wrapper
