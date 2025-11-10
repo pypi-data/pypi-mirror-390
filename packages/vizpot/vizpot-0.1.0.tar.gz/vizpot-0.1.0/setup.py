bd5666243936bdf94700f414730cdc11e96f8500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 03:32:20 +0000

    Add package vizpot/0.1.0/vizpot-0.1.0.tar.gz

---
+++ packages/vizpot/vizpot-0.1.0.tar.gz/vizpot-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="vizpot",
+    version="0.1.0",
+    description="Collection of visualization notebooks",
+    packages=find_packages(),
+    include_package_data=True,
+)
