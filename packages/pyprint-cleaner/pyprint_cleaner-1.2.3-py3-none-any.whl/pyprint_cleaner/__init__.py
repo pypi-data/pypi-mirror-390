44ed327d6c8b664476703d0a10ae8a716f0cac6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 15:06:42 +0000

    Add package pyprint-cleaner/1.2.3/pyprint_cleaner-1.2.3-py3-none-any.whl

---
+++ packages/pyprint-cleaner/pyprint_cleaner-1.2.3-py3-none-any.whl/pyprint_cleaner/__init__.py
@@ -0,0 +1,5 @@
+from .comment_prints import comment_unnecessary_prints
+from .uncomment_prints import uncomment_prints
+
+__version__ = "1.2.3"
+__all__ = ["comment_unnecessary_prints", "uncomment_prints"]
