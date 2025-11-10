aefbc7009b73445ff1a978fef969207c6bf4366e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 14:06:50 +0000

    Add package osbot-utils/3.26.0/osbot_utils-3.26.0.tar.gz

---
+++ packages/osbot-utils/osbot_utils-3.26.0.tar.gz/osbot_utils-3.26.0/osbot_utils/type_safe/type_safe_core/collections/Type_Safe__Dict.py
@@ -0,0 +1,98 @@
+from enum import Enum
+from typing                                                           import Type
+from osbot_utils.testing.__                                           import __
+from osbot_utils.type_safe.Type_Safe__Base                            import Type_Safe__Base
+from osbot_utils.type_safe.Type_Safe__Primitive                       import Type_Safe__Primitive
+from osbot_utils.type_safe.type_safe_core.collections.Type_Safe__List import Type_Safe__List
+from osbot_utils.utils.Objects                                        import class_full_name, serialize_to_dict
+
+
+class Type_Safe__Dict(Type_Safe__Base, dict):
+    def __init__(self, expected_key_type, expected_value_type, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.expected_key_type   = expected_key_type
+        self.expected_value_type = expected_value_type
+
+    def __contains__(self, key):
+        if super().__contains__(key):                                       # First try direct lookup
+            return True
+
+        try:                                                                # Then try with type conversion
+            converted_key = self.try_convert(key, self.expected_key_type)
+            return super().__contains__(converted_key)
+        except (ValueError, TypeError):
+            return False
+
+    def __getitem__(self, key):
+        try:
+            return super().__getitem__(key)                                     # First try direct lookup
+        except KeyError:
+            converted_key = self.try_convert(key, self.expected_key_type)       # Try converting the key
+            return super().__getitem__(converted_key)                           # and compare again
+
+    def __setitem__(self, key, value):                                          # Check type-safety before allowing assignment.
+        key   = self.try_convert(key  , self.expected_key_type  )
+        value = self.try_convert(value, self.expected_value_type)
+        self.is_instance_of_type(key  , self.expected_key_type)
+        self.is_instance_of_type(value, self.expected_value_type)
+        super().__setitem__(key, value)
+
+    def __enter__(self): return self
+    def __exit__ (self, type, value, traceback): pass
+
+    def json(self):
+        from osbot_utils.type_safe.Type_Safe import Type_Safe
+
+        def serialize_value(v):
+            """Recursively serialize values, handling nested structures"""
+            if isinstance(v, Type_Safe):
+                return v.json()
+            elif isinstance(v, Type_Safe__Primitive):
+                return v.__to_primitive__()
+            elif isinstance(v, type):
+                return class_full_name(v)
+            elif isinstance(v, dict):
+                # Recursively handle nested dictionaries
+                return {k2: serialize_value(v2) for k2, v2 in v.items()}
+            elif isinstance(v, (list, tuple, set, frozenset)):
+                # Recursively handle sequences
+                serialized = [serialize_value(item) for item in v]
+                if isinstance(v, list):
+                    return serialized
+                elif isinstance(v, tuple):
+                    return tuple(serialized)
+                else:  # set
+                    return set(serialized)
+            else:
+                return serialize_to_dict(v)                             # Use serialize_to_dict for unknown types (so that we don't return a non json object)
+
+
+        result = {}
+        for key, value in self.items():
+            if isinstance(key, (type, Type)):                           # Handle Type objects as keys
+                key = f"{key.__module__}.{key.__name__}"
+            elif isinstance(key, Enum):                                 #  Handle Enum keys
+                key = key.value
+            elif isinstance(key, Type_Safe__Primitive):
+                key = key.__to_primitive__()
+
+            result[key] = serialize_value(value)                        # Use recursive serialization for values
+
+        return result
+
+    def get(self, key, default=None):       # this makes it consistent with the modified behaviour of __get__item
+        try:
+            return self[key]                # Use __getitem__ with conversion
+        except KeyError:
+            return default                  # Return default instead of raising
+
+    def keys(self) -> Type_Safe__List:
+        return Type_Safe__List(self.expected_key_type, super().keys())
+
+    def obj(self) -> __:
+        from osbot_utils.testing.__helpers import dict_to_obj
+        return dict_to_obj(self.json())
+
+    def values(self) -> Type_Safe__List:
+        return Type_Safe__List(self.expected_value_type, super().values())
