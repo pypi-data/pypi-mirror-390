52a8d46ac7e539eeed2bbbd0ceea4f28d1b79268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:05:47 +0000

    Add package pydisort/1.3.6/pydisort-1.3.6-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/pydisort/pydisort-1.3.6-cp313-cp313-manylinux_2_27_x86_64.manylinux_2_28_x86_64.whl/pydisort/disort/index.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace disort {
+
+namespace index {
+constexpr int IEX = 0;
+constexpr int ISS = 1;
+constexpr int IPM = 2;
+
+constexpr int IUP = 0;
+constexpr int IDN = 1;
+}  // namespace index
+
+}  // namespace disort
