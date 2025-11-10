290d8053558432d0e3ed227ade1e77d79c01a0ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:12:29 +0000

    Add package pycmd2/0.5.10/pycmd2-0.5.10-cp38-abi3-win_amd64.whl

---
+++ packages/pycmd2/pycmd2-0.5.10-cp38-abi3-win_amd64.whl/pycmd2/dev/env_javascript.py
@@ -0,0 +1,37 @@
+"""功能: 初始化 python 环境变量."""
+
+from __future__ import annotations
+
+import logging
+
+from typer import Argument
+from typing_extensions import Annotated
+
+from pycmd2.client import get_client
+
+cli = get_client()
+logger = logging.getLogger(__name__)
+
+
+NODE_VERSIONS: dict[str, str] = {
+    "V20": "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -",
+    "V18": "curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -",
+}
+
+
+def install_nodejs(node_ver: str) -> None:
+    cli.run_cmdstr(NODE_VERSIONS.get(node_ver, ""))
+
+
+@cli.app.command()
+def main(
+    node_ver: Annotated[
+        str,
+        Argument(help=f"nodejs 版本: {NODE_VERSIONS.keys()}"),
+    ] = "V18",
+) -> None:
+    if cli.is_windows:
+        logger.error("当前系统为windows, 请下载压缩包直接安装")
+        return
+
+    install_nodejs(node_ver)
