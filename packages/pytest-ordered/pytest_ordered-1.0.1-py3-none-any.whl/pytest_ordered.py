c356d1ed0257e50ed3db31bf4a70ddd152e554ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 14:06:33 +0000

    Add package pytest-ordered/1.0.1/pytest_ordered-1.0.1-py3-none-any.whl

---
+++ packages/pytest-ordered/pytest_ordered-1.0.1-py3-none-any.whl/pytest_ordered.py
@@ -0,0 +1,40 @@
+import re
+
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addini("order", "The order in which to run tests.", type="linelist")
+
+
+def pytest_collection_modifyitems(session, config, items):
+    if not config.getini("order"):
+        return
+    order = expand_test_order("\n".join(config.getini("order")))
+    ranks = []
+    errors = set()
+    for test in items:
+        location = test.nodeid.split("::")[0]
+        try:
+            rank = order.index(location)
+        except ValueError:
+            errors.add(location)
+            continue
+        ranks.append(rank)
+    if errors:
+        raise pytest.UsageError(
+            f'Test files {list(errors)} do not match any of the '
+            f'test file paths listed in the "order" section of the '
+            f'pytest.ini/setup.cfg file.')
+    args = sorted(range(len(items)), key=ranks.__getitem__)
+    items[:] = [items[i] for i in args]
+
+
+def expand_test_order(x):
+    lines = re.findall("-( *)(.+)", x)
+    out = []
+    levels = {}
+    for _prefix, _name in lines:
+        levels[len(_prefix)] = _name
+        out.append("".join(j for (i, j) in levels.items() if i <= len(_prefix)))
+    return out
