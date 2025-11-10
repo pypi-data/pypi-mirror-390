7da8c4486b953d468f4160c78915cd8270fef196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 14:06:38 +0000

    Add package pytest-cocotb-pyuvm/0.1.0/pytest_cocotb_pyuvm-0.1.0-py3-none-any.whl

---
+++ packages/pytest-cocotb-pyuvm/pytest_cocotb_pyuvm-0.1.0-py3-none-any.whl/pyuvm/plugin.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2025 Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
+# SPDX-License-Identifier: Apache-2.0
+
+"""Pytest plugin that enables using pytest as the regression manager for running pyuvm tests."""
