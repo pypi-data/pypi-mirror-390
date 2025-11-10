6b1120ecdc4f24e7909933d352dccf4c75d3ea35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:06:03 +0000

    Add package kroma/1.3.1/kroma-1.3.1-py3-none-any.whl

---
+++ packages/kroma/kroma-1.3.1-py3-none-any.whl/kroma/__init__.py
@@ -0,0 +1,13 @@
+from . import html_color
+from . import ansi_color
+from .utils import ansi_supported
+from .enums import ANSIColors, HTMLColors
+
+__name__ = "kroma"
+__all__ = [
+    "html_color",
+    "ansi_color",
+    "ansi_supported",
+    "ANSIColors",
+    "HTMLColors"
+]
