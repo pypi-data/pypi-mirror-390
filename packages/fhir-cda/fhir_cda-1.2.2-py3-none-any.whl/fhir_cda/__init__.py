72c94465d2742f4bd7810d36a285d7a3e249ee39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:23 +0000

    Add package fhir-cda/1.2.2/fhir_cda-1.2.2-py3-none-any.whl

---
+++ packages/fhir-cda/fhir_cda-1.2.2-py3-none-any.whl/fhir_cda/__init__.py
@@ -0,0 +1,11 @@
+from .core import Annotator
+from datetime import datetime
+
+__title__ = "fhir_cda"
+__version__ = "1.2.2"
+__author__ = "Linun Gao"
+__license__ = "Apache-2.0"
+__copyright__ = f"Copyright 2024-{datetime.now().year} ABI"
+
+# Version synonym
+VERSION = __version__
