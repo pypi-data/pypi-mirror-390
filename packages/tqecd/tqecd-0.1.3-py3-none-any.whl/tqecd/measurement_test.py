97edde0ec727ef090af941f0ec2a00006f5c9728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 14:06:34 +0000

    Add package tqecd/0.1.3/tqecd-0.1.3-py3-none-any.whl

---
+++ packages/tqecd/tqecd-0.1.3-py3-none-any.whl/tqecd/measurement_test.py
@@ -0,0 +1,40 @@
+import pytest
+
+from tqecd.exceptions import TQECDException
+from tqecd.measurement import (
+    RelativeMeasurementLocation,
+    get_relative_measurement_index,
+)
+
+
+def test_initialisation() -> None:
+    RelativeMeasurementLocation(-1, 1)
+
+    with pytest.raises(
+        TQECDException,
+        match=r"^Relative measurement offsets should be strictly negative\.$",
+    ):
+        RelativeMeasurementLocation(0, 1)
+    with pytest.raises(
+        TQECDException,
+        match=r"^Relative measurement offsets should be strictly negative\.$",
+    ):
+        RelativeMeasurementLocation(1, 1)
+
+
+def test_offset_by() -> None:
+    rml = RelativeMeasurementLocation(-1, 1)
+    assert rml.offset_by(-19) == RelativeMeasurementLocation(-20, 1)
+    with pytest.raises(
+        TQECDException,
+        match=r"^Relative measurement offsets should be strictly negative\.$",
+    ):
+        rml.offset_by(1)
+
+
+def test_get_relative_measurement_index() -> None:
+    all_measured_qubits = list(range(10))
+    for qubit in all_measured_qubits:
+        rml = get_relative_measurement_index(all_measured_qubits, qubit)
+        assert rml.qubit_index == qubit
+        assert rml.offset == qubit - len(all_measured_qubits)
