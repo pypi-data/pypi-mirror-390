364d58cdf71a82a17087c60b92c39a4b822edb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 15:06:29 +0000

    Add package phenom-api-management/0.0.16/phenom_api_management-0.0.16.tar.gz

---
+++ packages/phenom-api-management/phenom_api_management-0.0.16.tar.gz/phenom_api_management-0.0.16/phenom/entity/communications.py
@@ -0,0 +1,18 @@
+from phenom.api.communicationssms.sms_communications_api import SMSCommunicationsApi
+from phenom.api.communicationsemail.email_communications_api import EmailCommunicationsApi
+from phenom.api.communicationsemail.forward_profile_api import ForwardProfileApi
+
+class Communications(object):
+    def __init__(self, token, gateway_url, apikey=None):
+        self.__token = token
+        self.__gateway_url = gateway_url
+        self.__apikey = apikey
+
+    def sms_communications_api(self):
+        return SMSCommunicationsApi(self.__token, self.__gateway_url, self.__apikey)
+
+    def email_communications_api(self):
+        return EmailCommunicationsApi(self.__token, self.__gateway_url, self.__apikey)
+
+    def forward_profile_api(self):
+        return ForwardProfileApi(self.__token, self.__gateway_url, self.__apikey)
