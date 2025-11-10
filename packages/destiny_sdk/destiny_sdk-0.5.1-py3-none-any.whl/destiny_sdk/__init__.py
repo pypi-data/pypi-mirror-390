edab88e70edde82dde3d2327ce3704542a5aa95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 15:06:21 +0000

    Add package destiny_sdk/0.5.1/destiny_sdk-0.5.1-py3-none-any.whl

---
+++ packages/destiny_sdk/destiny_sdk-0.5.1-py3-none-any.whl/destiny_sdk/__init__.py
@@ -0,0 +1,23 @@
+"""The DESTINY SDK provides files for interacting with DESTINY repository."""
+
+from . import (
+    auth,
+    client,
+    enhancements,
+    identifiers,
+    imports,
+    references,
+    robots,
+    visibility,
+)
+
+__all__ = [
+    "auth",
+    "client",
+    "enhancements",
+    "identifiers",
+    "imports",
+    "references",
+    "robots",
+    "visibility",
+]
