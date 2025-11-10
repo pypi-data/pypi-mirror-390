1b1c29082f2b2b498958fa53df6f99db881c4213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 15:06:52 +0000

    Add package mblt-model-zoo/0.3.5/mblt_model_zoo-0.3.5-py3-none-any.whl

---
+++ packages/mblt-model-zoo/mblt_model_zoo-0.3.5-py3-none-any.whl/mblt_model_zoo/vision/instance_segmentation/__init__.py
@@ -0,0 +1,4 @@
+from .yolov5_seg import YOLOv5nSeg, YOLOv5sSeg, YOLOv5mSeg, YOLOv5lSeg, YOLOv5xSeg
+from .yolov8_seg import YOLOv8nSeg, YOLOv8sSeg, YOLOv8mSeg, YOLOv8lSeg, YOLOv8xSeg
+from .yolov9_seg import YOLOv9cSeg
+from .yolo11_seg import YOLO11nSeg, YOLO11sSeg, YOLO11mSeg, YOLO11lSeg, YOLO11xSeg
