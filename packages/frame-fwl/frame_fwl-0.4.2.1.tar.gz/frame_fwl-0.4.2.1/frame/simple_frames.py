62320761951f924e5050938358dd9c19c9921b3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 04:12:46 +0000

    Add package frame-fwl/0.4.2.1/frame_fwl-0.4.2.1.tar.gz

---
+++ packages/frame-fwl/frame_fwl-0.4.2.1.tar.gz/frame_fwl-0.4.2.1/frame/simple_frames.py
@@ -0,0 +1,10 @@
+'''
+# Simple Frames Core
+Simplified module of frame concept for beginers.
+'''
+
+
+from .frame_core import (Frame, Framer, framing, framing_result, fExec, fGet, fVar, fSys)
+from .frame_core.exceptions import *
+from .frame_core.plugins import (MathPlugin)
+from .frame_core.plugins_system import(PluginBase, PluginRegistry, register_plugin)
