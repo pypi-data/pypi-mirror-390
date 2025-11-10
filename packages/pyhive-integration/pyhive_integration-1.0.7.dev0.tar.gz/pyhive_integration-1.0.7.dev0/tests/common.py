bd73cc24c1b69fbde27d407f9e3ec4c50cf8ff1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:05:53 +0000

    Add package pyhive-integration/1.0.7.dev0/pyhive_integration-1.0.7.dev0.tar.gz

---
+++ packages/pyhive-integration/pyhive_integration-1.0.7.dev0.tar.gz/pyhive_integration-1.0.7.dev0/tests/common.py
@@ -0,0 +1,11 @@
+"""Mock services for tests."""
+
+# pylint: skip-file
+
+
+class MockConfig:
+    """Mock config for tests."""
+
+
+class MockDevice:
+    """Mock Device for tests."""
---
+++ packages/pyhive-integration/pyhive_integration-1.0.7.dev0.tar.gz/pyhive_integration-1.0.7.dev0/tests/test_hub.py
@@ -0,0 +1,8 @@
+"""Test hub framework."""
+
+
+def test_hub_smoke():
+    """Test for hub smoke."""
+    result = None
+
+    assert result
