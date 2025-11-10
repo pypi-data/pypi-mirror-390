b876fbdda71de450450cc65ccbf09545f994f7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:31 +0000

    Add package py-template-engine/0.2.5/py_template_engine-0.2.5-py3-none-any.whl

---
+++ packages/py-template-engine/py_template_engine-0.2.5-py3-none-any.whl/py_template_engine/sub_engines/FunctionTemplater.py
@@ -0,0 +1,27 @@
+import re
+from functools import reduce
+
+from py_template_engine.RenderError import RenderError
+from py_template_engine.TemplaterInterface import TemplaterInterface
+
+
+class FunctionTemplater(TemplaterInterface):
+    def render(self, template: str, **kwargs) -> str:
+        return re.sub(
+            r"{{(\w+(\.\w+)*)\(\)}}",
+            lambda m: self.process(m.group(1).strip(), **kwargs),
+            template,
+        )
+
+    def process(self, function_name: str, **kwargs) -> str:
+        try:
+            return reduce(
+                lambda acc, part: acc[part], function_name.split("."), kwargs
+            )()
+        except (KeyError, TypeError) as e:
+            if self.raise_on_error:
+                raise RenderError(
+                    f"Trying to insert function {function_name} but could not find {e}"
+                )
+            else:
+                return f"{{{{{function_name}()}}}}"
