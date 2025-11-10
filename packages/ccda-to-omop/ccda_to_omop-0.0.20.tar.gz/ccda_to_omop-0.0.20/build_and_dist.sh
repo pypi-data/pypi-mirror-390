c39ca730f388cfd9d747e03a92d1a52286e19fdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 05:06:23 +0000

    Add package ccda-to-omop/0.0.20/ccda_to_omop-0.0.20.tar.gz

---
+++ packages/ccda-to-omop/ccda_to_omop-0.0.20.tar.gz/ccda_to_omop-0.0.20/build_and_dist.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+VERSION="0.0.20"
+
+#python3 -m build
+/Users/croeder/git/ccda_to_omop_simple_driver/cto_driver_env/bin/python3 -m build
+
+
+#python3 -m twine upload dist/ccda_to_omop-${VERSION}-py3-none-any.whl dist/ccda_to_omop-${VERSION}.tar.gz
+/Users/croeder/git/ccda_to_omop_simple_driver/cto_driver_env/bin/python3 -m twine upload dist/ccda_to_omop-${VERSION}-py3-none-any.whl dist/ccda_to_omop-${VERSION}.tar.gz
+
