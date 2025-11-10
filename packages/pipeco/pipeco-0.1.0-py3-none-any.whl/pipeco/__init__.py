2dfa19d0f059391a52250497bd524087158d4310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:06:01 +0000

    Add package pipeco/0.1.0/pipeco-0.1.0-py3-none-any.whl

---
+++ packages/pipeco/pipeco-0.1.0-py3-none-any.whl/pipeco/__init__.py
@@ -0,0 +1,12 @@
+from .contracts import Step, Context
+from .pipeline import Pipeline, BaseModel
+from .registry import register, get_step
+
+__all__ = [
+    "Step",
+    "Context",
+    "Pipeline",
+    "BaseModel",
+    "register",
+    "get_step",
+]
