4e99fd508769a5b1e38985fb44495bc4e00184c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 14:06:28 +0000

    Add package noesis-core/0.0.0a1/noesis_core-0.0.0a1.tar.gz

---
+++ packages/noesis-core/noesis_core-0.0.0a1.tar.gz/noesis_core-0.0.0a1/README.md
@@ -0,0 +1,5 @@
+# Noēsis Core (placeholder)
+
+This is a placeholder package to reserve `noesis-core` on PyPI.
+
+For updates: https://github.com/saraeloop/noesis
---
+++ packages/noesis-core/noesis_core-0.0.0a1.tar.gz/noesis_core-0.0.0a1/src/noesis/__init__.py
@@ -0,0 +1,11 @@
+"""
+noesis-core (placeholder)
+
+Install:    pip install noesis-core
+Import:     import noesis   
+
+This is a temporary placeholder to reserve the name. The real API will land in >=0.1.0.
+Follow: https://github.com/saraeloop/noesis
+"""
+__all__ = []
+__version__ = "0.0.0a1"
