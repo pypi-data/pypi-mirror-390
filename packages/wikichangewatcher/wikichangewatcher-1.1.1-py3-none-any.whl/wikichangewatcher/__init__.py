a57a7b983c97ad6676af5b640f1f3447b1d3e3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 05:06:45 +0000

    Add package wikichangewatcher/1.1.1/wikichangewatcher-1.1.1-py3-none-any.whl

---
+++ packages/wikichangewatcher/wikichangewatcher-1.1.1-py3-none-any.whl/wikichangewatcher/__init__.py
@@ -0,0 +1,15 @@
+__author__ = "Erik Nyquist"
+__license__ = "Apache 2.0"
+__maintainer__ = "Erik Nyquist"
+__version__ = "1.1.1"
+__email__ = "eknyquist@gmail.com"
+
+from wikichangewatcher.wikichangewatcher import FieldFilter, FilterCollection, IpV4Filter, IpV6Filter, WikiChangeWatcher, MatchType
+from wikichangewatcher.wikichangewatcher import FieldRegexSearchFilter, UsernameRegexSearchFilter, PageUrlRegexSearchFilter
+
+import logging
+logging.basicConfig(format="[%(asctime)s][%(levelname)s][%(module)s:%(lineno)s]: %(message)s")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.WARNING)
+logger.info(f"wikichangewatcher {__version__}")
