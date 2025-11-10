81132004662bd79e1192d2f00395af384a267756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:06:41 +0000

    Add package eykturkish/0.1.13/eykturkish-0.1.13-py3-none-any.whl

---
+++ packages/eykturkish/eykturkish-0.1.13-py3-none-any.whl/eykturkish/basic.py
@@ -0,0 +1,19 @@
+import sys,os
+def girdi(nedenilcek = ">>"):
+    return input(nedenilcek)
+def crosscls():
+    if sys.platform.startswith("win"):
+        os.system("cls")
+    else:
+        os.system("clear")
+def baslik(yazi):
+    print(fr"----- {yazi} -----")
+def ehe(): # öylesine, eğlence için
+    print("ehe")
+def satirbasi():
+    print("\r")
+def renkmodunuac():
+    os.system('')
+def bossatir(sayi: 1):
+    for i in range(sayi):
+        print()
