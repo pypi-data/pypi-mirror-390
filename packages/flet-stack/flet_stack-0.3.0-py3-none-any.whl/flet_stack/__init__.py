ddf75c646f0668e1a90256d8bd6d433b7c7b9483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 03:31:28 +0000

    Add package flet-stack/0.3.0/flet_stack-0.3.0-py3-none-any.whl

---
+++ packages/flet-stack/flet_stack-0.3.0-py3-none-any.whl/flet_stack/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.3.0"
+__author__ = "Fasil"
+__email__ = "fasilwdr@hotmail.com"
+__license__ = "MIT"
+
+from .router import (
+    route,
+    FletStack
+)
+
+__all__ = [
+    "route",
+    "FletStack"
+]
