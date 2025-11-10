92d6cfe777ad639168e82019a6e97bb326791362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:24 +0000

    Add package adcp/1.3.0/adcp-1.3.0-py3-none-any.whl

---
+++ packages/adcp/adcp-1.3.0-py3-none-any.whl/adcp/testing/__init__.py
@@ -0,0 +1,38 @@
+"""Test helpers for AdCP client library.
+
+Provides pre-configured test agents for examples and quick testing.
+"""
+
+from __future__ import annotations
+
+from adcp.testing.test_helpers import (
+    CREATIVE_AGENT_CONFIG,
+    TEST_AGENT_A2A_CONFIG,
+    TEST_AGENT_A2A_NO_AUTH_CONFIG,
+    TEST_AGENT_MCP_CONFIG,
+    TEST_AGENT_MCP_NO_AUTH_CONFIG,
+    TEST_AGENT_TOKEN,
+    create_test_agent,
+    creative_agent,
+    test_agent,
+    test_agent_a2a,
+    test_agent_a2a_no_auth,
+    test_agent_client,
+    test_agent_no_auth,
+)
+
+__all__ = [
+    "test_agent",
+    "test_agent_a2a",
+    "test_agent_no_auth",
+    "test_agent_a2a_no_auth",
+    "creative_agent",
+    "test_agent_client",
+    "create_test_agent",
+    "TEST_AGENT_TOKEN",
+    "TEST_AGENT_MCP_CONFIG",
+    "TEST_AGENT_A2A_CONFIG",
+    "TEST_AGENT_MCP_NO_AUTH_CONFIG",
+    "TEST_AGENT_A2A_NO_AUTH_CONFIG",
+    "CREATIVE_AGENT_CONFIG",
+]
