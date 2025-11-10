49b8b58bba72e3c955b45c07a2324826c7216ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 03:32:44 +0000

    Add package numpay/0.0.7/numpay-0.0.7.tar.gz

---
+++ packages/numpay/numpay-0.0.7.tar.gz/numpay-0.0.7/numpay/__init__.py
@@ -0,0 +1,48 @@
+# ===============================
+# Root-Finding Methods
+# ===============================
+from .numpay import fixed_point
+from .numpay import bisection
+from .numpay import regula_falsi
+from .numpay import newton_raphson
+from .numpay import secant
+
+# ===============================
+# Linear Algebra Methods
+# ===============================
+from .numpay import linear
+from .numpay import lu_decomposition
+from .numpay import jacobi_iteration
+from .numpay import gauss_seidel
+
+# ===============================
+# Polynomial Methods
+# ===============================
+from .numpay import horner
+from .numpay import vander_matrix
+
+# ===============================
+# Interpolation Methods
+# ===============================
+from .numpay import interpolation
+from .numpay import least_squares
+
+# ===============================
+# Numerical Differentiation
+# ===============================
+from .numpay import differentiation
+
+# ===============================
+# Numerical Integration
+# ===============================
+from .numpay import integration
+
+# ===============================
+# Utility Functions
+# ===============================
+from .numpay import full
+from .numpay import doc
+
+
+
+
---
+++ packages/numpay/numpay-0.0.7.tar.gz/numpay-0.0.7/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='numpay',                # the package name
+    version='0.0.7',
+    description='helo',
+    author='aaaroo',
+    author_email='aaro@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+    python_requires='>=2.1',
+)
+
