997acca0e22d5c2724aab898a0d56f35c94f086b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 03:31:49 +0000

    Add package invoicingj-pdf/1.0.0/invoicingj_pdf-1.0.0.tar.gz

---
+++ packages/invoicingj-pdf/invoicingj_pdf-1.0.0.tar.gz/invoicingj-pdf-1.0.0/invoicing/__init__.py
@@ -0,0 +1,5 @@
+from .invoice import generate
+
+invoice.generate("invoices", "output",
+                 "product_id", "product_name", "amount_purchased",
+                 "price_per_unit", "total_price")
