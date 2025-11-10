45fec4f3b8df965a4ed67f957706a4da7e1bbd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 06:06:02 +0000

    Add package lib-hero-mvc-ps/0.1.1/lib_hero_mvc_ps-0.1.1.tar.gz

---
+++ packages/lib-hero-mvc-ps/lib_hero_mvc_ps-0.1.1.tar.gz/lib_hero_mvc_ps-0.1.1/hero_mvc/model/dto.py
@@ -0,0 +1,14 @@
+from model.models import HeroBase
+
+class HeroPublic(HeroBase):
+    id: int
+
+
+class HeroCreate(HeroBase):
+    secret_name: str
+
+
+class HeroUpdate(HeroBase):
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
