82e3478150a6ea0d1dbfa30dff6fa1064422a36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:08:15 +0000

    Add package smartpush/1.8.8/smartpush-1.8.8-py3-none-any.whl

---
+++ packages/smartpush/smartpush-1.8.8-py3-none-any.whl/smartpush/email/universal_content.py
@@ -0,0 +1,21 @@
+import json
+
+
+# 创建universal
+def create_universal():
+    pass
+
+
+# 查询universal
+def query_universal():
+    pass
+
+
+# 更新universal
+def update_universal():
+    pass
+
+
+# 删除universal
+def delete_universal():
+    pass
