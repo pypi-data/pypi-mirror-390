3ce08cce741d3c2f7933d42985e0d1af5c8fd7ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 15:06:54 +0000

    Add package prism-blanklines/0.5.4/prism_blanklines-0.5.4.tar.gz

---
+++ packages/prism-blanklines/prism_blanklines-0.5.4.tar.gz/prism_blanklines-0.5.4/src/prism.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Prism - Blank Line Enforcement Script
+Can be run directly as src/prism.py or installed as `prism` command.
+
+Copyright (c) 2025-2026 Greg Smethells. All rights reserved.
+See the accompanying AUTHORS file for a complete list of authors.
+This file is subject to the terms and conditions defined in LICENSE.
+"""
+
+from prism.cli import main
+
+if __name__ == '__main__':
+  main()
---
+++ packages/prism-blanklines/prism_blanklines-0.5.4.tar.gz/prism_blanklines-0.5.4/src/prism/__init__.py
@@ -0,0 +1,8 @@
+"""
+Prism - Blank Line Enforcement for Python Code
+Copyright (c) 2025-2026 Greg Smethells. All rights reserved.
+See the accompanying AUTHORS file for a complete list of authors.
+This file is subject to the terms and conditions defined in LICENSE.
+"""
+
+__version__ = '0.1.0'
