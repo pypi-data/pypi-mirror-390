16521a101acedc145e39b831ab7370f6f73eac1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:15 +0000

    Add package otlmow-converter/1.15rc4/otlmow_converter-1.15rc4-py3-none-any.whl

---
+++ packages/otlmow-converter/otlmow_converter-1.15rc4-py3-none-any.whl/otlmow_converter/FileFormats/CsvImporter.py
@@ -0,0 +1,172 @@
+import ast
+import csv
+import re
+from asyncio import sleep
+from pathlib import Path
+from typing import Iterable
+from otlmow_model.OtlmowModel.BaseClasses.OTLObject import OTLObject
+from otlmow_converter.AbstractImporter import AbstractImporter
+from otlmow_converter.Exceptions.NoTypeUriInTableError import NoTypeUriInTableError
+from otlmow_converter.Exceptions.TypeUriNotInFirstRowError import TypeUriNotInFirstRowError
+from otlmow_converter.FileFormats.DotnotationTableConverter import DotnotationTableConverter
+from otlmow_converter.SettingsManager import load_settings, GlobalVariables
+
+csv.field_size_limit(2147483647)
+
+
+load_settings()
+
+csv_settings = GlobalVariables.settings['formats']['csv']
+csv_dotnotation_settings = csv_settings['dotnotation']
+SEPARATOR = csv_dotnotation_settings['separator']
+CARDINALITY_SEPARATOR = csv_dotnotation_settings['cardinality_separator']
+CARDINALITY_INDICATOR = csv_dotnotation_settings['cardinality_indicator']
+WAARDE_SHORTCUT = csv_dotnotation_settings['waarde_shortcut']
+CAST_LIST = csv_settings['cast_list']
+CAST_DATETIME = csv_settings['cast_datetime']
+ALLOW_NON_OTL_CONFORM_ATTRIBUTES = csv_settings['allow_non_otl_conform_attributes']
+WARN_FOR_NON_OTL_CONFORM_ATTRIBUTES = csv_settings['warn_for_non_otl_conform_attributes']
+DELIMITER = csv_settings['delimiter']
+
+
+class CsvImporter(AbstractImporter):
+    @classmethod
+    def to_objects(cls, filepath: Path, **kwargs) -> Iterable[OTLObject]:
+        delimiter = DELIMITER
+        quote_char = '"'
+
+        if kwargs is not None:
+            if 'delimiter' in kwargs:
+                delimiter = kwargs['delimiter']
+            if 'quote_char' in kwargs:
+                quote_char = kwargs['quote_char']
+        else:
+            kwargs = {}
+
+        separator = kwargs.get('separator', SEPARATOR)
+        cardinality_separator = kwargs.get('cardinality_separator', CARDINALITY_SEPARATOR)
+        cardinality_indicator = kwargs.get('cardinality_indicator', CARDINALITY_INDICATOR)
+        waarde_shortcut = kwargs.get('waarde_shortcut', WAARDE_SHORTCUT)
+        cast_list = kwargs.get('cast_list', CAST_LIST)
+        cast_datetime = kwargs.get('cast_datetime', CAST_DATETIME)
+        allow_non_otl_conform_attributes = kwargs.get('allow_non_otl_conform_attributes',
+                                                      ALLOW_NON_OTL_CONFORM_ATTRIBUTES)
+        warn_for_non_otl_conform_attributes = kwargs.get('warn_for_non_otl_conform_attributes',
+                                                         WARN_FOR_NON_OTL_CONFORM_ATTRIBUTES)
+
+        if filepath is None:
+            raise ValueError(f'Can not write a file to: {filepath}')
+
+        if delimiter == '':
+            delimiter = ';'
+
+        model_directory = None
+        if kwargs is not None and 'model_directory' in kwargs:
+            model_directory = kwargs['model_directory']
+
+        try:
+            with open(filepath, encoding='utf-8') as file:
+                csv_reader = csv.reader(file, delimiter=delimiter, quotechar=quote_char)
+                data = [next(csv_reader)]
+                for row in csv_reader:
+                    r = []
+                    for d in row:
+                        if d is None:
+                            r.append(None)
+                        elif d == 'True':
+                            r.append(True)
+                        elif d == 'False':
+                            r.append(False)
+                        else:
+                            r.append(d)
+                    data.append(r)
+
+                list_of_dicts = DotnotationTableConverter.transform_2d_sequence_to_list_of_dicts(
+                    two_d_sequence=data, empty_string_equals_none=True)
+                return DotnotationTableConverter.get_data_from_table(
+                    table_data=list_of_dicts, model_directory=model_directory,
+                    separator=separator, cardinality_indicator=cardinality_indicator,
+                    waarde_shortcut=waarde_shortcut, cardinality_separator=cardinality_separator,
+                    cast_datetime=cast_datetime, cast_list=cast_list,
+                    allow_non_otl_conform_attributes=allow_non_otl_conform_attributes,
+                    warn_for_non_otl_conform_attributes=warn_for_non_otl_conform_attributes)
+        except TypeUriNotInFirstRowError as e:
+            raise TypeUriNotInFirstRowError(
+                message=f'The typeURI is not in the first row in file {filepath.name}.'
+                f' Please remove the excess rows',
+                file_path=filepath,
+            ) from e
+        except NoTypeUriInTableError as e:
+            raise NoTypeUriInTableError(
+                message=f'Could not find typeURI within 5 rows in the csv file {filepath.name}',
+                file_path=filepath,
+            ) from e
+
+    @classmethod
+    async def to_objects_async(cls, filepath: Path, **kwargs) -> Iterable[OTLObject]:
+        delimiter = DELIMITER
+        quote_char = '"'
+
+        if kwargs is not None:
+            if 'delimiter' in kwargs:
+                delimiter = kwargs['delimiter']
+            if 'quote_char' in kwargs:
+                quote_char = kwargs['quote_char']
+        else:
+            kwargs = {}
+
+        separator = kwargs.get('separator', SEPARATOR)
+        cardinality_separator = kwargs.get('cardinality_separator', CARDINALITY_SEPARATOR)
+        cardinality_indicator = kwargs.get('cardinality_indicator', CARDINALITY_INDICATOR)
+        waarde_shortcut = kwargs.get('waarde_shortcut', WAARDE_SHORTCUT)
+        cast_list = kwargs.get('cast_list', CAST_LIST)
+        cast_datetime = kwargs.get('cast_datetime', CAST_DATETIME)
+        allow_non_otl_conform_attributes = kwargs.get('allow_non_otl_conform_attributes',
+                                                      ALLOW_NON_OTL_CONFORM_ATTRIBUTES)
+        warn_for_non_otl_conform_attributes = kwargs.get('warn_for_non_otl_conform_attributes',
+                                                         WARN_FOR_NON_OTL_CONFORM_ATTRIBUTES)
+
+        if filepath is None:
+            raise ValueError(f'Can not write a file to: {filepath}')
+
+        if delimiter == '':
+            delimiter = ';'
+
+        model_directory = None
+        if kwargs is not None and 'model_directory' in kwargs:
+            model_directory = kwargs['model_directory']
+
+        try:
+            with open(filepath, encoding='utf-8') as file:
+                csv_reader = csv.reader(file, delimiter=delimiter, quotechar=quote_char)
+                data = [next(csv_reader)]
+                for row in csv_reader:
+                    await sleep(0)
+                    r = []
+                    for d in row:
+                        try:
+                            r.append(ast.literal_eval(d))
+                        except (SyntaxError, ValueError):
+                            r.append(str(d))
+                    data.append(r)
+
+                list_of_dicts = await DotnotationTableConverter.transform_2d_sequence_to_list_of_dicts_async(
+                    two_d_sequence=data, empty_string_equals_none=True)
+                return await DotnotationTableConverter.get_data_from_table_async(
+                    table_data=list_of_dicts, model_directory=model_directory,
+                    separator=separator, cardinality_indicator=cardinality_indicator,
+                    waarde_shortcut=waarde_shortcut, cardinality_separator=cardinality_separator,
+                    cast_datetime=cast_datetime, cast_list=cast_list,
+                    allow_non_otl_conform_attributes=allow_non_otl_conform_attributes,
+                    warn_for_non_otl_conform_attributes=warn_for_non_otl_conform_attributes)
+        except TypeUriNotInFirstRowError as e:
+            raise TypeUriNotInFirstRowError(
+                message=f'The typeURI is not in the first row in file {filepath.name}.'
+                        f' Please remove the excess rows',
+                file_path=filepath,
+            ) from e
+        except NoTypeUriInTableError as e:
+            raise NoTypeUriInTableError(
+                message=f'Could not find typeURI within 5 rows in the csv file {filepath.name}',
+                file_path=filepath,
+            ) from e
