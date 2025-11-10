eb26fd116792665e57597a8064670b9f2e9218f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-10 16:07:04 +0000

    Add package konigle/0.22.0/konigle-0.22.0.tar.gz

---
+++ packages/konigle/konigle-0.22.0.tar.gz/konigle-0.22.0/src/konigle/managers/core/__init__.py
@@ -0,0 +1,10 @@
+"""
+Core resource managers for the Konigle SDK.
+
+This module exports managers for core platform resources like media assets,
+uploads, and connections.
+"""
+
+from .connections import *
+from .media_asset import *
+from .upload import *
---
+++ packages/konigle/konigle-0.22.0.tar.gz/konigle-0.22.0/src/konigle/models/core/__init__.py
@@ -0,0 +1,40 @@
+"""
+Core resource models for the Konigle SDK.
+
+This module exports models for core platform resources like media assets,
+uploads, sites, and connections.
+"""
+
+from .connections import Connection
+from .media_asset import (
+    Document,
+    DocumentCreate,
+    DocumentUpdate,
+    Image,
+    ImageCreate,
+    ImageGenerate,
+    ImageUpdate,
+    Video,
+    VideoCreate,
+    VideoUpdate,
+)
+from .site import Site, SiteUpdate
+from .upload import Upload, UploadCreate
+
+__all__ = [
+    "Connection",
+    "Document",
+    "DocumentCreate",
+    "DocumentUpdate",
+    "Image",
+    "ImageCreate",
+    "ImageGenerate",
+    "ImageUpdate",
+    "Video",
+    "VideoCreate",
+    "VideoUpdate",
+    "Site",
+    "SiteUpdate",
+    "Upload",
+    "UploadCreate",
+]
