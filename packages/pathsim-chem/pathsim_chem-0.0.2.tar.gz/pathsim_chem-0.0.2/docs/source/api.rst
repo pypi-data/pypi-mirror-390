5e4dc9ee2e36505aa2905da87c42aa489f1aae5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:30 +0000

    Add package pathsim-chem/0.0.2/pathsim_chem-0.0.2.tar.gz

---
+++ packages/pathsim-chem/pathsim_chem-0.0.2.tar.gz/pathsim_chem-0.0.2/docs/source/api.rst
@@ -0,0 +1,12 @@
+API Reference
+=============
+
+This page provides the API reference for PathSim-Chem modules and functions.
+
+For the core PathSim API, see the `PathSim API Reference <https://pathsim.readthedocs.io/en/latest/api.html>`_.
+
+.. autosummary::
+   :toctree: _autosummary
+   :recursive:
+
+   pathsim_chem
---
+++ packages/pathsim-chem/pathsim_chem-0.0.2.tar.gz/pathsim_chem-0.0.2/src/pathsim_chem/__init__.py
@@ -0,0 +1,13 @@
+"""
+PathSim-Chem: Chemical Engineering Blocks for PathSim
+
+A toolbox providing specialized blocks for chemical engineering simulations
+in the PathSim framework.
+"""
+
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+__all__ = ["__version__"]
