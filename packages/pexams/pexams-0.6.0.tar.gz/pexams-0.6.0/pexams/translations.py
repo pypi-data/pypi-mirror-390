a296bf250b0031130283c7a2ac0d08f164bd5465
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:09:12 +0000

    Add package pexams/0.6.0/pexams-0.6.0.tar.gz

---
+++ packages/pexams/pexams-0.6.0.tar.gz/pexams-0.6.0/pexams/translations.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+
+LANG_STRINGS = {
+    "en": {
+        "name": "Student name", "id": "Student ID", "title": "Answer sheet - Model", "course": "Course", "date": "Date", "signature": "Signature",
+        "instructions_title": "Instructions",
+        "instructions_id": "<b>ID:</b> Use one character (A-Z, 0-9) per box.",
+        "instructions_answers": "<b>Answers:</b> Completely fill the box of your chosen answer.",
+        "instructions_corrections": "<b>Corrections:</b> Use correction fluid to erase mistakes.",
+        "instructions_example_correct": "Correct:",
+        "instructions_example_incorrect": "Incorrect:"
+    },
+    "es": {
+        "name": "Nombre alumno", "id": "Nº identificación", "title": "Hoja de respuestas - Modelo", "course": "Curso", "date": "Fecha", "signature": "Firma",
+        "instructions_title": "Instrucciones",
+        "instructions_id": "<b>ID:</b> Un carácter (A-Z, 0-9) por casilla.",
+        "instructions_answers": "<b>Respuestas:</b> Rellene completamente la casilla.",
+        "instructions_corrections": "<b>Correcciones:</b> Use líquido corrector para borrar.",
+        "instructions_example_correct": "Correcto:",
+        "instructions_example_incorrect": "Incorrecto:"
+    },
+    "ca": {
+        "name": "Nom alumne", "id": "Nº identificació", "title": "Full de respostes - Model", "course": "Curs", "date": "Data", "signature": "Signatura",
+        "instructions_title": "Instruccions",
+        "instructions_id": "<b>ID:</b> Un caràcter (A-Z, 0-9) per casella.",
+        "instructions_answers": "<b>Respostes:</b> Ompliu completament la casella.",
+        "instructions_corrections": "<b>Correccions:</b> Utilitzeu líquid corrector per esborrar.",
+        "instructions_example_correct": "Correcte:",
+        "instructions_example_incorrect": "Incorrecte:"
+    },
+    "de": {
+        "name": "Name des Schülers", "id": "ID-Nummer", "title": "Antwortblatt - Modell", "course": "Kurs", "date": "Datum", "signature": "Unterschrift",
+        "instructions_title": "Anweisungen",
+        "instructions_id": "<b>ID:</b> Ein Zeichen (A-Z, 0-9) pro Feld.",
+        "instructions_answers": "<b>Antworten:</b> Füllen Sie das Feld vollständig aus.",
+        "instructions_corrections": "<b>Korrekturen:</b> Verwenden Sie Korrekturflüssigkeit.",
+        "instructions_example_correct": "Richtig:",
+        "instructions_example_incorrect": "Falsch:"
+    },
+    "fr": {
+        "name": "Nom de l'étudiant", "id": "Nº d'identification", "title": "Feuille de réponses - Modèle", "course": "Cours", "date": "Date", "signature": "Signature",
+        "instructions_title": "Instructions",
+        "instructions_id": "<b>ID:</b> Un caractère (A-Z, 0-9) par case.",
+        "instructions_answers": "<b>Réponses:</b> Remplissez entièrement la case.",
+        "instructions_corrections": "<b>Corrections:</b> Utilisez du correcteur liquide.",
+        "instructions_example_correct": "Correct:",
+        "instructions_example_incorrect": "Incorrect:"
+    },
+    "it": {
+        "name": "Nome dello studente", "id": "N. di identificazione", "title": "Foglio di risposte - Modello", "course": "Corso", "date": "Data", "signature": "Firma",
+        "instructions_title": "Istruzioni",
+        "instructions_id": "<b>ID:</b> Un carattere (A-Z, 0-9) per casella.",
+        "instructions_answers": "<b>Risposte:</b> Riempire completamente la casella.",
+        "instructions_corrections": "<b>Correzioni:</b> Usare il correttore liquido.",
+        "instructions_example_correct": "Corretto:",
+        "instructions_example_incorrect": "Errato:"
+    },
+    "nl": {
+        "name": "Naam student", "id": "Identificatienummer", "title": "Antwoordblad - Model", "course": "Cursus", "date": "Datum", "signature": "Handtekening",
+        "instructions_title": "Instructies",
+        "instructions_id": "<b>ID:</b> Eén teken (A-Z, 0-9) per vak.",
+        "instructions_answers": "<b>Antwoorden:</b> Vul het vak volledig in.",
+        "instructions_corrections": "<b>Correcties:</b> Gebruik correctievloeistof.",
+        "instructions_example_correct": "Correct:",
+        "instructions_example_incorrect": "Incorrect:"
+    },
+    "pt": {
+        "name": "Nome do aluno", "id": "Nº de identificação", "title": "Folha de respostas - Modelo", "course": "Curso", "date": "Data", "signature": "Assinatura",
+        "instructions_title": "Instruções",
+        "instructions_id": "<b>ID:</b> Um caractere (A-Z, 0-9) por caixa.",
+        "instructions_answers": "<b>Respostas:</b> Preencha a caixa completamente.",
+        "instructions_corrections": "<b>Correções:</b> Use líquido corretor.",
+        "instructions_example_correct": "Correto:",
+        "instructions_example_incorrect": "Incorreto:"
+    },
+    "ru": {
+        "name": "Имя студента", "id": "Идентификационный номер", "title": "Лист ответов - Модель", "course": "Курс", "date": "Дата", "signature": "Подпись",
+        "instructions_title": "Инструкции",
+        "instructions_id": "<b>ID:</b> Один символ (A-Z, 0-9) в каждой ячейке.",
+        "instructions_answers": "<b>Ответы:</b> Полностью заполните ячейку.",
+        "instructions_corrections": "<b>Исправления:</b> Используйте корректирующую жидкость.",
+        "instructions_example_correct": "Правильно:",
+        "instructions_example_incorrect": "Неправильно:"
+    },
+    "zh": {
+        "name": "学生姓名", "id": "识别号码", "title": "答题纸 - 模型", "course": "课程", "date": "日期", "signature": "签名",
+        "instructions_title": "说明",
+        "instructions_id": "<b>ID:</b> 每格一个字符 (A-Z, 0-9)。",
+        "instructions_answers": "<b>答案:</b> 完全填满选择框。",
+        "instructions_corrections": "<b>更正:</b> 使用修正液。",
+        "instructions_example_correct": "正确:",
+        "instructions_example_incorrect": "错误:"
+    },
+    "ja": {
+        "name": "学生氏名", "id": "識別番号", "title": "解答用紙 - モデル", "course": "コース", "date": "日付", "signature": "署名",
+        "instructions_title": "説明",
+        "instructions_id": "<b>ID:</b> 各ボックスに1文字（A-Z、0-9）。",
+        "instructions_answers": "<b>解答:</b> ボックスを完全に塗りつぶしてください。",
+        "instructions_corrections": "<b>修正:</b> 修正液を使用してください。",
+        "instructions_example_correct": "正解:",
+        "instructions_example_incorrect": "不正解:"
+    }
+}
