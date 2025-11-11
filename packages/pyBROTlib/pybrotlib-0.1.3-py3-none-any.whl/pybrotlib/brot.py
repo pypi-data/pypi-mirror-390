114b78649ef59c8288eb51c34e1bae195a06c807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:19 +0000

    Add package pyBROTlib/0.1.3/pybrotlib-0.1.3-py3-none-any.whl

---
+++ packages/pyBROTlib/pybrotlib-0.1.3-py3-none-any.whl/pybrotlib/brot.py
@@ -0,0 +1,16 @@
+from .dome import BROTDome
+from .focus import BROTFocus
+from .mirrorcovers import BROTMirrorCovers
+from .telescope import BROTTelescope
+from .transport import Transport
+
+
+class BROT:
+    def __init__(self, transport: Transport, telescope_name: str):
+        self.dome = BROTDome(transport, telescope_name)
+        self.focus = BROTFocus(transport, telescope_name)
+        self.mirrorcovers = BROTMirrorCovers(transport, telescope_name)
+        self.telescope = BROTTelescope(transport, telescope_name)
+
+
+__all__ = ["BROT"]
