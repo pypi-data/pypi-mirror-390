4de82a983793bb1d658b0c4e1225ae9432c67b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:10 +0000

    Add package fhir-sheets/2.1.10/fhir_sheets-2.1.10-py3-none-any.whl

---
+++ packages/fhir-sheets/fhir_sheets-2.1.10-py3-none-any.whl/fhir_sheets/core/config/FhirSheetsConfiguration.py
@@ -0,0 +1,14 @@
+from typing import Any, Dict
+
+
+class FhirSheetsConfiguration():
+    def __init__(self, data: Dict[str, Any]):
+        self.preview_mode = data.get('preview_mode', False)
+        self.medications_as_reference = data.get('medications_as_reference', False)
+        self.random_seed = data.get('random_seed', False)
+    
+    def __repr__(self) -> str:
+        return (f"FhirSheetsConfiguration("
+                f"preview_mode={self.preview_mode}, "
+                f"medications_as_reference={self.medications_as_reference}, "
+                f"random_seed={self.random_seed})")
