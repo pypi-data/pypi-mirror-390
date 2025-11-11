5097dca7411d02e7614416c37a7d43e057257a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 04:08:40 +0000

    Add package leukeleu-django-gdpr/1.6.0/leukeleu_django_gdpr-1.6.0.tar.gz

---
+++ packages/leukeleu-django-gdpr/leukeleu_django_gdpr-1.6.0.tar.gz/leukeleu_django_gdpr-1.6.0/tests/test_settings.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import warnings
+
+# Enable all warnings
+warnings.resetwarnings()
+# Warn only once per module
+warnings.simplefilter("module")
+# Redirect warnings output to the logging system
+logging.captureWarnings(True)  # noqa: FBT003
+
+# Disable all log output, except warnings
+LOGGING = {
+    "version": 1,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+        "null": {"class": "logging.NullHandler"},
+    },
+    "loggers": {
+        "": {"handlers": ["null"]},
+        "py.warnings": {
+            "handlers": ["console"],
+            "level": "WARNING",
+        },
+    },
+}
+
+PACKAGE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(PACKAGE_DIR)
+
+SECRET_KEY = "django-insecure-test-secret-key"
+
+USE_TZ = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "tests.custom_users",
+    "leukeleu_django_gdpr",
+]
+
+AUTH_USER_MODEL = "custom_users.CustomUser"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "OPTIONS": {
+            "context_processors": [
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    }
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
