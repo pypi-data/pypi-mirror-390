6e104b4a6175184a3c8263641bbe0c093feccaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 08:07:31 +0000

    Add package augment-sdk/0.1.1/augment_sdk-0.1.1.tar.gz

---
+++ packages/augment-sdk/augment_sdk-0.1.1.tar.gz/augment_sdk-0.1.1/augment/__init__.py
@@ -0,0 +1,30 @@
+"""Augment SDK - Python client for Augment CLI agent"""
+
+__version__ = "0.1.1"
+
+from .agent import Agent, Model, VerificationResult
+from .exceptions import (
+    AugmentError,
+    AugmentCLIError,
+    AugmentJSONError,
+    AugmentNotFoundError,
+    AugmentParseError,
+    AugmentWorkspaceError,
+    AugmentVerificationError,
+)
+from .listener import AgentListener, LoggingAgentListener
+
+__all__ = [
+    "Agent",
+    "Model",
+    "VerificationResult",
+    "AgentListener",
+    "LoggingAgentListener",
+    "AugmentError",
+    "AugmentCLIError",
+    "AugmentJSONError",
+    "AugmentNotFoundError",
+    "AugmentParseError",
+    "AugmentWorkspaceError",
+    "AugmentVerificationError",
+]
