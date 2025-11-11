16847720b4e18c7c4d096057afba1c79f579ddd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:07:59 +0000

    Add package webdav-tck/2025.11.1/webdav_tck-2025.11.1.tar.gz

---
+++ packages/webdav-tck/webdav_tck-2025.11.1.tar.gz/webdav_tck-2025.11.1/src/webdav_tck/__init__.py
@@ -0,0 +1,10 @@
+"""webdav_tck: WebDAV server protocol compliance test suite."""
+
+from __future__ import annotations
+
+try:
+    from webdav_tck._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0+unknown"
+
+__all__ = ["__version__"]
