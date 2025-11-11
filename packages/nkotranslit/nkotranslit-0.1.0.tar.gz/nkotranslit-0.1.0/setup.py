a8121e386e82ea518070d85b3a7620da36fed850
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:26:51 +0000

    Add package nkotranslit/0.1.0/nkotranslit-0.1.0.tar.gz

---
+++ packages/nkotranslit/nkotranslit-0.1.0.tar.gz/nkotranslit-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="nkotranslit",    
+    version="0.1.0",
+    description="A bidirectional Latin ↔ N'Ko transliteration library for Bamanankan.",
+    author="sudopnig01@gmail.com",
+    author_email="sudopnig01@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=[
+        # No external dependencies for now
+    ],
+    entry_points={
+        "console_scripts": [
+            "latin2nko=nkotranslit.cli:latin_to_nko_cli",
+            "nko2latin=nkotranslit.cli:nko_to_latin_cli",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+         "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+
+)
