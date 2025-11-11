a3e5ad6d3afadb2b60e57e2ea01baa05d0a1ed7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:01 +0000

    Add package check-sdist/1.3.0/check_sdist-1.3.0-py3-none-any.whl

---
+++ packages/check-sdist/check_sdist-1.3.0-py3-none-any.whl/check_sdist/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2023 Henry Schreiner. All rights reserved.
+
+check-sdist: Check the contents of an SDist vs. git
+"""
+
+from __future__ import annotations
+
+__version__ = "1.3.0"
+
+__all__ = ["__version__"]
