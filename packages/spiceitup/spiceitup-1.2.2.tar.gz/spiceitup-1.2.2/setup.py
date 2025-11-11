91a503e7b0a7f62f210f5c133b22499298cbbb73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:28:19 +0000

    Add package spiceitup/1.2.2/spiceitup-1.2.2.tar.gz

---
+++ packages/spiceitup/spiceitup-1.2.2.tar.gz/spiceitup-1.2.2/setup.py
@@ -0,0 +1,42 @@
+import os
+
+from importlib_metadata import entry_points
+from setuptools import setup, find_packages
+
+def load_requirements(filename):
+    with open('/srv/data_quicklook/' + filename, 'r') as f:
+        return [
+            line.strip()
+            for line in f
+            if line.strip() and not line.startswith('#')
+        ]
+
+setup(
+    name='spiceitup',
+    version='1.2.2',
+    description='Quicklook tool for level 2 SPICE imaging',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='David Picard',
+    author_email='david.picard@universite-paris-saclay.fr',
+    url='https://git.ias.u-psud.fr/spice/data_quicklook',
+    packages=['spiceitup'],
+    install_requires=[
+        "PyQt5",
+        "sunraster",
+        "pyqtgraph",
+        "qdarkgraystyle",
+        "scipy",
+        "opencv-python-headless"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.7',
+    entry_points={
+        'console_scripts': [
+            'spiceitup=spiceitup.__main__:main'
+        ]
+    }
+)
