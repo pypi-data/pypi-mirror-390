eb825add9970a60e3187ee17e81f76467159ff76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:28:19 +0000

    Add package spiceitup/1.2.2/spiceitup-1.2.2-py3-none-any.whl

---
+++ packages/spiceitup/spiceitup-1.2.2-py3-none-any.whl/spiceitup/__main__.py
@@ -0,0 +1,30 @@
+import sys
+import warnings
+
+from run import Main
+from settings.const import Const
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+
+def main():
+    warnings.filterwarnings("ignore")
+    # Used to prevent to show All-NaN RuntimeWarning
+    # Slices in the data cube can be filled with NaN
+    # Of course, comment this line while you are developing
+
+    if not QApplication.instance():
+        app = QApplication(sys.argv)
+    else:
+        app = QApplication.instance()
+
+    win = Main()
+    win.applyStyle(app)
+    win.resize(Const.window_width, Const.window_height)
+    win.setWindowIcon(QIcon('images/icon.png'))
+    win.show()
+
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    main()
