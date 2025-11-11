17213a0809f4d29a0a0efbbc0da2feb941be5737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:06:48 +0000

    Add package smartasync/0.2.0/smartasync-0.2.0.tar.gz

---
+++ packages/smartasync/smartasync-0.2.0.tar.gz/smartasync-0.2.0/src/smartasync/__init__.py
@@ -0,0 +1,9 @@
+"""SmartAsync - Unified sync/async API decorator.
+
+Provides transparent sync/async method calling with automatic context detection.
+"""
+
+from .core import smartasync
+
+__version__ = "0.1.0"
+__all__ = ["smartasync"]
