c755743644a7076368c077ee97ebbedfcd7ffee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:27:38 +0000

    Add package opendaq/3.30.0/opendaq-3.30.0-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/opendaq/opendaq-3.30.0-cp310-cp310-macosx_10_15_x86_64.whl/opendaq/__init__.py
@@ -0,0 +1,21 @@
+from .opendaq import *
+import os
+
+OPENDAQ_MODULES_DIR = os.path.dirname(os.path.abspath(__file__))
+OPENDAQ_CWD = os.getcwd()
+__version__ = '3.30.0'
+
+# BUG: in mac os creating the project with `-> IInstance:` is failing`. 
+# To build successfully, we need to remove the `-> IInstance:` from the code.
+def Instance(*args):
+    if len(args) == 0:
+        builder = opendaq.InstanceBuilder()
+        builder.module_path = OPENDAQ_MODULES_DIR
+        return opendaq.InstanceFromBuilder(builder)
+    else:
+        return opendaq.Instance(*args)
+
+def InstanceBuilder() -> IInstanceBuilder:
+    builder = opendaq.InstanceBuilder()
+    builder.module_path = OPENDAQ_MODULES_DIR
+    return builder
