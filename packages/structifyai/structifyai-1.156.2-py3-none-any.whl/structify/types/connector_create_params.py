50ec855497ba33e7a5c9fbeb4c7b2d807b5b1b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 08:06:31 +0000

    Add package structifyai/1.156.2/structifyai-1.156.2-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.156.2-py3-none-any.whl/structify/types/connector_create_params.py
@@ -0,0 +1,46 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Dict, Optional
+from typing_extensions import Literal, Required, TypedDict
+
+__all__ = ["ConnectorCreateParams"]
+
+
+class ConnectorCreateParams(TypedDict, total=False):
+    known_connector_type: Required[
+        Literal[
+            "Slack",
+            "Confluence",
+            "Dropbox",
+            "GoogleDrive",
+            "Snowflake",
+            "Hubspot",
+            "Salesforce",
+            "Supabase",
+            "Sharepoint",
+            "Notion",
+            "Jira",
+            "Linear",
+            "Intercom",
+            "Gmail",
+            "Airtable",
+            "Trello",
+            "Postgresql",
+            "Sap",
+            "Oracle",
+            "Manual",
+        ]
+    ]
+
+    name: Required[str]
+
+    team_id: Required[str]
+
+    description: Optional[str]
+
+    refresh_script: Optional[str]
+
+    secrets: Dict[str, str]
+    """Optional secrets/environment variables for the connector"""
