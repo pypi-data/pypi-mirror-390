9e7423bcbc3e31b871e3f1f6602126490d476884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:28:33 +0000

    Add package xkits-config-yaml/0.1/xkits_config_yaml-0.1.tar.gz

---
+++ packages/xkits-config-yaml/xkits_config_yaml-0.1.tar.gz/xkits_config_yaml-0.1/setup_config_yaml.py
@@ -0,0 +1,49 @@
+# coding=utf-8
+
+from urllib.parse import urljoin
+
+from setuptools import setup
+from setuptools.command.install import install
+
+from xkits_config import __author__
+from xkits_config import __author_email__
+from xkits_config import __description__
+from xkits_config import __urlhome__
+from xkits_config import __version__
+
+__urlcode__ = __urlhome__
+__urldocs__ = __urlhome__
+__urlbugs__ = urljoin(__urlhome__, "issues")
+
+
+def all_requirements():
+    return [
+        f"xkits-config-file>={__version__}",
+        "PyYAML",
+    ]
+
+
+class CustomInstallCommand(install):
+    """Customized setuptools install command"""
+
+    def run(self):
+        install.run(self)  # Run the standard installation
+        # Execute your custom code after installation
+
+
+setup(
+    name="xkits-config-yaml",
+    version=__version__,
+    description=__description__,
+    url=__urlhome__,
+    author=__author__,
+    author_email=__author_email__,
+    project_urls={"Source Code": __urlcode__,
+                  "Bug Tracker": __urlbugs__,
+                  "Documentation": __urldocs__},
+    py_modules=["xkits_config_yaml"],
+    install_requires=all_requirements(),
+    cmdclass={
+        "install": CustomInstallCommand,
+    }
+)
