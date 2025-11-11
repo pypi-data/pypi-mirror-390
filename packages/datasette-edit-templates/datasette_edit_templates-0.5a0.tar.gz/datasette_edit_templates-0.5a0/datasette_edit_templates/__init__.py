493c4c62540b40b8014f54d6fee62251860e11b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:10 +0000

    Add package datasette-edit-templates/0.5a0/datasette_edit_templates-0.5a0.tar.gz

---
+++ packages/datasette-edit-templates/datasette_edit_templates-0.5a0.tar.gz/datasette_edit_templates-0.5a0/datasette_edit_templates/__init__.py
@@ -0,0 +1,276 @@
+from datasette import hookimpl
+from datasette.permissions import Action
+from jinja2 import FunctionLoader, TemplateNotFound
+from datasette.utils.asgi import Response, Forbidden
+import datetime
+
+TABLE = "_templates_"
+CREATE_TABLE_SQLS = [
+    """
+    CREATE TABLE {table} (
+        [id] INTEGER PRIMARY KEY,
+        [template] TEXT,
+        [created] TEXT,
+        [body] TEXT
+    );""".format(
+        table=TABLE
+    ),
+    """
+    CREATE INDEX idx_template_created ON {table}(template, created);
+    """.format(
+        table=TABLE
+    ),
+]
+LOAD_TEMPLATE_SQL = """
+select template, body
+from {table}
+group by template
+having created = max(created)
+""".format(
+    table=TABLE
+)
+GET_TEMPLATE_SQL = "SELECT id, body FROM {} WHERE template = :template ORDER BY created DESC LIMIT 1".format(
+    TABLE
+)
+WRITE_TEMPLATE_SQL = "INSERT INTO {} (template, created, body) VALUES (:template, :created, :body)".format(
+    TABLE
+)
+
+
+def pretty_datetime(d):
+    if d:
+        return datetime.datetime.fromisoformat(d).strftime("%Y-%m-%d %H:%M:%S")
+    else:
+        return ""
+
+
+def get_environment(datasette, request):
+    if hasattr(datasette, "get_jinja_environment"):
+        return datasette.get_jinja_environment(request)
+    return datasette.jinja_env
+
+
+@hookimpl
+def startup(datasette):
+    datasette._edit_templates = {}
+
+    async def inner():
+        db = get_database(datasette)
+        # Does the table exist?
+        if not await db.table_exists(TABLE):
+            for sql in CREATE_TABLE_SQLS:
+                await db.execute_write(sql, block=True)
+        else:
+            # Load all templates from that table
+            rows = await db.execute(LOAD_TEMPLATE_SQL)
+            for name, content in rows:
+                datasette._edit_templates[name] = content
+
+    return inner
+
+
+@hookimpl
+def register_actions():
+    return (
+        Action(
+            name="edit-templates",
+            description="Edit Datasette templates",
+        ),
+    )
+
+
+@hookimpl
+def menu_links(datasette, actor):
+    config = datasette.plugin_config("datasette-edit-templates") or {}
+    if "menu_label" not in config:
+        menu_label = "Edit templates"
+    else:
+        menu_label = config.get("menu_label")
+    if menu_label is None:
+        return
+
+    async def inner():
+        if not await datasette.allowed(action="edit-templates", actor=actor):
+            return
+        return [
+            {
+                "href": datasette.urls.path("/-/edit-templates"),
+                "label": menu_label,
+            },
+        ]
+
+    return inner
+
+
+def get_database(datasette):
+    plugin_config = datasette.plugin_config("datasette-edit-templates") or {}
+    if plugin_config.get("internal_db"):
+        return datasette.get_internal_database()
+    return datasette.get_database(plugin_config.get("database"))
+
+
+class MyFunctionLoader(FunctionLoader):
+    def list_templates(self):
+        return []
+
+
+@hookimpl
+def prepare_jinja2_environment(env, datasette):
+    config = datasette.plugin_config("datasette-edit-templates") or {}
+    if config.get("skip_prepare_jinja2_environment"):
+        return
+
+    def load_func(path):
+        try:
+            code = datasette._edit_templates[path]
+            return code, path, lambda: True
+        except KeyError:
+            return None
+
+    env.loader.loaders.insert(0, MyFunctionLoader(load_func))
+
+
+async def edit_templates_index(request, datasette):
+    if not await datasette.allowed(
+        action="edit-templates",
+        actor=request.actor,
+    ):
+        raise Forbidden("Permission denied for edit-templates")
+    template_name = request.args.get("template")
+    if template_name:
+        return Response.redirect(
+            datasette.urls.path("/-/edit-templates/{}".format(template_name))
+        )
+    db = get_database(datasette)
+
+    # List both templates from DB and default unedited templates
+    templates = [
+        dict(row)
+        for row in await db.execute(
+            """
+            select template as name, max(created) as last_updated, count(*) as revisions
+            from {} group by template order by created desc
+            """.format(
+                TABLE
+            )
+        )
+    ]
+    by_name = {t["name"]: t for t in templates}
+
+    environment = get_environment(datasette, request)
+
+    for template in environment.list_templates():
+        if template.startswith("default:"):
+            continue
+        if template in by_name:
+            pass
+        else:
+            templates.append(
+                {
+                    "name": template,
+                    "last_edited": None,
+                    "revisions": "",
+                }
+            )
+
+    # Offer edit options for all disk templates
+    return Response.html(
+        await datasette.render_template(
+            "edit_templates_index.html",
+            {
+                "templates": templates,
+                "pretty_datetime": pretty_datetime,
+            },
+        )
+    )
+
+
+async def edit_template(request, datasette):
+    if not await datasette.allowed(
+        action="edit-templates",
+        actor=request.actor,
+    ):
+        raise Forbidden("Permission denied for edit-templates")
+    template = request.url_vars["template"]
+    db = get_database(datasette)
+    if request.method == "POST":
+        post = await request.post_vars()
+        body = post["body"]
+        await db.execute_write(
+            WRITE_TEMPLATE_SQL,
+            {
+                "template": template,
+                "created": datetime.datetime.utcnow().isoformat(),
+                "body": body,
+            },
+            block=True,
+        )
+        datasette.add_message(request, "Template changes saved")
+        # Update the in-memory cache too
+        datasette._edit_templates[template] = body
+        # Clear the Jinja template cache so it sees the change
+        get_environment(datasette, request).cache.clear()
+        return Response.redirect(request.path)
+
+    create_from_scratch = False
+    row = (await db.execute(GET_TEMPLATE_SQL, {"template": template})).first()
+    requested_revision = request.args.get("revision")
+    revision = None
+    if requested_revision:
+        row = (
+            await db.execute(
+                "select id, created, body from {} where id = :id".format(TABLE),
+                {"id": requested_revision},
+            )
+        ).first()
+        revision = dict(row)
+    revisions = []
+    if row is None:
+        from_db = False
+        # Load it from disk instead
+        try:
+            environment = get_environment(datasette, request)
+            template_obj = environment.get_template(template)
+            body = open(template_obj.filename).read()
+        except TemplateNotFound:
+            body = ""
+            create_from_scratch = True
+    else:
+        from_db = True
+        body = row["body"]
+        # And load revisions
+        revisions = [
+            dict(revision)
+            for revision in (
+                await db.execute(
+                    "select id, created from {} where template = :template and id != :id order by created desc".format(
+                        TABLE
+                    ),
+                    {"template": template, "id": row["id"]},
+                )
+            ).rows
+        ]
+    return Response.html(
+        await datasette.render_template(
+            "edit_template.html",
+            {
+                "body": body,
+                "template": template,
+                "revision": revision,
+                "path": request.path,
+                "from_db": from_db,
+                "create_from_scratch": create_from_scratch,
+                "revisions": revisions,
+                "pretty_datetime": pretty_datetime,
+            },
+            request=request,
+        )
+    )
+
+
+@hookimpl
+def register_routes():
+    return [
+        (r"^/-/edit-templates$", edit_templates_index),
+        (r"^/-/edit-templates/(?P<template>.+)$", edit_template),
+    ]
---
+++ packages/datasette-edit-templates/datasette_edit_templates-0.5a0.tar.gz/datasette_edit_templates-0.5a0/tests/test_edit_templates.py
@@ -0,0 +1,209 @@
+from datasette_test import Datasette
+import datasette.version
+import pytest_asyncio
+import pytest
+import sqlite_utils
+
+
+TEMPLATES = [
+    # Duplicated these with same created just to
+    # make sure having exact same timestamp doesn't
+    # crash the server on startup
+    {
+        "id": 1,
+        "template": "_footer.html",
+        "created": "2023-01-01T00:00:00",
+        "body": "Hello world v2",
+    },
+    {
+        "id": 2,
+        "template": "_footer.html",
+        "created": "2023-01-01T00:00:00",
+        "body": "Hello world v2",
+    },
+    {
+        "id": 3,
+        "template": "_footer.html",
+        "created": "2022-01-01T00:00:00",
+        "body": "Hello world v1",
+    },
+]
+
+
+@pytest.fixture
+def db_path(tmpdir):
+    db_path = str(tmpdir / "test.db")
+    sqlite_utils.Database(db_path).vacuum()
+    return db_path
+
+
+@pytest.fixture
+def db(db_path):
+    return sqlite_utils.Database(db_path)
+
+
+@pytest_asyncio.fixture
+async def ds(db_path):
+    ds = Datasette([db_path])
+    ds.root_enabled = True
+    await ds.invoke_startup()
+    return ds
+
+
+@pytest.mark.asyncio
+async def test_loads_templates_on_startup(db_path, db):
+    db["_templates_"].insert_all(TEMPLATES)
+    ds = Datasette([db_path])
+    await ds.invoke_startup()
+    assert ds._edit_templates == {"_footer.html": "Hello world v2"}
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("skip", [True, False])
+async def test_skip_prepare_jinja2_environment(db_path, db, skip):
+    db["_templates_"].insert_all(TEMPLATES)
+    ds = Datasette(
+        [db_path],
+        config={
+            "plugins": {
+                "datasette-edit-templates": {"skip_prepare_jinja2_environment": skip}
+            }
+        },
+    )
+    await ds.invoke_startup()
+    assert ds._edit_templates == {"_footer.html": "Hello world v2"}
+    response = await ds.client.get("/")
+    if skip:
+        assert "Hello world v2" not in response.text
+    else:
+        assert "Hello world v2" in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("menu_label", ("NOT_SET", None, "Custom edit templates"))
+async def test_menu_link(db_path, menu_label):
+    plugin_config = {}
+    if menu_label != "NOT_SET":
+        plugin_config["datasette-edit-templates"] = {"menu_label": menu_label}
+    ds = Datasette([db_path], config={"plugins": plugin_config})
+    ds.root_enabled = True
+    await ds.invoke_startup()
+    response = await ds.client.get("/")
+    assert response.status_code == 200
+    assert "Edit templates" not in response.text
+    response = await ds.client.get(
+        "/", cookies={"ds_actor": ds.sign({"a": {"id": "root"}}, "actor")}
+    )
+    assert response.status_code == 200
+    if menu_label == "NOT_SET":
+        assert "Edit templates" in response.text
+    elif menu_label is None:
+        assert "Edit templates" not in response.text
+    else:
+        assert menu_label in response.text
+
+
+@pytest.mark.asyncio
+async def test_edit_template(ds, db):
+    assert ds._edit_templates == {}
+    actor = ds.sign({"a": {"id": "root"}}, "actor")
+    response1 = await ds.client.get(
+        "/-/edit-templates/_footer.html", cookies={"ds_actor": actor}
+    )
+    assert "New footer" not in response1.text
+    assert response1.status_code == 200
+    # Get CSRF cookie
+    csrftoken = response1.cookies["ds_csrftoken"]
+    response2 = await ds.client.post(
+        "/-/edit-templates/_footer.html",
+        cookies={"ds_actor": actor, "ds_csrftoken": csrftoken},
+        data={"body": "New footer", "csrftoken": csrftoken},
+    )
+    assert ds._edit_templates == {"_footer.html": "New footer"}
+    assert response2.status_code == 302
+    assert response2.headers["Location"] == "/-/edit-templates/_footer.html"
+    response3 = await ds.client.get("/")
+    assert "New footer" in response3.text
+
+
+@pytest.mark.asyncio
+async def test_create_new_template(ds, db):
+    assert ds._edit_templates == {}
+    actor = ds.sign({"a": {"id": "root"}}, "actor")
+    response1 = await ds.client.get("/-/edit-templates", cookies={"ds_actor": actor})
+    assert ' action="/-/edit-templates/" method="get">' in response1.text
+    assert (await ds.client.get("/foo")).status_code == 404
+    response2 = await ds.client.get(
+        "/-/edit-templates?template=pages/foo.html", cookies={"ds_actor": actor}
+    )
+    assert response2.status_code == 302
+    assert response2.headers["Location"] == "/-/edit-templates/pages/foo.html"
+    # Get CSRF cookie
+    csrftoken = (
+        await ds.client.get(
+            "/-/edit-templates/pages/foo.html", cookies={"ds_actor": actor}
+        )
+    ).cookies["ds_csrftoken"]
+    response2 = await ds.client.post(
+        "/-/edit-templates/pages/foo.html",
+        cookies={"ds_actor": actor, "ds_csrftoken": csrftoken},
+        data={"body": "Brand new page", "csrftoken": csrftoken},
+    )
+    assert ds._edit_templates == {"pages/foo.html": "Brand new page"}
+    response3 = await ds.client.get("/foo")
+    assert response3.status_code == 200
+    assert response3.text == "Brand new page"
+
+
+@pytest.mark.asyncio
+async def test_edit_template_permission_denied(ds):
+    for path in ("/-/edit-templates/_footer.html", "/-/edit-templates"):
+        response = await ds.client.get(path)
+        assert response.status_code == 403
+        assert "Permission denied" in response.text
+
+
+@pytest.mark.asyncio
+async def test_view_revisions(db_path, db):
+    db["_templates_"].insert_all(TEMPLATES)
+    ds = Datasette([db_path])
+    ds.root_enabled = True
+    await ds.invoke_startup()
+    ds_actor = ds.sign({"a": {"id": "root"}}, "actor")
+    response = await ds.client.get(
+        "/-/edit-templates/_footer.html", cookies={"ds_actor": ds_actor}
+    )
+    for fragment in (
+        '<a href="/-/edit-templates/_footer.html?revision=2">2023-01-01 00:00:00</a>',
+        '<a href="/-/edit-templates/_footer.html?revision=3">2022-01-01 00:00:00</a>',
+    ):
+        assert fragment in response.text
+    response2 = await ds.client.get(
+        "/-/edit-templates/_footer.html?revision=3", cookies={"ds_actor": ds_actor}
+    )
+    assert response2.status_code == 200
+    assert "Hello world v1" in response2.text
+    assert "readOnly" in response2.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.skipif(
+    datasette.version.__version_info__ < ("1", "0a5"),
+    reason="Requires Datasette 1.0a5 or higher",
+)
+@pytest.mark.parametrize("internal_db", (True, False))
+async def test_internal_db(internal_db):
+    ds = Datasette(
+        [],
+        memory=True,
+        config={"plugins": {"datasette-edit-templates": {"internal_db": internal_db}}},
+    )
+    db = ds.get_internal_database()
+    await db.execute_write_fn(
+        lambda conn: sqlite_utils.Database(conn)["_templates_"].insert_all(TEMPLATES)
+    )
+    await ds.invoke_startup()
+    if internal_db:
+        assert ds._edit_templates == {"_footer.html": "Hello world v2"}
+    else:
+        assert ds._edit_templates == {}
