47a546c1a79371895978481d687d3121248e4456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:06:09 +0000

    Add package stidantic/0.2.1/stidantic-0.2.1.tar.gz

---
+++ packages/stidantic/stidantic-0.2.1.tar.gz/stidantic-0.2.1/stidantic/bundle.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from pydantic import Field
+
+from stidantic.extension import ExtensionDefinition
+from stidantic.language import LanguageContent
+from stidantic.marking import MarkingDefinition
+from stidantic.sco import SCOs
+from stidantic.sdo import SDOs
+from stidantic.sro import SROs
+from stidantic.types import Identifier, StixCommon, StixCore
+
+
+# 8. Stix Bundle
+class StixBundle(StixCore):
+    id: Identifier
+    type: str = "bundle"
+    objects: list[
+        Annotated[
+            (SROs | SDOs | SCOs | MarkingDefinition | LanguageContent | ExtensionDefinition),
+            Field(discriminator="type"),
+        ]
+        | StixCommon
+    ]
