2eb672dea9133f41aab0452e9d4d24436647c5a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:04 +0000

    Add package dublindistance/0.1.2/dublindistance-0.1.2.tar.gz

---
+++ packages/dublindistance/dublindistance-0.1.2.tar.gz/dublindistance-0.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dublindistance",
+    version="0.1.2",
+    packages=find_packages(),
+    install_requires=["geopy"],
+    description="Simple Dublin area distance and transport suggestion tool",
+    author="Jeevanjot Singh",
+    license="MIT",
+)
