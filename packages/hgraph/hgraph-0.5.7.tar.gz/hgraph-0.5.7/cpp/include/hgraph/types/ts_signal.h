1fd4b6223bd588b8af894afa121032c1ddc49ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:55 +0000

    Add package hgraph/0.5.7/hgraph-0.5.7.tar.gz

---
+++ packages/hgraph/hgraph-0.5.7.tar.gz/hgraph-0.5.7/cpp/include/hgraph/types/ts_signal.h
@@ -0,0 +1,47 @@
+//
+// Created by Howard Henson on 06/06/2025.
+//
+
+#ifndef TS_SIGNAL_H
+#define TS_SIGNAL_H
+
+#include <hgraph/types/base_time_series.h>
+
+namespace hgraph {
+    struct TimeSeriesSignalInputBuilder;
+
+    struct TimeSeriesSignalInput : BaseTimeSeriesInput {
+        using ptr = nb::ref<TimeSeriesSignalInput>;
+        using BaseTimeSeriesInput::BaseTimeSeriesInput;
+
+        [[nodiscard]] nb::object py_value() const override;
+
+        [[nodiscard]] nb::object py_delta_value() const override;
+
+        [[nodiscard]] bool is_same_type(const TimeSeriesType *other) const override { return true; }
+
+        [[nodiscard]] TimeSeriesInput *get_input(size_t index) override;
+
+        // Override to aggregate from children like Python implementation
+        [[nodiscard]] bool valid() const override;
+
+        [[nodiscard]] bool modified() const override;
+
+        [[nodiscard]] engine_time_t last_modified_time() const override;
+
+        // Override to propagate to children
+        void make_active() override;
+
+        void make_passive() override;
+
+        void do_un_bind_output(bool unbind_refs) override;
+
+        static void register_with_nanobind(nb::module_ &m);
+
+    private:
+        friend TimeSeriesSignalInputBuilder;
+        mutable std::vector<ptr> _ts_values; // Lazily created child signals
+    };
+}
+
+#endif //TS_SIGNAL_H
