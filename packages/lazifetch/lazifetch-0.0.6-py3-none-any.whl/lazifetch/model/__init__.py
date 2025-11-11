bf22a81cdf9e5c2b03bb9a84e93754b31e576899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:27:36 +0000

    Add package lazifetch/0.0.6/lazifetch-0.0.6-py3-none-any.whl

---
+++ packages/lazifetch/lazifetch-0.0.6-py3-none-any.whl/lazifetch/model/__init__.py
@@ -0,0 +1,8 @@
+# src/lazifetch/model/__init__.py
+
+
+from .SemanticSearcher import SemanticSearcher
+from .OpenAILLM import OpenAILLM
+from .Result import Result
+
+__all__ = ["SemanticSearcher", "OpenAILLM", "Result"]
