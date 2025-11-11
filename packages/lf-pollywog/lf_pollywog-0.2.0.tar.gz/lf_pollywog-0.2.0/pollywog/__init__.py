d1ca51fc8a4111faa85bbf352c5f6fdff7a26972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 08:06:47 +0000

    Add package lf-pollywog/0.2.0/lf_pollywog-0.2.0.tar.gz

---
+++ packages/lf-pollywog/lf_pollywog-0.2.0.tar.gz/lf_pollywog-0.2.0/pollywog/__init__.py
@@ -0,0 +1,3 @@
+from .core import CalcSet, Variable, Number, Category, Filter, If, IfRow
+from .helpers import Sum, Average, Product, Normalize, WeightedAverage
+from .display import display_calcset, display_item, set_theme
