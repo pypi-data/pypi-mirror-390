ef15a8c22715b3cb455d09f79ee4d46ecaaa1d71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:11 +0000

    Add package HeroTeams1103/0.1.0/heroteams1103-0.1.0.tar.gz

---
+++ packages/HeroTeams1103/heroteams1103-0.1.0.tar.gz/heroteams1103-0.1.0/README.md
@@ -0,0 +1,3 @@
+Biblioteca com uso de SQLmodel e FastAPI para usos didaticos.
+
+Depois de instala a biblioteca é preciso usar o comando "uvicorn HeroTeams1103.main:app --reload" para rodar o código principal
---
+++ packages/HeroTeams1103/heroteams1103-0.1.0.tar.gz/heroteams1103-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Lendo o README.md para o PyPI
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="HeroTeams1103",
+    version="0.1.0",
+    description="Biblioteca de criação de heróis e times com fastapi.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Gustavo Firme Fiorot",
+    author_email="gffiorot@gmail.com",
+    url="https://github.com/gffiorot/HeroLib",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "fastapi",
+        "uvicorn",
+        "sqlmodel",
+        "pydantic",
+        "python-dotenv",
+        "email-validator",
+    ],
+    python_requires=">=3.8",
+)
