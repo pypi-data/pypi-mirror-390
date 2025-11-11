195fffa7c97a9edf762c3fe16b9214cdee603479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 04:09:02 +0000

    Add package white-api/1.0.3/white_api-1.0.3-py3-none-any.whl

---
+++ packages/white-api/white_api-1.0.3-py3-none-any.whl/white_api/__init__.py
@@ -0,0 +1,4 @@
+from .core import WhiteAPI, WhiteGiftAPI, WhiteStickerAPI, WhiteStickerToolsAPI
+
+__version__ = "1.0.3"
+__all__ = ['WhiteAPI', 'WhiteGiftAPI', 'WhiteStickerAPI', 'WhiteStickerToolsAPI']
