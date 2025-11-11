83a95db0d41f2a23b5900365cd1c887c09517afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:05:44 +0000

    Add package datasette-upload-dbs/0.4a0/datasette_upload_dbs-0.4a0.tar.gz

---
+++ packages/datasette-upload-dbs/datasette_upload_dbs-0.4a0.tar.gz/datasette_upload_dbs-0.4a0/datasette_upload_dbs/__init__.py
@@ -0,0 +1,145 @@
+from datasette import hookimpl
+from datasette.permissions import Action
+from datasette.database import Database
+from datasette.utils.asgi import Response, Forbidden
+from datasette.utils import to_css_class
+from datasette.utils.sqlite import sqlite3
+from starlette.requests import Request
+from shutil import copyfileobj
+import pathlib
+
+
+@hookimpl
+def register_actions(datasette):
+    return [
+        Action(
+            name="upload-dbs",
+            description="Upload SQLite database files",
+        )
+    ]
+
+
+@hookimpl
+def register_routes():
+    return [
+        (r"^/-/upload-dbs$", upload_dbs),
+        (r"^/-/upload-db$", lambda: Response.redirect("/-/upload-dbs")),
+    ]
+
+
+@hookimpl
+def menu_links(datasette, actor):
+    async def inner():
+        if await datasette.allowed(
+            actor=actor,
+            action="upload-dbs",
+        ) and _configured(datasette):
+            return [
+                {
+                    "href": datasette.urls.path("/-/upload-dbs"),
+                    "label": "Upload database",
+                },
+            ]
+
+    return inner
+
+
+@hookimpl
+def startup(datasette):
+    # Load any databases located in the directory folder
+    config = datasette.plugin_config("datasette-upload-dbs") or {}
+    if config.get("skip_startup_scan"):
+        return
+    directory = config.get("directory")
+    if not directory:
+        return
+    path = pathlib.Path(directory)
+    database_files = path.glob("*.db")
+    for file_path in database_files:
+        # Needs to set is_mutable=True here because the default was False
+        # in Datasette versions up to and including 0.62
+        datasette.add_database(
+            Database(datasette, path=str(file_path), is_mutable=True),
+        )
+
+
+def _configured(datasette):
+    return (datasette.plugin_config("datasette-upload-dbs") or {}).get("directory")
+
+
+async def upload_dbs(scope, receive, datasette, request):
+    if not await datasette.allowed(
+        actor=request.actor,
+        action="upload-dbs",
+    ):
+        raise Forbidden("Permission denied for upload-dbs")
+
+    directory = _configured(datasette)
+
+    if not directory:
+        raise Forbidden("datasette-upload-dbs plugin has not been correctly configured")
+
+    path = pathlib.Path(directory)
+
+    if request.method != "POST":
+        return Response.html(
+            await datasette.render_template("upload_dbs.html", request=request)
+        )
+
+    # We use the Starlette request object to handle file uploads
+    starlette_request = Request(scope, receive)
+
+    async def error(msg):
+        if is_xhr:
+            return Response.json({"ok": False, "error": msg})
+
+        return Response.html(
+            await datasette.render_template(
+                "upload_dbs.html",
+                {
+                    "error": msg,
+                },
+                request=request,
+            )
+        )
+
+    formdata = await starlette_request.form()
+    db_file = formdata["db"]
+    is_xhr = formdata.get("xhr")
+    db_name = (formdata.get("db_name") or "").strip()
+
+    if not db_name:
+        db_name = db_file.filename.split(".")[0]
+
+    db_name = to_css_class(db_name) or "db"
+
+    first_16 = db_file.file.read(16)
+    if first_16 != b"SQLite format 3\x00":
+        return await error("File is not a valid SQLite database (invalid header)")
+
+    path.mkdir(parents=True, exist_ok=True)
+
+    # Copy it to its final destination
+    filepath = path / (db_name + ".db")
+    with open(filepath, "wb+") as target_file:
+        db_file.file.seek(0)
+        copyfileobj(db_file.file, target_file)
+
+    # Now really verify it
+    conn = sqlite3.connect(str(filepath))
+    try:
+        conn.execute("select * from sqlite_master")
+    except sqlite3.Error as e:
+        # Delete file, it is invalid
+        filepath.unlink()
+        return await error(f"File is not a valid SQLite database ({e})")
+
+    # File is valid - add it to this Datasette instance
+    db = Database(datasette, path=str(filepath), is_mutable=True)
+    datasette.add_database(db)
+
+    redirect_url = datasette.urls.database(db.name)
+    if is_xhr:
+        return Response.json({"ok": True, "redirect": redirect_url})
+    else:
+        return Response.redirect(redirect_url)
---
+++ packages/datasette-upload-dbs/datasette_upload_dbs-0.4a0.tar.gz/datasette_upload_dbs-0.4a0/tests/test_upload_dbs.py
@@ -0,0 +1,191 @@
+from datasette.app import Datasette
+import sqlite3
+import pytest
+from io import BytesIO
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("auth", [True, False])
+async def test_menu(auth):
+    ds = Datasette(
+        memory=True,
+        config={"plugins": {"datasette-upload-dbs": {"directory": "."}}},
+    )
+    ds.root_enabled = True
+    cookies = {}
+    if auth:
+        cookies = {"ds_actor": ds.sign({"a": {"id": "root"}}, "actor")}
+    response = await ds.client.get("/", cookies=cookies)
+    assert response.status_code == 200
+    if auth:
+        assert "/-/upload-dbs" in response.text
+    else:
+        assert "/-/upload-dbs" not in response.text
+
+
+@pytest.mark.asyncio
+async def test_redirect():
+    ds = Datasette(memory=True)
+    response = await ds.client.get("/-/upload-db")
+    assert response.status_code == 302
+    assert response.headers["location"] == "/-/upload-dbs"
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("skip_startup_scan", (False, True))
+async def test_databases_loaded_on_startup(tmp_path_factory, skip_startup_scan):
+    uploads_directory = tmp_path_factory.mktemp("uploads")
+    for name in ("test1.db", "test2.db"):
+        db_path = uploads_directory / name
+        sqlite3.connect(str(db_path)).execute("create table t (id integer primary key)")
+    config = {"directory": str(uploads_directory)}
+    if skip_startup_scan:
+        config["skip_startup_scan"] = True
+
+    ds = Datasette(
+        memory=True,
+        config={"plugins": {"datasette-upload-dbs": config}},
+    )
+    await ds.invoke_startup()
+    db_names = {"test1", "test2"}
+    if skip_startup_scan:
+        # Startup scan skipped, so none of the uploaded DBs should be loaded
+        assert db_names.isdisjoint(ds.databases.keys())
+    else:
+        assert set(ds.databases.keys()).issuperset(db_names)
+        for name in db_names:
+            assert ds.databases[name].is_mutable
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "authed,configured,expected_error",
+    (
+        (False, False, "Permission denied for upload-dbs"),
+        (False, True, "Permission denied for upload-dbs"),
+        (True, False, "datasette-upload-dbs plugin has not been correctly configured"),
+    ),
+)
+async def test_errors(authed, configured, expected_error):
+    ds = Datasette(
+        memory=True,
+        config={
+            "plugins": (
+                {"datasette-upload-dbs": {"directory": "."}} if configured else {}
+            )
+        },
+    )
+    ds.root_enabled = True
+    cookies = {}
+    if authed:
+        cookies = {"ds_actor": ds.sign({"a": {"id": "root"}}, "actor")}
+    response = await ds.client.get("/-/upload-dbs", cookies=cookies)
+    assert response.status_code == 403
+    assert expected_error in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "bytes,expected_error",
+    (
+        (b"bad_bytes", "File is not a valid SQLite database (invalid header)"),
+        (
+            b"SQLite format 3\x00invalid",
+            "File is not a valid SQLite database (file is not a database)",
+        ),
+    ),
+)
+@pytest.mark.parametrize("xhr", (True, False))
+async def test_invalid_files(tmp_path_factory, bytes, expected_error, xhr):
+    uploads_directory = tmp_path_factory.mktemp("uploads")
+    ds = Datasette(
+        memory=True,
+        config={
+            "plugins": {"datasette-upload-dbs": {"directory": str(uploads_directory)}}
+        },
+    )
+    ds.root_enabled = True
+    csrftoken = await _get_csrftoken(ds)
+    # write to database
+    response = await ds.client.post(
+        "/-/upload-dbs",
+        cookies={
+            "ds_actor": ds.sign({"a": {"id": "root"}}, "actor"),
+            "ds_csrftoken": csrftoken,
+        },
+        data={"csrftoken": csrftoken, "xhr": "1" if xhr else ""},
+        files={"db": BytesIO(bytes)},
+    )
+    if xhr:
+        assert response.json() == {"ok": False, "error": expected_error}
+    else:
+        assert f'<p class="message-error">{expected_error}</p>' in response.text
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("xhr", (True, False))
+@pytest.mark.parametrize(
+    "db_file_name,db_name,expected_path",
+    (
+        ("temp.db", None, "/temp"),
+        (".db", None, "/d41d8c"),
+        ("temp.db", "custom", "/custom"),
+        ("temp.db", "a + b + c ~ d", "/a--b--c--d-26e27e"),
+    ),
+)
+async def test_upload(tmp_path_factory, xhr, db_file_name, db_name, expected_path):
+    uploads_directory = tmp_path_factory.mktemp("uploads")
+    tmp_directory = tmp_path_factory.mktemp("tmp")
+    ds = Datasette(
+        memory=True,
+        config={
+            "plugins": {"datasette-upload-dbs": {"directory": str(uploads_directory)}}
+        },
+    )
+    ds.root_enabled = True
+    csrftoken = await _get_csrftoken(ds)
+
+    temp = str(tmp_directory / db_file_name)
+    sqlite3.connect(temp).execute("create table t (id integer primary key)")
+
+    upload_data = {"csrftoken": csrftoken, "xhr": "1" if xhr else ""}
+    if db_name:
+        upload_data["db_name"] = db_name
+
+    response = await ds.client.post(
+        "/-/upload-dbs",
+        cookies={
+            "ds_actor": ds.sign({"a": {"id": "root"}}, "actor"),
+            "ds_csrftoken": csrftoken,
+        },
+        data=upload_data,
+        files={"db": open(temp, "rb")},
+    )
+    if xhr:
+        assert response.json() == {"ok": True, "redirect": expected_path}
+    else:
+        assert response.status_code == 302
+        assert response.headers["location"] == expected_path
+
+    # Datasette should serve that file
+    table_response = await ds.client.get(f"{expected_path}/t.json?_shape=array")
+    assert table_response.status_code == 200
+    assert table_response.json() == []
+
+    # Uploaded file should exist
+    conn = sqlite3.connect(temp)
+    assert conn.execute("select sql from sqlite_master").fetchall() == [
+        ("CREATE TABLE t (id integer primary key)",)
+    ]
+    # And it should be mutable
+    new_db = [db for db in ds.databases.values() if not db.name.startswith("_")][0]
+    assert new_db.is_mutable
+
+
+async def _get_csrftoken(ds):
+    return (
+        await ds.client.get(
+            "/-/upload-dbs",
+            cookies={"ds_actor": ds.sign({"a": {"id": "root"}}, "actor")},
+        )
+    ).cookies["ds_csrftoken"]
