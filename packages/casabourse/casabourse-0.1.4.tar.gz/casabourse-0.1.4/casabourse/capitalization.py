cd54057ed3499a35828a0e1968d197d584864e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:08:19 +0000

    Add package casabourse/0.1.4/casabourse-0.1.4.tar.gz

---
+++ packages/casabourse/casabourse-0.1.4.tar.gz/casabourse-0.1.4/casabourse/capitalization.py
@@ -0,0 +1,3 @@
+from ._legacy_bourse_api import (
+    get_capitalization_data, get_capitalization_overview
+)
---
+++ packages/casabourse/casabourse-0.1.4.tar.gz/casabourse-0.1.4/casabourse/volumes.py
@@ -0,0 +1,3 @@
+from ._legacy_bourse_api import (
+    get_volume_overview, get_volume_data
+)
