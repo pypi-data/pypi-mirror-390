3c6501d6e94b683255a40f4a607896add76d3bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:10 +0000

    Add package qoqo-myqlm/0.5.0/qoqo_myqlm-0.5.0-py3-none-any.whl

---
+++ packages/qoqo-myqlm/qoqo_myqlm-0.5.0-py3-none-any.whl/qoqo_myqlm/backend/myqlm_backend.py
@@ -0,0 +1,239 @@
+"""Backend producing MyQLM"""
+
+# Copyright © 2019-2024 HQS Quantum Simulations GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+from qoqo import Circuit
+from typing import Optional, Dict, List, Tuple, cast, Any
+from qoqo_myqlm.interface import myqlm_call_circuit
+import numpy as np
+import warnings
+import qat
+from qat.qpus import get_default_qpu
+import time
+
+
+class MyQLMBackend(object):
+    r"""Backend to qoqo that produces MyQLM output which can be imported.
+
+    This backend takes a qoqo circuit to be run on a certain device and returns a MyQLM file
+    containing the translated circuit. The circuit itself is translated using the qoqo_myqlm
+    interface.
+    """
+
+    def __init__(
+        self,
+        number_qubits: int = 1,
+        number_measurements: int = 1,
+        device: Optional[Any] = None,  # noqa
+        job_type: str = "SAMPLE",
+        observable: Optional[np.ndarray] = None,
+        qpu: Any = None,  # noqa
+        mode: str = "active_qubits_only",
+        time_QLM_submission: bool = False,
+    ) -> None:  # noqa
+        """Initialize MyQLM Backend
+
+        Args:
+            number_qubits: The number of qubits to use
+            number_measurements: The number of measurement repetitions. If set to 0:
+                - simulator: tries to output all the possible final states (with probabilities)
+                - quantum processor: uses the largest amount of shots authorised by the hardware
+            device: The device specification
+            job_type: MyQLM job type to run:
+                - SAMPLE (default): measures Z on all qubits
+                - OBS: measures a specific observable, defined by a matrix on all qubits
+            observable: if "OBS" is selected as the job type, this is the matrix of
+                        the observable to measure.
+            qpu: QPU machine to use (quantum processor or simulator) with relevant keywords
+            mode: noise mode, can be active_qubits_only, parallelization_blocks, all_qubits
+            time_QLM_submission: toggle the print of the timing for the job submission to QLM
+
+        Raises:
+            TypeError: Job_type specified is neither 'SAMPLE' nor 'OBS'
+        """
+        self.name = "myqlm"
+        self.number_qubits = number_qubits
+        self.number_measurements = number_measurements
+        self.device = device
+        self.job_type = job_type
+        self._timing_qlm = time_QLM_submission
+        if qpu is None:
+            qpu = get_default_qpu()
+        self.qpu = qpu
+        self.all_qubits = True if mode == "all_qubits" else False
+
+        if job_type == "SAMPLE":
+            if observable is not None:
+                warnings.warn(
+                    "SAMPLE job type given, ignoring the observable matrix",
+                    stacklevel=2,
+                )
+            self.observable = None
+        elif job_type == "OBS":
+            if observable is None:
+                warnings.warn(
+                    "OBS job_type given without observable matrix, using Z on all qubits",
+                    stacklevel=2,
+                )
+                observable = np.array([[1, 0], [0, -1]])
+            self.observable = observable
+        else:
+            raise TypeError("Job_type specified is neither 'SAMPLE' nor 'OBS'")
+
+    def run_circuit(self, circuit: Circuit) -> Tuple[
+        Dict[str, List[List[bool]]],
+        Dict[str, List[List[float]]],
+        Dict[str, List[List[complex]]],
+    ]:
+        """Turn the circuit into MyQLM and save to file
+
+        Args:
+            circuit: The circuit that is run
+
+        Returns:
+            Tuple[Dict[str, List[List[bool]]],
+                  Dict[str, List[List[float]]],
+                  Dict[str, List[List[complex]]]]
+        """
+        # Initializing the classical registers for calculation and output
+        internal_bit_register_dict: Dict[str, List[bool]] = dict()
+        internal_float_register_dict: Dict[str, List[float]] = dict()
+        internal_complex_register_dict: Dict[str, List[complex]] = dict()
+
+        output_bit_register_dict: Dict[str, List[List[bool]]] = dict()
+        output_float_register_dict: Dict[str, List[List[float]]] = dict()
+        output_complex_register_dict: Dict[str, List[List[complex]]] = dict()
+
+        for bit_def in circuit.filter_by_tag("DefinitionBit"):
+            internal_bit_register_dict[bit_def.name()] = [
+                False for _ in range(bit_def.length())
+            ]
+            if bit_def.is_output():
+                output_bit_register_dict[bit_def.name()] = list()
+
+        for float_def in circuit.filter_by_tag("DefinitionFloat"):
+            internal_float_register_dict[float_def.name()] = [
+                0.0 for _ in range(float_def.length())
+            ]
+            if float_def.is_output():
+                output_float_register_dict[float_def.name()] = cast(
+                    List[List[float]], list()
+                )
+
+        for complex_def in circuit.filter_by_tag("DefinitionComplex"):
+            internal_complex_register_dict[complex_def.name()] = [
+                complex(0.0) for _ in range(complex_def.length())
+            ]
+            if complex_def.is_output():
+                output_complex_register_dict[complex_def.name()] = cast(
+                    List[List[complex]], list()
+                )
+
+        compiled_circuit = myqlm_call_circuit(
+            circuit, self.number_qubits, self.all_qubits
+        )
+
+        if self.observable is None:
+            job = compiled_circuit.to_job(
+                job_type="SAMPLE",
+                nbshots=self.number_measurements,
+                aggregate_data=False,
+            )
+        else:
+            obs = qat.core.Observable(nqbits=self.number_qubits, matrix=self.observable)
+            job = compiled_circuit.to_job(
+                job_type="OBS",
+                nbshots=self.number_measurements,
+                observable=obs,
+                aggregate_data=False,
+            )
+        start_time = time.time()
+        result = self.qpu.submit(job)
+        end_time = time.time()
+        if self._timing_qlm:
+            print(
+                f"Elapsed time (QLM job submission): {end_time - start_time:.2f} seconds"
+            )
+
+        for sample in result:
+            array = [qubit_state for qubit_state in sample.state]
+            output_bit_register_dict[list(output_bit_register_dict.keys())[0]].append(
+                array
+            )
+
+        return (
+            output_bit_register_dict,
+            output_float_register_dict,
+            output_complex_register_dict,
+        )
+
+    def run_measurement_registers(
+        self,
+        measurement: Any,  # noqa
+    ) -> Tuple[
+        Dict[str, List[List[bool]]],
+        Dict[str, List[List[float]]],
+        Dict[str, List[List[complex]]],
+    ]:
+        """Run all circuits of a measurement with the PyQuEST backend
+
+        Args:
+            measurement: The measurement that is run
+
+        Returns:
+            Tuple[Dict[str, List[List[bool]]],
+                  Dict[str, List[List[float]]],
+                  Dict[str, List[List[complex]]]]
+        """
+        constant_circuit = measurement.constant_circuit()
+        output_bit_register_dict: Dict[str, List[List[bool]]] = dict()
+        output_float_register_dict: Dict[str, List[List[float]]] = dict()
+        output_complex_register_dict: Dict[str, List[List[complex]]] = dict()
+        for circuit in measurement.circuits():
+            if constant_circuit is None:
+                run_circuit = circuit
+            else:
+                run_circuit = constant_circuit + circuit
+
+            (
+                tmp_bit_register_dict,
+                tmp_float_register_dict,
+                tmp_complex_register_dict,
+            ) = self.run_circuit(run_circuit)
+            output_bit_register_dict.update(tmp_bit_register_dict)
+            output_float_register_dict.update(tmp_float_register_dict)
+            output_complex_register_dict.update(tmp_complex_register_dict)
+        return (
+            output_bit_register_dict,
+            output_float_register_dict,
+            output_complex_register_dict,
+        )
+
+    def run_measurement(self, measurement: Any) -> Optional[Dict[str, float]]:  # noqa
+        """Run a circuit with the PyQuEST backend
+
+        Args:
+            measurement: The measurement that is run
+
+        Returns:
+            Optional[Dict[str, float]]
+        """
+        (
+            output_bit_register_dict,
+            output_float_register_dict,
+            output_complex_register_dict,
+        ) = self.run_measurement_registers(measurement)
+        return measurement.evaluate(
+            output_bit_register_dict,
+            output_float_register_dict,
+            output_complex_register_dict,
+        )
---
+++ packages/qoqo-myqlm/qoqo_myqlm-0.5.0-py3-none-any.whl/qoqo_myqlm/interface/myqlm_interface.py
@@ -0,0 +1,175 @@
+"""Define the MyQLM interface for qoqo operations."""
+
+# Copyright © 2019-2024 HQS Quantum Simulations GmbH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed under the License
+# is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing permissions and limitations under
+# the License.
+from qoqo import Circuit
+from typing import cast, List, Any
+import qat.lang.AQASM as qlm
+from .utilities import generate_VariableMSXX_matrix
+
+
+def myqlm_call_circuit(
+    circuit: Circuit, number_qubits: int, noise_mode_all_qubits: bool = False, **kwargs
+) -> qlm.Program:
+    """Translate the qoqo circuit into MyQLM ouput
+
+    The qoqo_myqlm interface iterates through the qoqo circuit and translates each qoqo operation
+    to MyQLM output (strings).
+
+    Args:
+        circuit: The qoqo circuit that is translated
+        number_qubits: Number of qubits in the quantum register
+        noise_mode_all_qubits: boolean to indicate whether to apply noise to all qubits or only to
+                                 active qubits
+        **kwargs: Additional keyword arguments
+
+    Returns:
+        qlm.Program: translated circuit
+    """
+    myqlm_program = qlm.Program()
+    qureg = myqlm_program.qalloc(number_qubits)
+    for op in circuit:
+        if "PragmaActiveReset" in op.tags():
+            myqlm_program.reset(op.involved_qubits)
+        elif "PragmaLoop" in op.tags():
+            number_of_repetitions = max(0, int(op.repetitions().value))
+            for _ in range(number_of_repetitions):
+                for op_loop in op.circuit():
+                    instructions = myqlm_call_operation(op_loop, qureg)
+                    if instructions is not None:
+                        myqlm_program.apply(*instructions)
+                        if noise_mode_all_qubits:
+                            apply_I_on_inactive_qubits(
+                                number_qubits, myqlm_program, qureg, instructions
+                            )
+        else:
+            instructions = myqlm_call_operation(op, qureg)
+            if instructions is not None:
+                myqlm_program.apply(*instructions)
+                if noise_mode_all_qubits:
+                    apply_I_on_inactive_qubits(
+                        number_qubits, myqlm_program, qureg, instructions
+                    )
+
+    myqlm_circuit = myqlm_program.to_circ()
+    return myqlm_circuit
+
+
+def apply_I_on_inactive_qubits(
+    number_qubits: int,
+    myqlm_program: qlm.program.Program,
+    qureg: qlm.bits.QRegister,
+    instructions: List,
+) -> None:
+    """Applies an I gate to all inactive qubits in a quantum circuit.
+
+    Args:
+        number_qubits: The total number of qubits in the circuit.
+        myqlm_program : The QLM program to which to add the gate operations.
+        qureg : The quantum register to which to apply the gate operations.
+        instructions : A list of instructions specifying the active qubits.
+    """
+    active_qubits = [int(qb.to_dict()["data"]) for qb in instructions[1:]]
+    for qubit in range(number_qubits):
+        if qubit not in active_qubits:
+            myqlm_program.apply(qlm.I, qureg[qubit])
+
+
+def myqlm_call_operation(operation: Any, qureg: qlm.Program.qalloc) -> List:  # noqa
+    """Translate a qoqo operation to MyQLM text
+
+    Args:
+        operation: The qoqo operation that is translated
+        qureg: The quantum register pyquest_cffi operates on
+
+    Returns:
+        List: arguments to be used in the "apply" function
+
+    Raises:
+        RuntimeError: Operation not in MyQLM backend
+    """
+    op = cast(List, None)
+    tags = operation.tags()
+    if "RotateZ" in tags:
+        op = [qlm.RZ(operation.theta().float()), qureg[operation.qubit()]]
+    elif "RotateX" in tags:
+        op = [qlm.RX(operation.theta().float()), qureg[operation.qubit()]]
+    elif "RotateY" in tags:
+        op = [qlm.RY(operation.theta().float()), qureg[operation.qubit()]]
+    elif "CNOT" in tags:
+        op = [qlm.CNOT, qureg[operation.control()], qureg[operation.target()]]
+    elif "Hadamard" in tags:
+        op = [qlm.H, qureg[operation.qubit()]]
+    elif "PauliX" in tags:
+        op = [qlm.X, qureg[operation.qubit()]]
+    elif "PauliY" in tags:
+        op = [qlm.Y, qureg[operation.qubit()]]
+    elif "PauliZ" in tags:
+        op = [qlm.Z, qureg[operation.qubit()]]
+    elif "SGate" in tags:
+        op = [qlm.S, qureg[operation.qubit()]]
+    elif "TGate" in tags:
+        op = [qlm.T, qureg[operation.qubit()]]
+    elif "ControlledPauliZ" in tags:
+        op = [qlm.CSIGN, qureg[operation.control()], qureg[operation.target()]]
+    elif "ControlledPauliY" in tags:
+        op = [qlm.Y.ctrl(), qureg[operation.control()], qureg[operation.target()]]
+    elif "SWAP" in tags:
+        op = [qlm.SWAP, qureg[operation.control()], qureg[operation.target()]]
+    elif "ISwap" in tags:
+        op = [qlm.ISWAP, qureg[operation.control()], qureg[operation.target()]]
+    elif "VariableMSXX" in tags:
+        gate = qlm.AbstractGate(
+            "VariableMSXX",
+            [float],
+            arity=2,
+            matrix_generator=generate_VariableMSXX_matrix,
+        )
+        op = [
+            gate(operation.theta().float()),
+            qureg[operation.control()],
+            qureg[operation.target()],
+        ]
+
+    elif "SingleQubitGate" in tags:
+        matrix = operation.unitary_matrix()
+        gate = qlm.AbstractGate(tags[-1], [], arity=1, matrix_generator=lambda: matrix)
+        op = [gate(), qureg[operation.qubit()]]
+    elif "SingleQubitGateOperation" in tags:
+        matrix = operation.unitary_matrix()
+        gate = qlm.AbstractGate(tags[-1], [], arity=1, matrix_generator=lambda: matrix)
+        op = [gate(), qureg[operation.qubit()]]
+    elif "TwoQubitGateOperation" in tags:
+        gate_name = tags[-1]
+        matrix = operation.unitary_matrix()
+        gate = qlm.AbstractGate(gate_name, [], arity=2, matrix_generator=lambda: matrix)
+        op = [gate(), qureg[operation.control()], qureg[operation.target()]]
+    elif "MeasureQubit" in tags:
+        pass
+    elif "Definition" in tags:
+        pass
+    elif "PragmaRepeatedMeasurement" in tags:
+        pass
+    elif "PragmaSetNumberOfMeasurements" in tags:
+        pass
+    elif "PragmaStartDecompositionBlock" in tags:
+        pass
+    elif "PragmaGlobalPhase" in tags:
+        pass
+    elif "PragmaStopDecompositionBlock" in tags:
+        pass
+    elif "PragmaStopParallelBlock" in tags:
+        pass
+    else:
+        raise RuntimeError(f"Operation not in MyQLM backend tags={tags}")
+
+    return op
