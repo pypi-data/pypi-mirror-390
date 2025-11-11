5cf08203d21f98362bfe7e573edb9a5682862d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:20 +0000

    Add package csemodule/0.1.0/csemodule-0.1.0.tar.gz

---
+++ packages/csemodule/csemodule-0.1.0.tar.gz/csemodule-0.1.0/csemodule/__init__.py
@@ -0,0 +1,14 @@
+"""
+Example Module
+"""
+
+__version__ = "0.1.0"
+
+def hello_world():
+    """Return a greeting message."""
+    return "Hello, World!"
+
+def add_numbers(a, b):
+    """Add two numbers and return the result."""
+    return a + b
+
