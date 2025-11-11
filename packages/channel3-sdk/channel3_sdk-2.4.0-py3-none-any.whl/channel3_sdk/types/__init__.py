93f2c00826e3f149fa5e97f6467305a0736f0252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 08:06:30 +0000

    Add package channel3-sdk/2.4.0/channel3_sdk-2.4.0-py3-none-any.whl

---
+++ packages/channel3-sdk/channel3_sdk-2.4.0-py3-none-any.whl/channel3_sdk/types/__init__.py
@@ -0,0 +1,14 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from .brand import Brand as Brand
+from .price import Price as Price
+from .variant import Variant as Variant
+from .brand_list_params import BrandListParams as BrandListParams
+from .availability_status import AvailabilityStatus as AvailabilityStatus
+from .search_perform_params import SearchPerformParams as SearchPerformParams
+from .search_perform_response import SearchPerformResponse as SearchPerformResponse
+from .enrich_enrich_url_params import EnrichEnrichURLParams as EnrichEnrichURLParams
+from .product_retrieve_response import ProductRetrieveResponse as ProductRetrieveResponse
+from .enrich_enrich_url_response import EnrichEnrichURLResponse as EnrichEnrichURLResponse
