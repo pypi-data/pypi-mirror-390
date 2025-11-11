b7b6f6d75b1e58828b2d9c1249a287cafd47dc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 04:08:47 +0000

    Add package aidev-ai-blueking/1.2.8.post2/aidev_ai_blueking-1.2.8.post2.tar.gz

---
+++ packages/aidev-ai-blueking/aidev_ai_blueking-1.2.8.post2.tar.gz/aidev_ai_blueking-1.2.8.post2/aidev_ai_blueking/urls.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.urls import re_path
+
+from .views import IndexView
+
+urlpatterns = [
+    re_path(r"^$", IndexView.as_view(), name="index"),
+    re_path(r"^page/$", IndexView.as_view(), name="index"),
+    re_path(r"^side-slider/$", IndexView.as_view(), name="index"),
+    re_path(r"^403/$", IndexView.as_view(), name="index"),
+]
