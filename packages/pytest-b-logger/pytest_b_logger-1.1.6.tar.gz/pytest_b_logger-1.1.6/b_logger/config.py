5fc93429a3601f3ddb6033ee68df59b30f83549f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:08:53 +0000

    Add package pytest-b-logger/1.1.6/pytest_b_logger-1.1.6.tar.gz

---
+++ packages/pytest-b-logger/pytest_b_logger-1.1.6.tar.gz/pytest_b_logger-1.1.6/b_logger/config.py
@@ -0,0 +1,99 @@
+from typing import Optional
+import yaml
+from pathlib import Path
+
+from b_logger.utils.paths import pathfinder
+
+
+class BLoggerConfig:
+    _instance: Optional["BLoggerConfig"] = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    @classmethod
+    def get(cls) -> "BLoggerConfig":
+        if cls._instance is None:
+            cls._instance = BLoggerConfig()
+        return cls._instance
+
+    def __init__(self, path: str = f'{pathfinder.project_root()}/blog.config.yaml'):
+        self._extra = {}
+
+        config_path = Path(path)
+        self._data = self._load_config_file(config_path)
+
+        # YAML
+        self.project_name: Optional[str] = self._data.get("project_name")
+        self.env: Optional[str] = self._data.get("env")
+        self.base_url: Optional[str] = self._data.get("base_url")
+
+        self.integrations = self._data.get("integrations", {})
+        self.qase: bool = bool(self.integrations.get("qase", False))
+        self.allure: bool = bool(self.integrations.get("allure", False))
+
+        self.links = self._data.get("links", {})
+
+        # Other
+        self._extra = {
+            k: v for k, v in self._data.items()
+            if not hasattr(self, k)
+        }
+
+    @staticmethod
+    def _load_config_file(path: Path = None) -> dict:
+        if not path.exists():
+            raise FileNotFoundError(f'[BLogger][ERROR] blog.config.yaml file not found: {path}')
+        with path.open("r", encoding="utf-8") as f:
+            return yaml.safe_load(f) or {}
+
+    def apply_cli_options(self, config):
+        for opt_name in config.option.__dict__:
+            if opt_name.startswith("blog_"):
+                value = getattr(config.option, opt_name)
+                if value is not None:
+                    field_name = opt_name.replace("blog_", "")
+                    setattr(self, field_name, value)
+
+    def __getitem__(self, key: str):
+        return getattr(self, key, self._extra.get(key, None))
+
+    def __setitem__(self, key: str, value):
+        if hasattr(self, key):
+            setattr(self, key, value)
+        else:
+            self._extra[key] = value
+
+    def __getattr__(self, key):
+        _extra = object.__getattribute__(self, "_extra") if "_extra" in self.__dict__ else {}
+        if key in _extra:
+            return _extra[key]
+        print(f'[BLogger][WARN] blog_config object has no attribute "{key}"')
+
+    def __setattr__(self, key, value):
+        if key in {
+            "_data", "_extra", "project_name", "env", "base_url",
+            "qase", "allure", "links"
+        }:
+            object.__setattr__(self, key, value)
+        else:
+            self._extra[key] = value
+
+    def as_dict(self) -> dict:
+        base = {
+            "project_name": self.project_name,
+            "env": self.env,
+            "base_url": self.base_url,
+            "qase": self.qase,
+            "allure": self.allure,
+            "links": self.links,
+        }
+        return {**base, **self._extra}
+
+    def __repr__(self):
+        return f"<BLoggerConfig {self.as_dict()}>"
+
+
+blog_config = BLoggerConfig.get()
---
+++ packages/pytest-b-logger/pytest_b_logger-1.1.6.tar.gz/pytest_b_logger-1.1.6/b_logger/entities/tests.py
@@ -0,0 +1,99 @@
+import time
+import uuid
+from typing import Any
+
+from b_logger.entities.attachments import Attachment
+from b_logger.entities.steps import StepContainer
+from b_logger.entities.statuses import TestStatus
+from b_logger.utils.basedatamodel import BaseDataModel
+from b_logger.utils.paths import b_logs_tmp_steps_path
+
+
+# class TestError(BaseDataModel):
+#     def __init__(self, exc, stacktrace):
+#         self.exception = exc
+#         self.stacktrace = stacktrace
+
+
+# class TestInfo(BaseDataModel):
+#     def __init__(self):
+#         self.description = None
+#         self.parameters = {}
+#         self.known_bugs = []
+#         self.other = []
+#
+#     def add_parameter(self, key, value):
+#         self.parameters[key]= value
+
+
+class TestReport(BaseDataModel):
+
+    def __init__(self, module: str = None, name: str = None, originalname: str = None):
+        self.module: str = module
+        self.name: str = name
+        self.originalname: str = originalname
+        self.status: TestStatus = TestStatus.NONE
+        self.start_time = round(time.time(), 4)
+        self.duration: float | None = None
+        self.description: str | None = None
+        self.info = {}
+        self.attachments = []
+        self.known_bugs = []
+        self.steps = None
+        self.error = None
+        self.stacktrace = None
+
+    def set_status(self, status: TestStatus):
+        self.status = status
+
+    def set_duration(self, duration: float):
+        self.duration = duration
+
+    def set_steps(self, steps_id):
+        self.steps = steps_id
+
+    def set_error(self, error: str):
+        self.error = error
+
+    def set_stacktrace(self, stacktrace: str):
+        self.stacktrace = stacktrace
+
+    def set_description(self, description: str):
+        self.description = description
+
+    def modify_description(self, description: str):
+        self.description += f'\n\n{description}'
+
+    def add_known_bug(self, bug):
+        self.known_bugs.append(bug)
+
+    def add_info(self, info: dict):
+        for key, value in info.items():
+            if key in self.info:
+                existing = self.info[key]
+                if not isinstance(existing, list):
+                    self.info[key] = [existing]
+                self.info[key].append(value)
+            else:
+                self.info[key] = value
+
+    def add_links(self, links: dict):
+        existing_links = self.info.get('links', {})
+        if existing_links:
+            for key, value in links.items():
+                existing_links[key] = value
+        else:
+            self.info['links'] = {}
+            for key, value in links.items():
+                self.info['links'][key] = value
+
+    def add_attachment(self, attachment: Attachment):
+        self.attachments.append(attachment)
+
+    # def get_steps(self):
+    #     steps_path = f'{b_logs_tmp_steps_path()}/{self.steps}.json'
+    #     try:
+    #         steps = StepContainer.from_json(steps_path)
+    #         return steps
+    #     except Exception as e:
+    #         print(f'[BLogger][WARN] Unable to get steps for {self.name}: {e}')
