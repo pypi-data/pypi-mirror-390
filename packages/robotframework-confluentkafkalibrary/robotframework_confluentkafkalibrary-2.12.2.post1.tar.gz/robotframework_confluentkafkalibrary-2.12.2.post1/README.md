b83a40b4acf7654014f4e5bc0e50cea4d19d091c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:06:12 +0000

    Add package robotframework-confluentkafkalibrary/2.12.2.post1/robotframework_confluentkafkalibrary-2.12.2.post1.tar.gz

---
+++ packages/robotframework-confluentkafkalibrary/robotframework_confluentkafkalibrary-2.12.2.post1.tar.gz/robotframework_confluentkafkalibrary-2.12.2.post1/README.md
@@ -0,0 +1,60 @@
+# Robot Framework - ConfluentKafkaLibrary
+
+ConfluentKafkaLibrary library is a wrapper for the [confluent-kafka-python](https://github.com/confluentinc/confluent-kafka-python).
+
+ConfluentKafkaLibrary is compatible with the latest version of confluent-kafka-python, where the library versions have a 1:1 correspondence (e.g., ConfluentKafkaLibrary 2.9.0 corresponds to confluent-kafka-python 2.9.0). Bug fixes and updates are denoted by a post-release identifier, such as `2.9.0.post1`.
+
+## Documentation
+
+The keyword documentation for ConfluentKafkaLibrary can be found [here](https://robooo.github.io/robotframework-ConfluentKafkaLibrary/)
+
+To generate the documentation, use the following command:
+
+```
+python -m robot.libdoc -f html src/ConfluentKafkaLibrary docs/index.html
+```
+
+## Installation
+
+To install the library, run the following command:
+
+```
+pip install robotframework-confluentkafkalibrary
+```
+
+Extra packages:
+* `[avro]` = `['fastavro >= 1.3.2', 'avro >= 1.11.1']`
+* `[json]` = `['jsonschema >= 3.2.0', 'pyrsistent >= 0.20.0']`
+* `[protobuf]` = `['protobuf >= 4.22.0', 'googleapis-common-protos >= 1.66.0']`
+* `[schemaregistry]` = `['httpx>=0.26', 'cachetools >= 5.5.0', 'attrs >= 24.3.0']`
+
+* To install all dependencies use `[all]` extension like:
+
+```
+pip install robotframework-confluentkafkalibrary[all]
+```
+
+## Usage
+
+In most cases, you can refer to the [confluent-kafka-python documentation](https://docs.confluent.io/platform/current/clients/confluent-kafka-python/html/index.html) for guidance. Every keyword in ConfluentKafkaLibrary is designed to match the corresponding Python functions. If you are unsure about the pre-configured keywords, please visit the  [robotframework-ConfluentKafkaLibrary documentation](https://robooo.github.io/robotframework-ConfluentKafkaLibrary/). The Kafka team maintains the up-to-date documentation for configuration properties and their values [here](https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md).
+
+* You can find basic usage examples in the [./examples/test.robot](./examples/test.robot)
+* For more complex examples, such as handling byte data from a topic, using multiple consumers, or running threaded avro consumers, please refer to the [documentation](https://robooo.github.io/robotframework-ConfluentKafkaLibrary/#Examples).
+
+## Testing
+
+* The library is tested using black-box tests written in Robot Framework.
+  * You can find the test files in the [examples/ directory](./examples) directory.
+* For testing, a dockerized enterprise Kafka platform with schema registry support and REST proxy is used. The platform is deployed and tested for each pull request and merge to the master branch.
+  * See [docker-compose.yml](./examples/docker-compose.yml) file with the necessary configuration.
+* Tests are divided into the following files:
+  * test.robot - Basic tests to verify functionality of the Consumer and Producer.
+  * test_adminclient.robot - Verifications of admin client functionality.
+  * test_avro.robot - Verifications of avro and serializers functionality.
+* Not executable example of oauth usage can be found [here](https://github.com/robooo/robotframework-ConfluentKafkaLibrary/blob/master/examples/test_oauth.robot#L14)
+  * Update of deployment https://github.com/robooo/robotframework-ConfluentKafkaLibrary/issues/21 is required.
+* The core testing logic involves producing data to Kafka, connecting one consumer in a thread, and working with the results in specific test cases.
+
+## Known Limitations:
+* Unable to install robotframework-confluentkafkalibrary on Amazon EC2 graviton instance type
+  * see the [steps to resolve](https://github.com/robooo/robotframework-ConfluentKafkaLibrary/issues/33#issuecomment-1464644752)
---
+++ packages/robotframework-confluentkafkalibrary/robotframework_confluentkafkalibrary-2.12.2.post1.tar.gz/robotframework_confluentkafkalibrary-2.12.2.post1/src/ConfluentKafkaLibrary/admin_client.py
@@ -0,0 +1,233 @@
+import uuid
+from confluent_kafka.admin import AdminClient
+from confluent_kafka import KafkaException, TopicCollection
+
+
+class KafkaAdminClient():
+
+    def __init__(self):
+        self.admin_clients = {}
+
+    def create_admin_client(
+        self,
+        group_id=None,
+        server="127.0.0.1",
+        port="9092",
+        **kwargs
+    ):
+        if group_id is None:
+            group_id = str(uuid.uuid4())
+
+        admin_client = AdminClient({
+            'bootstrap.servers': '{}:{}'.format(server, port),
+            **kwargs})
+
+        self.admin_clients[group_id] = admin_client
+        return group_id
+
+    def list_groups(self, group_id, states=None, types=None, request_timeout=10):
+        """List consumer groups.
+        - ``states`` (list(ConsumerGroupState)): filter consumer groups which are currently in these states.
+            For example usage see 'AdminClient List Consumer Groups' at
+            examples/test_adminclient.py
+            Default: `None`.
+        - ``request_timeout`` (int): Maximum response time before timing out.
+            Default: `10`.
+        """
+        states = states or []
+        types = types or []
+
+        future = self.admin_clients[group_id].list_consumer_groups(request_timeout=request_timeout, states=set(states), types=set(types))
+        return future.result()
+
+    def describe_groups(self, group_id, group_ids, request_timeout=10, **kwargs):
+        """Describe consumer groups.
+        - ``group_ids`` (list(str)): List of group_ids which need to be described.
+        - ``request_timeout`` (int): Maximum response time before timing out.
+            Default: `10`.
+        """
+        response = self.admin_clients[group_id].describe_consumer_groups(group_ids, request_timeout=request_timeout, **kwargs)
+
+        groups_results={}
+        for con_id in group_ids:
+            try:
+                if response[con_id].exception() is None:
+                    groups_results[con_id] = response[con_id].result()
+                else:
+                    groups_results[con_id] = response[con_id].exception()
+            except KafkaException as e:
+                return f"Failed to describe group {con_id}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return groups_results
+
+    def delete_groups(self, group_id, group_ids, request_timeout=10):
+        """Delete the given consumer groups.
+        - ``group_ids`` (list(str)): List of group_ids which need to be deleted.
+        - ``request_timeout`` (int): Maximum response time before timing out.
+            Default: `10`.
+        """
+        response = self.admin_clients[group_id].delete_consumer_groups(group_ids, request_timeout=request_timeout)
+
+        groups_results={}
+        for con_id in group_ids:
+            try:
+                if response[con_id].exception() is None:
+                    groups_results[con_id] = response[con_id].result()
+                else:
+                    groups_results[con_id] = response[con_id].exception()
+            except KafkaException as e:
+                return f"Failed to delete group {con_id}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return groups_results
+
+    def create_topics(self, group_id, new_topics, **kwargs):
+        """Create one or more new topics and wait for each one to finish.
+        - ``new_topics`` (list(NewTopic) or NewTopic): A list of specifications (NewTopic)
+            or a single instance for the topics that should be created.
+        """
+        fs = None
+        if isinstance(new_topics, list):
+            fs = self.admin_clients[group_id].create_topics(new_topics, **kwargs)
+        else:
+            fs = self.admin_clients[group_id].create_topics([new_topics], **kwargs)
+
+        topics_results={}
+        for topic, f in fs.items():
+            try:
+                if f.exception() is None:
+                    topics_results[topic] = f.result()
+                else:
+                    topics_results[topic] = f.exception()
+            except KafkaException as e:
+                return f"Failed to create topic {topic}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return topics_results
+
+    def delete_topics(self, group_id, topics, **kwargs):
+        if isinstance(topics, str):
+            topics = [topics]
+
+        fs = self.admin_clients[group_id].delete_topics(topics, **kwargs)
+
+        topics_results={}
+        for topic, f in fs.items():
+            try:
+                if f.exception() is None:
+                    topics_results[topic] = f.result()
+                else:
+                    topics_results[topic] = f.exception()
+            except KafkaException as e:
+                return f"Failed to delete topic {topic}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return topics_results
+
+    def create_partitions(self, group_id, new_partitions, **kwargs):
+        """Create additional partitions for the given topics.
+        - ``new_partitions``  (list(NewPartitions) or NewPartitions): New partitions to be created.
+        """
+        fs = None
+        if isinstance(new_partitions, list):
+            fs = self.admin_clients[group_id].create_partitions(new_partitions, **kwargs)
+        else:
+            fs = self.admin_clients[group_id].create_partitions([new_partitions], **kwargs)
+
+        partitions_results={}
+        for partition, f in fs.items():
+            try:
+                if f.exception() is None:
+                    partitions_results[partition] = f.result()
+                else:
+                    partitions_results[partition] = f.exception()
+            except KafkaException as e:
+                return f"Failed to add partitions to topic {partition}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return partitions_results
+
+    def describe_configs(self, group_id, resources, **kwargs):
+        """Get the configuration of the specified resources.
+        - ``resources``  (list(ConfigResource) or ConfigResource): Resources to get the configuration for.
+        """
+        fs = None
+        if isinstance(resources, list):
+            fs = self.admin_clients[group_id].describe_configs(resources, **kwargs)
+        else:
+            fs = self.admin_clients[group_id].describe_configs([resources], **kwargs)
+
+        config_results={}
+        for config, f in fs.items():
+            try:
+                if f.exception() is None:
+                    config_results[config.name] = f.result()
+                else:
+                    config_results[config.name] = f.exception()
+            except KafkaException as e:
+                return f"Failed to describe config {config.name}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return config_results
+
+    def describe_topics(self, group_id, topics, **kwargs):
+        """Describe topics.
+        - ``topics``  (list(str) or str): List of topic names or only topic name to describe.
+        """
+        if isinstance(topics, list):
+            topics = TopicCollection(topics)
+        else:
+            topics = TopicCollection([topics])
+
+        topics = self.admin_clients[group_id].describe_topics(topics, **kwargs)
+        topics_results={}
+        for topic, f in topics.items():
+            try:
+                if f.exception() is None:
+                    topics_results[topic] = f.result()
+                else:
+                    topics_results[topic] = f.exception()
+            except KafkaException as e:
+                return f"Failed to describe topic {topic.name}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return topics_results
+
+    def describe_cluster(self, group_id, **kwargs):
+        """Describe cluster.
+        """
+        cluster = self.admin_clients[group_id].describe_cluster(**kwargs)
+        try:
+            if cluster.exception() is None:
+                cluster = cluster.result()
+            else:
+                cluster = cluster.exception()
+        except KafkaException as e:
+            return f"Failed to describe cluster: {e}"
+        except (TypeError, ValueError ) as e:
+            return f"Invalid input: {e}"
+        return cluster
+
+    def alter_configs(self, group_id, resources, **kwargs):
+        """Update configuration properties for the specified resources.
+        - ``resources``  (list(ConfigResource) or ConfigResource): Resources to update configuration of.
+        """
+        fs = None
+        if isinstance(resources, list):
+            fs = self.admin_clients[group_id].alter_configs(resources, **kwargs)
+        else:
+            fs = self.admin_clients[group_id].alter_configs([resources], **kwargs)
+
+        config_results={}
+        for config, f in fs.items():
+            try:
+                if f.exception() is None:
+                    config_results[config.name] = f.result()
+                else:
+                    config_results[config.name] = f.exception()
+            except KafkaException as e:
+                return f"Failed to alter config {config.name}: {e}"
+            except (TypeError, ValueError ) as e:
+                return f"Invalid input: {e}"
+        return config_results
