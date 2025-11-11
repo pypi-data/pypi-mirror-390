95ddfd2b9987db4bf67aefde1ee25ed9e01c5af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:28:18 +0000

    Add package novemberff/0.1.0/novemberff-0.1.0.tar.gz

---
+++ packages/novemberff/novemberff-0.1.0.tar.gz/novemberff-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="novemberff",
+    version="0.1.0",
+    description="Custom implementation of the AMBER forcefield, focused on simplicity and verbose outputs.",
+    keywords="Amber Forcefield MolecularDynamics MD Simulation",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="DiegoBarMor",
+    author_email="diegobarmor42@gmail.com",
+    url="https://github.com/diegobarmor/novemberff",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
