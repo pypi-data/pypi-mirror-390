b203faae07798c4a5d6d2e15ef02e25838e88df1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 08:07:02 +0000

    Add package piezense/0.0.3/piezense-0.0.3.tar.gz

---
+++ packages/piezense/piezense-0.0.3.tar.gz/piezense-0.0.3/setup.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    name="piezense",
+    version="0.0.3",
+    author="Haptica Robotics",
+    author_email="info@hapticarobotics.com",
+    packages=["piezense"],
+    description="A Python package for interfacing with PieZense pneumatic systems",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/haptica-robotics/piezense",
+    license='LicenseRef-Proprietary',
+    python_requires='>=3.8',
+    install_requires=["bleak==1.1.1"]
+)
+
