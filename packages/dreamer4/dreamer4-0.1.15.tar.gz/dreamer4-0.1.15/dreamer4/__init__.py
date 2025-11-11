10e39aca8be9cbd25a34356ed1bc8603ac4c7b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:09:25 +0000

    Add package dreamer4/0.1.15/dreamer4-0.1.15.tar.gz

---
+++ packages/dreamer4/dreamer4-0.1.15.tar.gz/dreamer4-0.1.15/dreamer4/__init__.py
@@ -0,0 +1,11 @@
+from dreamer4.dreamer4 import (
+    VideoTokenizer,
+    DynamicsWorldModel
+)
+
+
+from dreamer4.trainers import (
+    VideoTokenizerTrainer,
+    BehaviorCloneTrainer,
+    DreamTrainer
+)
