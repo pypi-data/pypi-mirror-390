be2b0e678a02303ae2c34dc4bd896487f4a75fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:06:42 +0000

    Add package superoptix/0.1.9/superoptix-0.1.9-py3-none-any.whl

---
+++ packages/superoptix/superoptix-0.1.9-py3-none-any.whl/superoptix/vendor/deepagents/__init__.py
@@ -0,0 +1,7 @@
+"""DeepAgents package."""
+
+from superoptix.vendor.deepagents.graph import create_deep_agent
+from superoptix.vendor.deepagents.middleware.filesystem import FilesystemMiddleware
+from superoptix.vendor.deepagents.middleware.subagents import CompiledSubAgent, SubAgent, SubAgentMiddleware
+
+__all__ = ["CompiledSubAgent", "FilesystemMiddleware", "SubAgent", "SubAgentMiddleware", "create_deep_agent"]
