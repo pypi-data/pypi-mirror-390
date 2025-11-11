8612bc2e93360186cb4c475f459ee0f8b1d48d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 04:08:45 +0000

    Add package ordeq_common/2.2.0/ordeq_common-2.2.0-py3-none-any.whl

---
+++ packages/ordeq_common/ordeq_common-2.2.0-py3-none-any.whl/ordeq_common/io/bytes_buffer.py
@@ -0,0 +1,74 @@
+from dataclasses import InitVar, dataclass, field
+from io import BytesIO
+
+from ordeq import IO
+
+
+@dataclass(frozen=True, eq=False)
+class BytesBuffer(IO[bytes]):
+    """IO that uses an in-memory bytes buffer to load and save data. Useful
+    for buffering data across nodes without writing to disk.
+
+    Example:
+
+    ```pycon
+    >>> from ordeq_common import BytesBuffer
+    >>> buffer = BytesBuffer()
+    >>> buffer.load()
+    b''
+
+    ```
+
+    The buffer is initially empty, unless provided with initial data:
+
+    ```pycon
+    >>> buffer = BytesBuffer(b"Initial data")
+    >>> buffer.load()
+    b'Initial data'
+
+    ```
+
+    Saving to the buffer appends data to the existing content:
+
+    ```pycon
+    >>> buffer.save(b"New data")
+    >>> buffer.load()
+    b'Initial dataNew data'
+
+    ```
+
+    Example in a node:
+
+    ```pycon
+    >>> from ordeq_args import CommandLineArg
+    >>> from ordeq_common import BytesBuffer, Literal
+    >>> from ordeq import node, run
+    >>> result = BytesBuffer()
+    >>> @node(
+    ...     inputs=[BytesBuffer(b"Hello"), Literal(b"you")], outputs=result
+    ... )
+    ... def greet(greeting: bytes, name: bytes) -> bytes:
+    ...     return greeting + b" to " + name + b"!"
+    >>> _ = run(greet)
+    >>> result.load()
+    b'Hello to you!'
+
+    ```
+
+    """
+
+    _buffer: BytesIO = field(default_factory=BytesIO, init=False)
+    value: InitVar[bytes | None] = None
+
+    def __post_init__(self, value: bytes | None):
+        if value is not None:
+            self._buffer.write(value)
+
+    def load(self) -> bytes:
+        return self._buffer.getvalue()
+
+    def save(self, data: bytes) -> None:
+        self._buffer.write(data)
+
+    def persist(self, _) -> None:
+        return super().persist(self._buffer.getvalue())
---
+++ packages/ordeq_common/ordeq_common-2.2.0-py3-none-any.whl/ordeq_common/io/string_buffer.py
@@ -0,0 +1,75 @@
+from dataclasses import InitVar, dataclass, field
+from io import StringIO
+
+from ordeq import IO
+
+
+@dataclass(frozen=True, eq=False)
+class StringBuffer(IO[str]):
+    """IO that uses an in-memory string buffer to load and save data. Useful
+    for buffering data across nodes without writing to disk.
+
+    Example:
+
+    ```pycon
+    >>> from ordeq_common import StringBuffer
+    >>> buffer = StringBuffer()
+    >>> buffer.load()
+    ''
+
+    ```
+
+    The buffer is initially empty, unless provided with initial data:
+
+    ```pycon
+    >>> buffer = StringBuffer("Initial data")
+    >>> buffer.load()
+    'Initial data'
+
+    ```
+
+    Saving to the buffer appends data to the existing content:
+
+    ```pycon
+    >>> buffer.save("New data")
+    >>> buffer.load()
+    'Initial dataNew data'
+
+    ```
+
+    Example in a node:
+
+    ```pycon
+    >>> from ordeq_args import CommandLineArg
+    >>> from ordeq_common import StringBuffer, Literal
+    >>> from ordeq import node, run
+    >>> result = StringBuffer()
+    >>> @node(
+    ...     inputs=[StringBuffer("Hello"), Literal("you")],
+    ...     outputs=result
+    ... )
+    ... def greet(greeting: str, name: str) -> str:
+    ...     return f"{greeting} to {name}!"
+    >>> _ = run(greet)
+    >>> result.load()
+    'Hello to you!'
+
+    ```
+
+    """
+
+    _buffer: StringIO = field(default_factory=StringIO, init=False)
+    value: InitVar[str | None] = None
+
+    def __post_init__(self, value: str | None):
+        if value is not None:
+            self._buffer.write(value)
+
+    def load(self) -> str:
+        return self._buffer.getvalue()
+
+    def save(self, data: str) -> None:
+        self._buffer.write(data)
+
+    def persist(self, _) -> None:
+        return super().persist(self._buffer.getvalue())
