ba0784c1eece1617b57e561ee1d3fbcd6fb47307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 14:06:37 +0000

    Add package iam-policy-validator/1.7.1/iam_policy_validator-1.7.1-py3-none-any.whl

---
+++ packages/iam-policy-validator/iam_policy_validator-1.7.1-py3-none-any.whl/iam_validator/__version__.py
@@ -0,0 +1,7 @@
+"""Version information for IAM Validator.
+
+This file is the single source of truth for the package version.
+"""
+
+__version__ = "1.7.1"
+__version_info__ = tuple(int(part) for part in __version__.split("."))
