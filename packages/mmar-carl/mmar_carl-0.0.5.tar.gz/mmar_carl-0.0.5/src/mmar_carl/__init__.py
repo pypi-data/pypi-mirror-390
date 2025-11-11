137145f03e1d2b16dac92a528b478932f455b145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 05:07:55 +0000

    Add package mmar-carl/0.0.5/mmar_carl-0.0.5.tar.gz

---
+++ packages/mmar-carl/mmar_carl-0.0.5.tar.gz/mmar_carl-0.0.5/src/mmar_carl/__init__.py
@@ -0,0 +1,22 @@
+"""
+MAESTRO CARL (Collaborative Agent Reasoning Library)
+
+A library for building chain-of-thought reasoning systems with DAG-based parallel execution.
+"""
+
+from .chain import ChainBuilder, ReasoningChain
+from .executor import DAGExecutor
+from .models import Language, PromptTemplate, ReasoningContext, ReasoningResult, StepDescription, StepExecutionResult
+
+__version__ = "0.0.5"
+__all__ = [
+    "Language",
+    "StepDescription",
+    "ReasoningContext",
+    "StepExecutionResult",
+    "ReasoningResult",
+    "PromptTemplate",
+    "ReasoningChain",
+    "ChainBuilder",
+    "DAGExecutor",
+]
