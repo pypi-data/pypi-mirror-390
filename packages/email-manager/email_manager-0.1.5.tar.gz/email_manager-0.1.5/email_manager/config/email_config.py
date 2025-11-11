8386a3a0f303d0d1e45f3d7daee45fd329f1a2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 07:05:37 +0000

    Add package email-manager/0.1.5/email_manager-0.1.5.tar.gz

---
+++ packages/email-manager/email_manager-0.1.5.tar.gz/email_manager-0.1.5/email_manager/config/email_config.py
@@ -0,0 +1,16 @@
+class EmailConfig:
+    def __init__(
+        self,
+        provider_type: str,
+        timeout: int = 10,
+        max_retries: int = 3,
+        mailgun_params: dict = None,
+        smtp_params: dict = None,
+        mailtrap_params: dict = None,
+    ):
+        self.provider_type = provider_type.lower()
+        self.timeout = timeout
+        self.max_retries = max_retries
+        self.mailgun_params = mailgun_params or {}
+        self.smtp_params = smtp_params or {}
+        self.mailtrap_params = mailtrap_params or {}
