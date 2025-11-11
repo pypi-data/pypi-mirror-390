d8ec74c724fd981832179f0ef9acedfbde2f316d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 06:09:13 +0000

    Add package epicallypowerful/1.0.3/epicallypowerful-1.0.3-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl

---
+++ packages/epicallypowerful/epicallypowerful-1.0.3-cp314-cp314-manylinux2014_aarch64.manylinux_2_17_aarch64.manylinux_2_28_aarch64.whl/epicallypowerful/sensing/__init__.py
@@ -0,0 +1,9 @@
+# Import Mangement for epicallypowerful sensing modules
+import epicallypowerful.sensing.imu_data
+import epicallypowerful.sensing.mpu9250.mpu9250_imu
+import epicallypowerful.sensing.microstrain.microstrain_imu
+import epicallypowerful.sensing.open_imu.open_imu
+from .imu_data import IMUData
+from .microstrain.microstrain_imu import MicroStrainIMUs
+from .mpu9250.mpu9250_imu import MPU9250IMUs
+from .open_imu.open_imu import OpenIMUs
