4f2f8fe50387698ecd9ef2e1ecab9cf77c29b472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 04:08:45 +0000

    Add package appligator/0.0.8/appligator-0.0.8-py3-none-any.whl

---
+++ packages/appligator/appligator-0.0.8-py3-none-any.whl/appligator/__init__.py
@@ -0,0 +1,12 @@
+#  Copyright (c) 2025 by the Eozilla team and contributors
+#  Permissions are hereby granted under the terms of the Apache 2.0 License:
+#  https://opensource.org/license/apache-2-0.
+
+from importlib.metadata import version
+
+__version__ = version("appligator")
+
+
+__all__ = [
+    "__version__",
+]
