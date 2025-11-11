39c2c28837dc92cfe9944af6aa1efe29359eae20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:27:52 +0000

    Add package gilgamesh-summarizer/0.1.9/gilgamesh_summarizer-0.1.9.tar.gz

---
+++ packages/gilgamesh-summarizer/gilgamesh_summarizer-0.1.9.tar.gz/gilgamesh_summarizer-0.1.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gilgamesh_summarizer',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[
+        'rdflib',
+        'unsloth',
+        'transformers',
+        'torch',
+        'networkx',
+        'tqdm',
+        'pandas',
+        'pyjedai',
+    ],
+    python_requires='>=3.10',
+    author='Kostas Plas',
+    author_email='kplas@di.uoa.gr',
+    description='A package for summarizing RDF graphs for Question Answering pipelines',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
