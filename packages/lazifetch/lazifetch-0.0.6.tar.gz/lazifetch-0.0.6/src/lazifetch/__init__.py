aafdff28b285189452692ac2efb8f7a24f247bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-11-11 03:27:36 +0000

    Add package lazifetch/0.0.6/lazifetch-0.0.6.tar.gz

---
+++ packages/lazifetch/lazifetch-0.0.6.tar.gz/lazifetch-0.0.6/src/lazifetch/__init__.py
@@ -0,0 +1,11 @@
+# src/lazifetch/__init__.py
+
+
+from .model.SemanticSearcher import SemanticSearcher
+from .model.Result import Result
+from .utils.llm import get_llms
+
+__all__ = ["SemanticSearcher", "Result", "get_llms"]
+
+
+__version__ = "0.0.6"
